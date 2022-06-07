dc8b256a7a76f28cd01ce2cb39a93a060df18034
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2022-06-07 17:05:08 +0100

    initial layout of base project

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
+plugins {
+    id("org.springframework.boot") version "2.7.0" apply false
+    id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
+    kotlin("jvm") version "1.6.21" apply false
+    kotlin("plugin.spring") version "1.6.21" apply false
+}
+
+allprojects {
+    group = "uk.gov.justice.digital"
+
+    tasks.withType<JavaCompile> {
+        sourceCompatibility = "17"
+    }
+
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "17"
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+    tasks.withType<BootJar> {
+        enabled = false
+    }
+}
+
+subprojects {
+    apply {
+        plugin("org.springframework.boot")
+        plugin("io.spring.dependency-management")
+        plugin("org.jetbrains.kotlin.jvm")
+        plugin("org.jetbrains.kotlin.plugin.spring")
+    }
+
+    repositories {
+        mavenLocal()
+        mavenCentral()
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "probation-integration-services"
