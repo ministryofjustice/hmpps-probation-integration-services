d4ef725eaf1247ea5c59a14eff9535af90a689d7
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2022-06-08 16:16:24 +0100

    added app insights jar at root

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,13 +3,29 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 plugins {
     id("org.springframework.boot") version "2.7.0" apply false
     id("io.spring.dependency-management") version "1.0.11.RELEASE" apply false
-    kotlin("jvm") version "1.6.21" apply false
+    kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21" apply false
     id("com.google.cloud.tools.jib") version "3.2.1" apply false
 }
 
+val agentDeps: Configuration by configurations.creating
+
+project.dependencies.add("agentDeps", "com.microsoft.azure:applicationinsights-agent:3.2.11")
+val copyAgentTask = project.tasks.register("copyAgent", Copy::class.java) {
+    from(agentDeps)
+    into("${project.buildDir}/agent")
+    rename("applicationinsights-agent(.+).jar", "agent.jar")
+}
+
+tasks.getByName("assemble").dependsOn(copyAgentTask)
+
 allprojects {
     group = "uk.gov.justice.digital"
+
+    repositories {
+        mavenLocal()
+        mavenCentral()
+    }
 
     tasks.withType<JavaCompile> {
         sourceCompatibility = "17"
@@ -31,25 +47,12 @@ allprojects {
     }
 }
 
-// Add project name here to prevent attempting to create jib containers
-val noContainerModules = listOf<String>()
-
 subprojects {
     apply {
         plugin("org.springframework.boot")
         plugin("io.spring.dependency-management")
         plugin("org.jetbrains.kotlin.jvm")
         plugin("org.jetbrains.kotlin.plugin.spring")
-
-        if(project.name notIn noContainerModules) {
-            plugin("com.google.cloud.tools.jib")
-        }
-    }
-
-    repositories {
-        mavenLocal()
-        mavenCentral()
+        plugin("com.google.cloud.tools.jib")
     }
 }
-
-infix fun String.notIn(list: List<String>): Boolean = !list.contains(this)
