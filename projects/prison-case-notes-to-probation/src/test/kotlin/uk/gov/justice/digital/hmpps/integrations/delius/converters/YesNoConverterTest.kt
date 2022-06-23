f3af397f9397ea396432cc5e704cab49956ca822
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2022-06-23 15:12:19 +0100

    yes no converter test

---
+++ projects/prison-case-notes-to-probation/src/test/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/converters/YesNoConverterTest.kt
@@ -0,0 +1,39 @@
+package uk.gov.justice.digital.hmpps.integrations.delius.converters
+
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+class YesNoConverterTest {
+    private val converter = BooleanYesNoConverter()
+
+    @ParameterizedTest
+    @MethodSource("databaseColumParams")
+    fun `convert to database column`(attribute: Boolean, column: Char) {
+        val result = converter.convertToDatabaseColumn(attribute)
+        assertThat(result, equalTo(column))
+    }
+
+    @ParameterizedTest
+    @MethodSource("attributeParams")
+    fun `convert from database column`(column: Char, attribute: Boolean) {
+        val result = converter.convertToEntityAttribute(column)
+        assertThat(result, equalTo(attribute))
+    }
+
+    companion object {
+        @JvmStatic
+        private fun databaseColumParams(): List<Arguments> = listOf(
+            Arguments.of(true, 'Y'),
+            Arguments.of(false, 'N')
+        )
+
+        @JvmStatic
+        private fun attributeParams(): List<Arguments> = listOf(
+            Arguments.of('N', false),
+            Arguments.of('Y', true)
+        )
+    }
+}
