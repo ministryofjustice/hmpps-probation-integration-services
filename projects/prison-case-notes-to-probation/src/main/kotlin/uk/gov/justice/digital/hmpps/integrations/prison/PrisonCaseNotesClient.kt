1633fa955b6a71d1e38bf96207ffe8dd5b469566
Marcus Aspin <maspin@unilink.com>
2022-06-27 16:14:24 +0100

    Merge fixes

--- projects/prison-case-notes-to-probation/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/prison/PrisonCaseNotesClient.kt
+++ projects/prison-case-notes-to-probation/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/prison/PrisonCaseNotesClient.kt
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 
-@FeignClient(name = "case-notes", url = "\${integrations.prison-case-notes.url}")
+@FeignClient(name = "prison-case-notes", url = "\${integrations.prison-case-notes.url}")
 interface PrisonCaseNotesClient {
     @GetMapping(value = ["/case-notes/{offenderId}/{caseNoteId}"])
     fun getCaseNote(@PathVariable offenderId: String, @PathVariable caseNoteId: Long): PrisonCaseNote?
