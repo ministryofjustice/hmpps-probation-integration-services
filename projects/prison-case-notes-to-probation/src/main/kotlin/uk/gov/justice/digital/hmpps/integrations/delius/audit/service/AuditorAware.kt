02dc3e571aa6d6bc69801ab4a7a8beffc656e718
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2022-07-01 12:19:35 +0100

    reverted to using lazy block to initialise service user as post construct only works when db is pre-populated, not dev and integration test

--- projects/prison-case-notes-to-probation/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/audit/service/AuditorAware.kt
+++ projects/prison-case-notes-to-probation/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/audit/service/AuditorAware.kt
@@ -13,7 +13,7 @@ class AuditorAware : AuditorAware<Long> {
     override fun getCurrentAuditor(): Optional<Long> {
         val principal = SecurityContextHolder.getContext().authentication?.principal
         return if (principal is ServicePrincipal) {
-            Optional.ofNullable(principal.userId)
+            Optional.ofNullable(principal.userId.value)
         } else Optional.empty()
     }
 }
