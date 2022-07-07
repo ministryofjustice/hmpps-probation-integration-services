d18ce6d2cc8d3a20929b589c3d69371cdea7958d
Marcus Aspin <maspin@unilink.com>
2022-07-07 09:50:04 +0100

    Updated to use JMS delivery-delay property

--- projects/prison-case-notes-to-probation/src/main/kotlin/uk/gov/justice/digital/hmpps/listener/CaseNoteConverter.kt
+++ projects/prison-case-notes-to-probation/src/main/kotlin/uk/gov/justice/digital/hmpps/listener/CaseNoteConverter.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.listener
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.boot.autoconfigure.jms.JmsProperties
 import org.springframework.jms.support.converter.MessageConverter
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.integrations.prison.PrisonOffenderEvent
@@ -10,24 +11,33 @@ import javax.jms.Session
 import javax.jms.TextMessage
 
 @Component
-class CaseNoteConverter(private val om: ObjectMapper) : MessageConverter {
+class CaseNoteConverter(
+    private val objectMapper: ObjectMapper,
+    private val jmsProperties: JmsProperties
+) : MessageConverter {
     override fun toMessage(caseNoteMessage: Any, session: Session): Message {
         val message = session.createTextMessage()
         message.text = when (caseNoteMessage) {
             is String -> caseNoteMessage
-            is PrisonOffenderEvent -> om.writeValueAsString(PrisonOffenderEventMessage(om.writeValueAsString(caseNoteMessage)))
-            is PrisonOffenderEventMessage -> om.writeValueAsString(caseNoteMessage)
+            is PrisonOffenderEvent -> objectMapper.writeValueAsString(PrisonOffenderEventMessage(objectMapper.writeValueAsString(caseNoteMessage)))
+            is PrisonOffenderEventMessage -> objectMapper.writeValueAsString(caseNoteMessage)
             else -> throw IllegalArgumentException("Unexpected message type")
         }
-        // We could move this somewhere and make it conditional, as it's only required when using ActiveMQ.
-        // SQS has the "Delay Seconds" property to achieve the same thing - see https://docs.aws.amazon.com/AWSSimpleQueueService/latest/SQSDeveloperGuide/sqs-delay-queues.html
-        message.setLongProperty("AMQ_SCHEDULED_DELAY", 60 * 1000)
+
+        // We should move this somewhere and make it conditional, as it's only required when using ActiveMQ.
+        // Amazon SQS supports the JMS 2.0 deliveryDelay property, whereas ActiveMQ does not. See https://github.com/apache/activemq/pull/729.
+        // (note: schedulerSupport must be enabled on the ActiveMQ broker)
+        message.setLongProperty("AMQ_SCHEDULED_DELAY", jmsProperties.template.deliveryDelay.toMillis())
+
         return message
     }
 
     override fun fromMessage(message: Message): PrisonOffenderEvent {
         if (message is TextMessage) {
-            return om.readValue(om.readValue(message.text, PrisonOffenderEventMessage::class.java).message, PrisonOffenderEvent::class.java)
+            return objectMapper.readValue(
+                objectMapper.readValue(message.text, PrisonOffenderEventMessage::class.java).message,
+                PrisonOffenderEvent::class.java
+            )
         }
         throw IllegalArgumentException("Unable to convert $message to a PrisonOffenderEvent")
     }
