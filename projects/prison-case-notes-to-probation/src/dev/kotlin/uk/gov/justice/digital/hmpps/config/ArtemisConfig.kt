a05b34c17d1c8981726a2dc6151ee42b364d3c2e
Marcus Aspin <maspin@unilink.com>
2022-07-13 09:00:24 +0100

    Use PathMatchingResourcePatternResolver to dynamically get artemis-plugin script files

--- projects/prison-case-notes-to-probation/src/dev/kotlin/uk/gov/justice/digital/hmpps/config/ArtemisConfig.kt
+++ projects/prison-case-notes-to-probation/src/dev/kotlin/uk/gov/justice/digital/hmpps/config/ArtemisConfig.kt
@@ -6,42 +6,22 @@ import io.hawt.springboot.HawtioPlugin
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.io.ClassPathResource
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver
 import org.springframework.jmx.export.annotation.ManagedResource
 
 @Configuration
 class ArtemisConfig {
-    @Bean /* This bean replicates the plugin setup in the artemis-plugin:war dependency.
+    @Bean /* This bean replicates the plugin setup in the artemis-plugin:war dependency. The references scripts are
+             exposed to the Hawtio UI using the `spring.web.resources.static-locations` property in application.yml.
              See the org.apache.activemq.hawtio.plugin.PluginContextListener#contextInitialized method */
     fun hawtioPlugin() = HawtioPlugin(
         "artemis-plugin", "", "",
-        arrayOf(
-            // This list comes from the web.xml file in the artemis-plugin war
-            "js/artemisHelpers.js",
-            "js/artemisPlugin.js",
-            "js/components/addressSendMessage.js",
-            "js/components/addresses.js",
-            "js/components/browse.js",
-            "js/components/connections.js",
-            "js/components/consumers.js",
-            "js/components/createAddress.js",
-            "js/components/createQueue.js",
-            "js/components/deleteAddress.js",
-            "js/components/deleteQueue.js",
-            "js/components/diagram.js",
-            "js/components/help.js",
-            "js/components/navigation.js",
-            "js/components/preferences.js",
-            "js/components/producers.js",
-            "js/components/queues.js",
-            "js/components/sendMessage.js",
-            "js/components/sessions.js",
-            "js/components/status.js",
-            "js/components/tree.js",
-            "js/services/pagination.js",
-            "js/services/resource.js",
-            "js/services/sendMessageService.js",
-            "js/services/toolbar.js"
-        )
+        PathMatchingResourcePatternResolver()
+            .getResources("classpath:/plugin/**")
+            .map { (it as ClassPathResource).path.removePrefix("plugin/") }
+            .filter { it.endsWith(".js") }
+            .toTypedArray()
     )
 
     @Bean /* This bean bypasses the artemis-plugin authorization when rendering the "Send Message" and "Browse Messages"
