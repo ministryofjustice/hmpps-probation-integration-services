02e5f1957befce6de1620f2c7c07698d95cf9aa0
Marcus Aspin <maspin@unilink.com>
2022-07-14 10:59:20 +0100

    Change wiremock port to prevent conflicts

--- projects/prison-case-notes-to-probation/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/DLQRetryTest.kt
+++ projects/prison-case-notes-to-probation/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/DLQRetryTest.kt
@@ -1,34 +1,26 @@
 package uk.gov.justice.digital.hmpps
 
-import io.specto.hoverfly.junit.core.Hoverfly
-import io.specto.hoverfly.junit.core.HoverflyMode
-import io.specto.hoverfly.junit5.HoverflyExtension
-import io.specto.hoverfly.junit5.api.HoverflyConfig
-import io.specto.hoverfly.junit5.api.HoverflyCore
 import org.apache.activemq.artemis.core.server.embedded.EmbeddedActiveMQ
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.equalTo
 import org.hamcrest.Matchers.greaterThanOrEqualTo
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.jms.core.JmsTemplate
 import org.springframework.test.context.ActiveProfiles
-import uk.gov.justice.digital.hmpps.data.SimulationBuilder
 import uk.gov.justice.digital.hmpps.data.generator.CaseNoteMessageGenerator
 import java.time.Duration
 import java.time.LocalDateTime.now
 
 @ActiveProfiles("integration-test")
-@SpringBootTest(properties = ["spring.jms.template.delivery-delay=1s"])
-@HoverflyCore(
-    mode = HoverflyMode.SIMULATE,
-    config = HoverflyConfig(adminPort = 8888, proxyPort = 8500, webServer = true)
+@SpringBootTest(
+    properties = [
+        "spring.jms.template.delivery-delay=1s",
+        "wiremock.port=7978" // TODO replace this once we've moved to using a dynamic wiremock port.
+    ]
 )
-@ExtendWith(HoverflyExtension::class)
 class DLQRetryTest {
 
     @Value("\${integrations.prison-offender-events.queue}")
@@ -41,21 +33,7 @@ class DLQRetryTest {
     private lateinit var jmsTemplate: JmsTemplate
 
     @Autowired
-    private lateinit var simBuilder: SimulationBuilder
-
-    @Autowired
     private lateinit var embeddedActiveMQ: EmbeddedActiveMQ
-
-    @BeforeEach
-    fun setUp(hoverfly: Hoverfly) {
-        val sources = simBuilder.simulationsFromFile()
-        if (sources.isNotEmpty()) {
-            hoverfly.simulate(
-                sources.first(),
-                *sources.drop(1).toTypedArray()
-            )
-        }
-    }
 
     @Test
     fun `dlq messages are retried after a delay`() {
