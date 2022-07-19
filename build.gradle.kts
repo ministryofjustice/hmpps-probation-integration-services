e1a791b4c0acf1dd675587ab36484c3d35191edb
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2022-07-19 13:16:05 +0100

    ktlintFormat

--- build.gradle.kts
+++ build.gradle.kts
@@ -94,9 +94,10 @@ subprojects {
 
 configure<org.sonarqube.gradle.SonarQubeExtension> {
     properties {
-        property("sonar.coverage.jacoco.xmlReportPaths",
-            project.subprojects.joinToString { "${project.buildDir}/reports/jacoco/test/jacocoTestReport.xml" }
-                    + ", ${project.buildDir}/reports/jacoco/jacocoTestReport/jacocoTestReport.xml"
+        property(
+            "sonar.coverage.jacoco.xmlReportPaths",
+            project.subprojects.joinToString { "${project.buildDir}/reports/jacoco/test/jacocoTestReport.xml" } +
+                ", ${project.buildDir}/reports/jacoco/jacocoTestReport/jacocoTestReport.xml"
         )
     }
 }
