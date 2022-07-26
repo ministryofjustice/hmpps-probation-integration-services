a6ca0b9fc887e849c094ef5349a8bd032e79fbb4
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2022-07-26 17:27:58 +0100

    hardcoded userid to match test.user

--- projects/prison-case-notes-to-probation/src/dev/kotlin/uk/gov/justice/digital/hmpps/data/generator/UserGenerator.kt
+++ projects/prison-case-notes-to-probation/src/dev/kotlin/uk/gov/justice/digital/hmpps/data/generator/UserGenerator.kt
@@ -3,5 +3,5 @@ package uk.gov.justice.digital.hmpps.data.generator
 import uk.gov.justice.digital.hmpps.integrations.delius.audit.User
 
 object UserGenerator {
-    val APPLICATION_USER = User(IdGenerator.getAndIncrement(), "prison-case-notes-to-probation")
+    val APPLICATION_USER = User(2500007091L, "prison-case-notes-to-probation")
 }
