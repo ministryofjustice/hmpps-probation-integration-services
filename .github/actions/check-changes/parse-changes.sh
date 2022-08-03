99eaef9281cd5d53426500caf1af98e97a30e860
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2022-08-03 11:54:13 +0100

    removed case notes to probation

--- .github/actions/check-changes/parse-changes.sh
+++ .github/actions/check-changes/parse-changes.sh
@@ -8,12 +8,16 @@ function topLevelChanges() {
   sed -E 's|^projects/([^/$]*).*|\1|' | sort -u
 }
 
+function isActive() {
+  grep -v 'workforce-allocations-to-delius'
+}
+
 if [ "$COMMONS_CHANGED" == "true" ]; then
-  projects=$(cd projects && echo * | xargs -n1 | toJsonArray)
+  projects=$(cd projects && echo * | xargs -n1 | isActive | toJsonArray)
   echo "Changes detected in common files, rebuild/deploy everything"
   echo "::set-output name=projects::$projects"
 elif [ -n "$PROJECT_FILES" ]; then
-  projects=$(echo $PROJECT_FILES | xargs -n1 | topLevelChanges | toJsonArray)
+  projects=$(echo $PROJECT_FILES | xargs -n1 | topLevelChanges | isActive | toJsonArray)
   echo "Changes detected in: $projects"
   echo "::set-output name=projects::$projects"
 fi
