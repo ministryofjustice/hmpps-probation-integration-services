725ba5478f49afea812ba40c9a395096bbf978d4
Marcus Aspin <maspin@unilink.com>
2022-08-08 11:02:58 +0100

    PI-373 Enable workforce-allocations-to-delius deployment

--- .github/actions/check-changes/parse-changes.sh
+++ .github/actions/check-changes/parse-changes.sh
@@ -8,16 +8,12 @@ function topLevelChanges() {
   sed -E 's|^projects/([^/$]*).*|\1|' | sort -u
 }
 
-function isActive() {
-  grep -v 'workforce-allocations-to-delius'
-}
-
 if [ "$COMMONS_CHANGED" == "true" ]; then
-  projects=$(cd projects && echo * | xargs -n1 | isActive | toJsonArray)
+  projects=$(cd projects && echo * | xargs -n1 | toJsonArray)
   echo "Changes detected in common files, rebuild/deploy everything"
   echo "::set-output name=projects::$projects"
 elif [ -n "$PROJECT_FILES" ]; then
-  projects=$(echo $PROJECT_FILES | xargs -n1 | topLevelChanges | isActive | toJsonArray)
+  projects=$(echo $PROJECT_FILES | xargs -n1 | topLevelChanges | toJsonArray)
   echo "Changes detected in: $projects"
   echo "::set-output name=projects::$projects"
 fi
