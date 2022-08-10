4176c2b77b8442aff867711fc54162dfa9db037e
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2022-08-10 16:00:24 +0100

    updated alfresco config to use service context

--- projects/pre-sentence-reports-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/config/AlfrescoFeignConfig.kt
+++ projects/pre-sentence-reports-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/config/AlfrescoFeignConfig.kt
@@ -7,7 +7,7 @@ class AlfrescoFeignConfig {
     @Bean
     fun requestInterceptor() = RequestInterceptor { template ->
         template.header("X-DocRepository-Remote-User", "N00")
-        template.header("X-DocRepository-Real-Remote-User", "pre-sentence-reports-to-delius")
+        template.header("X-DocRepository-Real-Remote-User", ServiceContext.servicePrincipal()!!.username)
         template.header("Content-Type: multipart/form-data")
     }
 }
