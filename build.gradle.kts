09826300c0a45d035122db5b9fca6bc64a3a5859
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2022-08-18 13:50:34 +0100

    Upgraded Spring Boot to 2.7.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
     kotlin("jvm") version "1.7.10"
     kotlin("plugin.spring") version "1.7.10" apply false
     kotlin("plugin.jpa") version "1.7.10" apply false
-    id("org.springframework.boot") version "2.7.2" apply false
+    id("org.springframework.boot") version "2.7.3" apply false
     id("io.spring.dependency-management") version "1.0.13.RELEASE" apply false
     id("com.google.cloud.tools.jib") apply false
     id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
