8477ec754135d9bdb8d2b50e9179e7dd4821b6fa
Marcus Aspin <maspin@unilink.com>
2022-08-24 17:42:15 +0100

    Disable login page, and log telemetry messages in dev profile (#125)

--- libs/commons/src/main/kotlin/uk/gov/justice/digital/hmpps/telemetry/TelemetryService.kt
+++ libs/commons/src/main/kotlin/uk/gov/justice/digital/hmpps/telemetry/TelemetryService.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.telemetry
 
 import com.microsoft.applicationinsights.TelemetryClient
+import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.message.HmppsEvent
@@ -8,9 +9,13 @@ import uk.gov.justice.digital.hmpps.message.HmppsEvent
 @Async
 @Service
 class TelemetryService(private val telemetryClient: TelemetryClient = TelemetryClient()) {
+    companion object {
+        private val log = LoggerFactory.getLogger(this::class.java)
+    }
 
     fun trackEvent(name: String, properties: Map<String, String> = mapOf(), metrics: Map<String, Double> = mapOf()) {
-        telemetryClient.trackEvent(name, properties, metrics)
+        if (telemetryClient.isDisabled) log.debug("$name $properties $metrics")
+        else telemetryClient.trackEvent(name, properties, metrics)
     }
 
     fun hmppsEventReceived(hmppsEvent: HmppsEvent) {
