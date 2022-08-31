c110778ce35dce76ee949eb619a689ba91281b20
Marcus Aspin <maspin@unilink.com>
2022-08-31 08:31:00 +0100

    Fix latest tag

--- projects/person-search-index-from-delius/build.gradle.kts
+++ projects/person-search-index-from-delius/build.gradle.kts
@@ -3,7 +3,7 @@ apply(plugin = "com.palantir.docker")
 tasks {
     configure<com.palantir.gradle.docker.DockerExtension> {
         name = "ghcr.io/ministryofjustice/hmpps-probation-integration-services/${project.name}:${project.version}"
+        tag("latest", "ghcr.io/ministryofjustice/hmpps-probation-integration-services/${project.name}:latest")
         files(file("container").listFiles())
-        tag("latest", "latest")
     }
 }
