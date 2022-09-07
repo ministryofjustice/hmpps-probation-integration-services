73ad6514c129bfea3792d9609ac3a47cf33b68d8
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2022-09-07 14:26:58 +0100

    added active flag check on offender manager

--- projects/person-search-index-from-delius/container/statement.sql
+++ projects/person-search-index-from-delius/container/statement.sql
@@ -328,7 +328,7 @@ SELECT json_object('activeOffenders' VALUE (SELECT COUNT(1)
                                             WHERE SOFT_DELETED = 0
                                               AND EXISTS(SELECT 1
                                                          FROM OFFENDER_MANAGER om
-                                                         WHERE o.OFFENDER_ID = om.OFFENDER_ID
+                                                         WHERE o.OFFENDER_ID = om.OFFENDER_ID AND om.ACTIVE_FLAG = 1
                                                            AND om.SOFT_DELETED = 0)) RETURNING CLOB) "json",
        -1                                                                                            "offenderId"
 FROM DUAL
