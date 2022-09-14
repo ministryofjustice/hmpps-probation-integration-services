c33d77f208d0586307bed057d63381b9c82c87ef
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2022-09-14 17:02:04 +0100

    Ab pi 425 add management (#172)
    * PI-425 added management scripts to container start up process
    
    * added management schedule env var
    
    * added management schedule env var
    
    * attempt to add subdirectory
    
    * PI-425 updated from review
    
    * update entrypoint
    
    * added auth and fixed typo
    
    * removed increased worker count
    
    * testing schedule from branch
    
    * tail logs and remove unset variable from management script
    
    * pass variables in for host,username,password
    
    * setting default for expected and actual counts
    
    * reverted schedule to daily
    
    * removed -u

--- projects/person-search-index-from-delius/container/management/index-alias-management.sh
+++ projects/person-search-index-from-delius/container/management/index-alias-management.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euo pipefail
+set -eo pipefail
 
 function help {
   echo -e "\\nSCRIPT USAGE\\n"
@@ -37,9 +37,9 @@ done
 
 get_current_indices() {
   echo "Search URL: ${SEARCH_URL}"
-  export PRIMARY_INDEX=$(curl -sSf -XGET -H "Content-Type: application/json" "${SEARCH_URL}/_alias/${PERSON_SEARCH_PRIMARY}" | jq keys | jq -r '.[0]')
+  export PRIMARY_INDEX=$(curl -sSf -XGET -H "Content-Type: application/json" -u "${SEARCH_INDEX_USERNAME}:${SEARCH_INDEX_PASSWORD}" "${SEARCH_URL}/_alias/${PERSON_SEARCH_PRIMARY}" | jq keys | jq -r '.[0]')
   echo "Primary Index => ${PRIMARY_INDEX}"
-  export STANDBY_INDEX=$(curl -sSf -XGET -H "Content-Type: application/json" "${SEARCH_URL}/_alias/${PERSON_SEARCH_STANDBY}" | jq keys | jq -r '.[0]')
+  export STANDBY_INDEX=$(curl -sSf -XGET -H "Content-Type: application/json" -u "${SEARCH_INDEX_USERNAME}:${SEARCH_INDEX_PASSWORD}" "${SEARCH_URL}/_alias/${PERSON_SEARCH_STANDBY}" | jq keys | jq -r '.[0]')
   echo "Standby Index => ${STANDBY_INDEX}"
 
   if [ -z "$PRIMARY_INDEX" ] || [ -z "$STANDBY_INDEX" ] || [ "$PRIMARY_INDEX" = 'error' ] || [ "$STANDBY_INDEX" = 'error' ]; then
@@ -50,8 +50,8 @@ get_current_indices() {
 
 delete_ready_for_reindex() {
   echo "deleting ${STANDBY_INDEX} ready for indexing ..."
-  curl -sSf -XDELETE -H "Content-Type: application/json" "${SEARCH_URL}/${STANDBY_INDEX}"
-  curl -sSf -XPUT -H "Content-Type: application/json" "${SEARCH_URL}/${STANDBY_INDEX}" -d '{
+  curl -sSf -XDELETE -H "Content-Type: application/json" -u "${SEARCH_INDEX_USERNAME}:${SEARCH_INDEX_PASSWORD}" "${SEARCH_URL}/${STANDBY_INDEX}"
+  curl -sSf -XPUT -H "Content-Type: application/json" -u "${SEARCH_INDEX_USERNAME}:${SEARCH_INDEX_PASSWORD}" "${SEARCH_URL}/${STANDBY_INDEX}" -d '{
                                          "aliases": {
                                              "'"${PERSON_SEARCH_STANDBY}"'": {}
                                          }
@@ -59,17 +59,17 @@ delete_ready_for_reindex() {
 }
 
 check_count_document() {
-  export EXPECTED_COUNT=$(curl -sS -XGET -H "Content-Type: application/json" "${SEARCH_URL}/${STANDBY_INDEX}/_doc/-1" | jq '._source.activeOffenders')
+  export EXPECTED_COUNT=$(curl -sS -XGET -H "Content-Type: application/json" -u "${SEARCH_INDEX_USERNAME}:${SEARCH_INDEX_PASSWORD}" "${SEARCH_URL}/${STANDBY_INDEX}/_doc/-1" | jq '._source.activeOffenders')
 
   SECONDS=0
-  until [[ "${EXPECTED_COUNT}" -gt 0 ]]; do
+  until [[ "${EXPECTED_COUNT:-0}" -gt 0 ]]; do
     echo 'waiting for count to be indexed ... '
     if (("${SECONDS}" >= "${MAX_TIMEOUT}")); then
       echo "Timed out getting index count."
       exit 1
     fi
     sleep 10
-    export EXPECTED_COUNT=$(curl -sS -XGET -H "Content-Type: application/json" "${SEARCH_URL}/${STANDBY_INDEX}/_doc/-1" | jq '._source.activeOffenders')
+    export EXPECTED_COUNT=$(curl -sS -XGET -H "Content-Type: application/json" -u "${SEARCH_INDEX_USERNAME}:${SEARCH_INDEX_PASSWORD}" "${SEARCH_URL}/${STANDBY_INDEX}/_doc/-1" | jq '._source.activeOffenders')
   done
   echo "Expected Count After Indexing ${EXPECTED_COUNT}"
 }
@@ -78,16 +78,16 @@ wait_for_index_to_complete() {
   printf "\\nwaiting for indexing to complete ...\\n"
   check_count_document
 
-  ACTUAL_COUNT=$(curl -sS -XGET -H "Content-Type: application/json" "${SEARCH_URL}/${STANDBY_INDEX}/_count" | jq '.count')
+  ACTUAL_COUNT=$(curl -sS -XGET -H "Content-Type: application/json" -u "${SEARCH_INDEX_USERNAME}:${SEARCH_INDEX_PASSWORD}" "${SEARCH_URL}/${STANDBY_INDEX}/_count" | jq '.count')
 
-  until [[ "${ACTUAL_COUNT}" -ge "${EXPECTED_COUNT}" ]]; do
+  until [[ "${ACTUAL_COUNT:-0}" -ge "${EXPECTED_COUNT}" ]]; do
     echo 'waiting for actual count to be at least expected count ...'
     if (("${SECONDS}" >= "${MAX_TIMEOUT}")); then
       echo "Indexing process timed out: Expected ${EXPECTED_COUNT} but got ${ACTUAL_COUNT}"
       exit 1
     fi
     sleep 5
-    ACTUAL_COUNT=$(curl -sS -XGET -H "Content-Type: application/json" "${SEARCH_URL}/${STANDBY_INDEX}/_count" | jq '.count')
+    ACTUAL_COUNT=$(curl -sS -XGET -H "Content-Type: application/json" -u "${SEARCH_INDEX_USERNAME}:${SEARCH_INDEX_PASSWORD}" "${SEARCH_URL}/${STANDBY_INDEX}/_count" | jq '.count')
   done
   echo "Actual Count is ${ACTUAL_COUNT} => Expected ${EXPECTED_COUNT}"
 }
@@ -97,7 +97,7 @@ switch_aliases() {
   echo "primary => $PRIMARY_INDEX"
   echo "standby => $STANDBY_INDEX"
 
-  curl -sSf -XPOST -H "Content-Type: application/json" "${SEARCH_URL}"/_aliases -d '{
+  curl -sSf -XPOST -H "Content-Type: application/json" -u "${SEARCH_INDEX_USERNAME}:${SEARCH_INDEX_PASSWORD}" "${SEARCH_URL}"/_aliases -d '{
                                       "actions": [
                                         {
                                           "remove": {
