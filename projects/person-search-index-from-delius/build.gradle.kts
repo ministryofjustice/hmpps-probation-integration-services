22c9d1caa56a6bc22a4344c7805a776aff7e3d4c
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2022-09-14 10:23:45 +0100

    attempt to add subdirectory

--- projects/person-search-index-from-delius/build.gradle.kts
+++ projects/person-search-index-from-delius/build.gradle.kts
@@ -4,6 +4,6 @@ tasks {
     configure<com.palantir.gradle.docker.DockerExtension> {
         name = "ghcr.io/ministryofjustice/hmpps-probation-integration-services/${project.name}:${project.version}"
         tag("latest", "ghcr.io/ministryofjustice/hmpps-probation-integration-services/${project.name}:latest")
-        files(file("container").listFiles())
+        files("container")
     }
 }
