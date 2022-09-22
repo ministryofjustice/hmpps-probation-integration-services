7036e5202d236e6d0eba61356633c7a11accdc0a
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2022-09-22 13:57:45 +0100

    added test

---
+++ projects/pre-sentence-reports-to-delius/src/test/kotlin/uk/gov/justice/digital/hmpps/controller/PSRContextControllerTest.kt
@@ -0,0 +1,49 @@
+package uk.gov.justice.digital.hmpps.controller
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.whenever
+import uk.gov.justice.digital.hmpps.integrations.delius.presentencereport.Address
+import uk.gov.justice.digital.hmpps.integrations.delius.presentencereport.Court
+import uk.gov.justice.digital.hmpps.integrations.delius.presentencereport.LocalJusticeArea
+import uk.gov.justice.digital.hmpps.integrations.delius.presentencereport.Name
+import uk.gov.justice.digital.hmpps.integrations.delius.presentencereport.Offence
+import uk.gov.justice.digital.hmpps.integrations.delius.presentencereport.PreSentenceReportContext
+import uk.gov.justice.digital.hmpps.integrations.delius.presentencereport.PreSentenceReportService
+import java.text.SimpleDateFormat
+
+@ExtendWith(MockitoExtension::class)
+class PSRContextControllerTest {
+    @Mock
+    private lateinit var preSentenceReportService: PreSentenceReportService
+
+    @InjectMocks
+    private lateinit var pSRContextController: PSRContextController
+
+    @Test
+    fun `Get pre sentence report context`() {
+        whenever(preSentenceReportService.getPreSentenceReportContext("1")).thenReturn(getPreSentenceReportContext())
+        val result = pSRContextController.getPreSentenceReportContext("1")
+        Assertions.assertNotNull(result)
+    }
+
+    private fun getPreSentenceReportContext(): PreSentenceReportContext {
+
+        val formatter = SimpleDateFormat("dd-MM-yyyy")
+
+        return PreSentenceReportContext(
+            "X123123",
+            Name("forename", "surename", "middlename"),
+            formatter.parse("12-12-2000"),
+            "PNC123",
+            Address("N","building name","123", "StreetName", "Town", "District", "County", "NE1 2SW"),
+            Offence("MainOffence"),
+            listOf(Offence("other offence")),
+            Court("CourtName", LocalJusticeArea("Local justice area"))
+        )
+    }
+}
