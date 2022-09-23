8df911928cc6e099977979d17f6190fcea4240b9
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2022-09-23 08:43:43 +0100

    added test

--- projects/pre-sentence-reports-to-delius/src/test/kotlin/uk/gov/justice/digital/hmpps/controller/PSRContextControllerTest.kt
+++ projects/pre-sentence-reports-to-delius/src/test/kotlin/uk/gov/justice/digital/hmpps/controller/PSRContextControllerTest.kt
@@ -1,12 +1,15 @@
 package uk.gov.justice.digital.hmpps.controller
 
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
 import org.mockito.kotlin.whenever
+import uk.gov.justice.digital.hmpps.exception.NotFoundException
 import uk.gov.justice.digital.hmpps.integrations.delius.presentencereport.Address
 import uk.gov.justice.digital.hmpps.integrations.delius.presentencereport.Court
 import uk.gov.justice.digital.hmpps.integrations.delius.presentencereport.LocalJusticeArea
@@ -29,6 +32,14 @@ class PSRContextControllerTest {
         whenever(preSentenceReportService.getPreSentenceReportContext("1")).thenReturn(getPreSentenceReportContext())
         val result = pSRContextController.getPreSentenceReportContext("1")
         Assertions.assertNotNull(result)
+    }
+
+    @Test
+    fun `Get pre sentence report context not found`() {
+        whenever(preSentenceReportService.getPreSentenceReportContext("1")).thenThrow(NotFoundException("Not found"))
+
+        val exception: Exception = assertThrows(NotFoundException::class.java) { pSRContextController.getPreSentenceReportContext("1") }
+        assertTrue(exception.message!!.contains("Not found"))
     }
 
     private fun getPreSentenceReportContext(): PreSentenceReportContext {
