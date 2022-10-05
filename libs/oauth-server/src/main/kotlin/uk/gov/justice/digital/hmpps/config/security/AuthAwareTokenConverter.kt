308e34d63b241acb89c337dd93544d8d4999fe3d
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2022-10-05 09:14:54 +0100

    PI-459 separate oauth-server into lib

--- libs/oauth-server/src/main/kotlin/uk/gov/justice/digital/hmpps/config/security/AuthAwareTokenConverter.kt
+++ libs/oauth-server/src/main/kotlin/uk/gov/justice/digital/hmpps/config/security/AuthAwareTokenConverter.kt
@@ -1,4 +1,4 @@
-package uk.gov.justice.digital.hmpps.config
+package uk.gov.justice.digital.hmpps.config.security
 
 import org.springframework.core.convert.converter.Converter
 import org.springframework.security.authentication.AbstractAuthenticationToken
--- libs/oauth-server/src/main/kotlin/uk/gov/justice/digital/hmpps/config/security/OAuth2ServerSecurityConfig.kt
+++ libs/oauth-server/src/main/kotlin/uk/gov/justice/digital/hmpps/config/security/OAuth2ServerSecurityConfig.kt
@@ -1,13 +1,12 @@
-package uk.gov.justice.digital.hmpps.config
+package uk.gov.justice.digital.hmpps.config.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.web.SecurityFilterChain
-import uk.gov.justice.digital.hmpps.config.security.OAuth2ClientSecurityConfig
 
 @Configuration
-class OAuth2ResourceServerConfig : OAuth2ClientSecurityConfig() {
+class OAuth2ServerSecurityConfig : OAuth2ClientSecurityConfig() {
     @Bean
     fun configureResourceServer(http: HttpSecurity): SecurityFilterChain {
         filterChain(http)
