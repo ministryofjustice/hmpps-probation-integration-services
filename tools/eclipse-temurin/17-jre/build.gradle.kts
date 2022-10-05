3c7d9e453b3fbac393ca96d3d6bfb62bc794cf83
Marcus Aspin <maspin@unilink.com>
2022-10-05 10:09:33 +0100

    Fix tagging of tools images (#226)

--- tools/eclipse-temurin/17-jre/build.gradle.kts
+++ tools/eclipse-temurin/17-jre/build.gradle.kts
@@ -3,5 +3,6 @@ apply(plugin = "com.palantir.docker")
 tasks {
     configure<com.palantir.gradle.docker.DockerExtension> {
         name = "ghcr.io/ministryofjustice/hmpps-probation-integration-services/eclipse-temurin:17-jre-alpine"
+        tag("17-jre-alpine", "ghcr.io/ministryofjustice/hmpps-probation-integration-services/eclipse-temurin:17-jre-alpine")
     }
 }
--- tools/opensearch-logstash/8/build.gradle.kts
+++ tools/opensearch-logstash/8/build.gradle.kts
@@ -3,5 +3,6 @@ apply(plugin = "com.palantir.docker")
 tasks {
     configure<com.palantir.gradle.docker.DockerExtension> {
         name = "ghcr.io/ministryofjustice/hmpps-probation-integration-services/opensearch-logstash:8"
+        tag("8", "ghcr.io/ministryofjustice/hmpps-probation-integration-services/opensearch-logstash:8")
     }
 }
