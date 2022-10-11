e6649ef8ef189c780243739f1d83b86b662ad636
Marcus Aspin <maspin@unilink.com>
2022-10-11 13:00:05 +0100

    Use buildx for existing images

--- tools/opensearch-logstash/8/build.gradle.kts
+++ tools/opensearch-logstash/8/build.gradle.kts
@@ -4,5 +4,7 @@ tasks {
     configure<com.palantir.gradle.docker.DockerExtension> {
         name = "ghcr.io/ministryofjustice/hmpps-probation-integration-services/opensearch-logstash:8"
         tag("8", "ghcr.io/ministryofjustice/hmpps-probation-integration-services/opensearch-logstash:8")
+        buildx(true)
+        push(System.getenv("PUSH") == "true")
     }
 }
