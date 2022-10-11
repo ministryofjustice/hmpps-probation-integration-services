508fdf2311277bf6c689af1a1ba709b7cf65ac3c
Marcus Aspin <maspin@unilink.com>
2022-10-11 15:16:53 +0100

    Fix pushing multiplatform images (#241)
    * Updated to only push from main and added 17-jre-jammy build
    
    * Use buildx for existing images

--- tools/eclipse-temurin/17-jre-alpine/build.gradle.kts
+++ tools/eclipse-temurin/17-jre-alpine/build.gradle.kts
@@ -4,5 +4,7 @@ tasks {
     configure<com.palantir.gradle.docker.DockerExtension> {
         name = "ghcr.io/ministryofjustice/hmpps-probation-integration-services/eclipse-temurin:17-jre-alpine"
         tag("17-jre-alpine", "ghcr.io/ministryofjustice/hmpps-probation-integration-services/eclipse-temurin:17-jre-alpine")
+        buildx(true)
+        push(System.getenv("PUSH") == "true")
     }
 }
--- tools/settings.gradle.kts
+++ tools/settings.gradle.kts
@@ -1,5 +1,6 @@
 include(
-    "eclipse-temurin:17-jre",
+    "eclipse-temurin:17-jre-alpine",
+    "eclipse-temurin:17-jre-jammy",
     "eclipse-temurin:19-jre-jammy",
     "opensearch-logstash:8"
 )
