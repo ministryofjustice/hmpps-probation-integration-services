03da2837c67fb4f1473d1f9871fdf3e50a6ce5d1
Marcus Aspin <maspin@unilink.com>
2022-10-21 14:13:27 +0100

    Move ControllerAdvice from template into library

---
+++ libs/oauth-client/src/main/kotlin/uk/gov/justice/digital/hmpps/advice/ControllerAdvice.kt
@@ -0,0 +1,28 @@
+package uk.gov.justice.digital.hmpps.advice
+
+import org.springframework.http.HttpStatus.BAD_REQUEST
+import org.springframework.http.HttpStatus.NOT_FOUND
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import uk.gov.justice.digital.hmpps.exception.NotFoundException
+
+@RestControllerAdvice(basePackages = ["uk.gov.justice.digital.hmpps"])
+class ControllerAdvice {
+    @ExceptionHandler(NotFoundException::class)
+    fun handleNotFound(e: NotFoundException) = ResponseEntity
+        .status(NOT_FOUND)
+        .body(ErrorResponse(status = NOT_FOUND.value(), message = e.message))
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleMethodArgumentNotValid(e: MethodArgumentNotValidException) = ResponseEntity
+        .badRequest()
+        .body(
+            ErrorResponse(
+                status = BAD_REQUEST.value(),
+                message = "Method argument not valid",
+                fields = e.bindingResult.fieldErrors.map { FieldError(it.code, it.defaultMessage, it.field) }
+            )
+        )
+}
--- templates/projects/message-listener-with-api-client-and-server/src/main/kotlin/uk/gov/justice/digital/hmpps/controller/advice/ErrorResponse.kt
+++
@@ -1,6 +0,0 @@
-package uk.gov.justice.digital.hmpps.controller.advice
-
-data class ErrorResponse(
-    val status: Int,
-    val message: String? = null,
-)
