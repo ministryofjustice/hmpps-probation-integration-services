d758627baa5cc1455593134748bd12f3ebeea93e
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2022-10-21 13:39:35 +0100

    PI-498 create template for api only (no messaging)

--- templates/projects/api-client-and-server/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/IntegrationTest.kt
+++ templates/projects/api-client-and-server/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/IntegrationTest.kt
@@ -22,7 +22,6 @@ import java.util.concurrent.TimeoutException
 @ActiveProfiles("integration-test")
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 internal class IntegrationTest {
-    @Value("\${spring.jms.template.default-destination}") lateinit var queueName: String
 
     @Autowired lateinit var mockMvc: MockMvc
     @Autowired lateinit var wireMockServer: WireMockServer
