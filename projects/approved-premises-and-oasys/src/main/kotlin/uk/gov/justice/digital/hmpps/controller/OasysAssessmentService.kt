f98228d785fe2c88759eb02c292e954c31a24d0e
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2022-10-24 15:21:46 +0100

    Latest layer 3 Oasys assessment

--- projects/approved-premises-and-oasys/src/main/kotlin/uk/gov/justice/digital/hmpps/controller/OasysAssessmentService.kt
+++ projects/approved-premises-and-oasys/src/main/kotlin/uk/gov/justice/digital/hmpps/controller/OasysAssessmentService.kt
@@ -8,13 +8,14 @@ import uk.gov.justice.digital.hmpps.integrations.oasys.OasysClient
 @Service
 class OasysAssessmentService(private var oasysClient: OasysClient) {
     fun getLatestAssessment(crn: String): OasysAssessment {
-        val ordsAssessmentTimeline = oasysClient.getAssessmentTimeline(crn) ?: throw NotFoundException("No Assessments were found for crn= $crn")
+        val ordsAssessmentTimeline =
+            oasysClient.getAssessmentTimeline(crn) ?: throw NotFoundException("No Assessments were found for crn= $crn")
         val assessments =
             ordsAssessmentTimeline.timeline.sortedByDescending { it.completedDate }.stream().filter {
                 it.assessmentType == "LAYER3" &&
                     it.status == "COMPLETE"
             }
-        return assessments.findFirst().orElseThrow{
+        return assessments.findFirst().orElseThrow {
             NotFoundException("Latest layer 3 assessment not found for crn=$crn")
         }
     }
