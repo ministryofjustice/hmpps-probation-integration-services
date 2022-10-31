4dbf0754bf79454e9ff1e1b6f632f4ecfc9dfa5b
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2022-10-31 15:34:51 +0000

    debugging

--- schema-spy/run.sh
+++ schema-spy/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-java -Xmx8G -Xms8G -jar ./artifacts/schemaspy-6.1.0.jar -t orathin-service \
+java -Xmx2G -Xms2G -jar ./artifacts/schemaspy-6.1.0.jar -t orathin-service \
           -dp ./artifacts/ojdbc8.jar \
           -db "${DB}" \
           -host "${HOST}" \
