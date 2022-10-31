9b92938e8961badfa258fdd4259c140d1d0d18fe
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2022-10-31 12:34:53 +0000

    PI-247 Schema spy report GH action

--- schema-spy/run.sh
+++ schema-spy/run.sh
@@ -10,6 +10,6 @@ java -jar ./artifacts/schemaspy-6.1.0.jar -t orathin-service \
           -u "${USERNAME}" \
           -p "${PASSWORD}" \
           -cat "${SCHEMA}" \
-          -i "OFFENDER" \
+          -I "^(^Z.*$|^.*[0-9]$|^SPG.*$|^PRF_.*$|^MIS_.*$|^.*\\$.*$)$" \
           -vizjs \
           -o ./DeliusSchema
