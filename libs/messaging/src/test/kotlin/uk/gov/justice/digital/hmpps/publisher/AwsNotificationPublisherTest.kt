030c828d1d11de6a084b7968b1f0920e93e7ce94
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2022-11-07 10:55:22 +0000

    notification test

--- libs/messaging/src/test/kotlin/uk/gov/justice/digital/hmpps/publisher/AwsNotificationPublisherTest.kt
+++ libs/messaging/src/test/kotlin/uk/gov/justice/digital/hmpps/publisher/AwsNotificationPublisherTest.kt
@@ -3,7 +3,7 @@ package uk.gov.justice.digital.hmpps.publisher
 import com.fasterxml.jackson.databind.node.JsonNodeFactory
 import io.awspring.cloud.messaging.core.NotificationMessagingTemplate
 import org.hamcrest.MatcherAssert.assertThat
-import org.hamcrest.Matchers.containsInAnyOrder
+import org.hamcrest.Matchers.equalTo
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
@@ -15,6 +15,7 @@ import org.mockito.kotlin.verify
 import org.springframework.messaging.MessageHeaders
 import org.springframework.messaging.core.MessagePostProcessor
 import org.springframework.messaging.support.MessageBuilder
+import uk.gov.justice.digital.hmpps.message.MessageAttribute
 import uk.gov.justice.digital.hmpps.message.MessageAttributes
 import uk.gov.justice.digital.hmpps.message.Notification
 
@@ -35,7 +36,9 @@ class AwsNotificationPublisherTest {
     fun `can publish notification message`() {
         val json = JsonNodeFactory.instance.objectNode()
         json.put("Test", "value")
-        val notification = Notification(json, MessageAttributes("OFFENDER_UPDATED"))
+        val attrs = MessageAttributes("OFFENDER_UPDATED")
+        attrs["anotherValue"] = MessageAttribute("String", "Test")
+        val notification = Notification(json, attrs)
 
         val captor = ArgumentCaptor.forClass(MessagePostProcessor::class.java)
 
@@ -44,9 +47,17 @@ class AwsNotificationPublisherTest {
         verify(notificationTemplate).convertAndSend(
             eq(notification.message), captor.capture()
         )
-        val message = captor.value.postProcessMessage(MessageBuilder.createMessage(notification.message, MessageHeaders(mapOf())))
-        assertThat(message.headers.entries.map { Pair(it.key, it.value) },
-            containsInAnyOrder("eventType" to "OFFENDER_UPDATED")
+        val message = captor.value.postProcessMessage(
+            MessageBuilder.createMessage(notification.message, MessageHeaders(mapOf()))
+        )
+
+        assertThat(
+            message.headers["eventType"],
+            equalTo("OFFENDER_UPDATED")
+        )
+        assertThat(
+            message.headers["anotherValue"],
+            equalTo("Test")
         )
 
     }
