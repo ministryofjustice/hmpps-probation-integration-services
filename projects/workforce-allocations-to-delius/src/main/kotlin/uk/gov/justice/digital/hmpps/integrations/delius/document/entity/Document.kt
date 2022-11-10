14559c3e61f0bbcd14b9f6f3899ae022d964412f
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2022-11-10 16:31:24 +0000

    PI-538 Retrieving workforce documents

--- projects/workforce-allocations-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/document/entity/Document.kt
+++ projects/workforce-allocations-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/document/entity/Document.kt
@@ -47,7 +47,6 @@ abstract class Document : Relatable {
     open var alfrescoId: String? = null
     open var sensitive: Boolean = false
     open var lastSaved: ZonedDateTime = ZonedDateTime.now()
-
     @Column(name = "created_datetime")
     open var createdDate: ZonedDateTime = ZonedDateTime.now()
 }
