6ac112c8b513425c3f55aa2b177a3ee3415702e8
Marcus Aspin <maspin@unilink.com>
2022-11-15 17:24:55 +0000

    PI-581 Increase default page size for AP key worker endpoint

--- projects/approved-premises-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/controller/StaffController.kt
+++ projects/approved-premises-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/controller/StaffController.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.controller
 
 import org.springframework.data.domain.Pageable
+import org.springframework.data.web.PageableDefault
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -17,6 +18,6 @@ class StaffController(
     fun getStaff(
         @PathVariable code: String,
         @RequestParam(required = false, defaultValue = "false") keyWorker: Boolean,
-        pageable: Pageable = Pageable.unpaged()
+        @PageableDefault(value = 100) pageable: Pageable = Pageable.ofSize(100)
     ) = staffService.getStaffInApprovedPremises(code, keyWorker, pageable)
 }
