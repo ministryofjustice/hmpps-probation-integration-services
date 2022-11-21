9953edbadc0ef3c9cf71d6a3d4e81c28ef310d0c
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2022-11-21 16:53:24 +0000

    PI-579 Changes to Allocation Demand API to bring back all data for Unallocated Cases

--- projects/workforce-allocations-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/allocationdemand/AllocationResponse.kt
+++ projects/workforce-allocations-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/allocationdemand/AllocationResponse.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.api.allocationdemand
 
+import com.fasterxml.jackson.annotation.JsonValue
 import java.time.LocalDate
 
 data class AllocationResponse(
@@ -7,7 +8,9 @@ data class AllocationResponse(
     val name: Name,
     val event: Event,
     val sentence: Sentence?,
-    val initialAppointment: InitialAppointment?
+    val initialAppointment: InitialAppointment?,
+    val type: CaseType = CaseType.UNKNOWN,
+    val status: ProbationStatus
 )
 
 data class Name(
@@ -16,10 +19,28 @@ data class Name(
     val surname: String,
 )
 
-data class Event(val number: String, val manager: EventManager)
-data class EventManager(val code: String, val name: Name, val teamCode: String)
+data class Event(val number: String, val manager: Manager)
+data class Manager(val code: String, val name: Name, val teamCode: String, val grade: String? = null)
 data class Sentence(val type: String, val date: LocalDate, val length: String)
 
 data class InitialAppointment(val date: LocalDate)
 
 data class AllocationDemandResponse(val cases: List<AllocationResponse>)
+
+data class ProbationStatus(
+    val status: ManagementStatus,
+    val previousManager: Manager? = null
+)
+
+enum class ManagementStatus(
+    @JsonValue
+    val status: String
+) {
+    CURRENTLY_MANAGED("Currently managed"),
+    PREVIOUSLY_MANAGED("Previously managed"),
+    NEW_TO_PROBATION("New to probation")
+}
+
+enum class CaseType {
+    LICENSE, CUSTODY, COMMUNITY, UNKNOWN
+}
