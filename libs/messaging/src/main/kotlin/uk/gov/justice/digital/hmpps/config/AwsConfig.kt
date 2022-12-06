4dec76d2f0eb1a3974daf71119e917592ed0bc3d
Marcus Aspin <maspin@unilink.com>
2022-12-06 09:54:36 +0000

    PI-609 Fix MessageConversionException due to incorrect ObjectMapper

--- libs/messaging/src/main/kotlin/uk/gov/justice/digital/hmpps/config/AwsConfig.kt
+++ libs/messaging/src/main/kotlin/uk/gov/justice/digital/hmpps/config/AwsConfig.kt
@@ -1,16 +1,26 @@
 package uk.gov.justice.digital.hmpps.config
 
 import com.amazonaws.services.sns.AmazonSNS
+import com.fasterxml.jackson.databind.ObjectMapper
 import io.awspring.cloud.messaging.core.NotificationMessagingTemplate
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Conditional
 import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.converter.MappingJackson2MessageConverter
 
 @Configuration
 class AwsConfig {
     @Bean
     @Conditional(AwsCondition::class)
     @ConditionalOnProperty("messaging.producer.topic")
-    fun notificationMessagingTemplate(amazonSNS: AmazonSNS) = NotificationMessagingTemplate(amazonSNS)
+    fun notificationMessagingTemplate(objectMapper: ObjectMapper, amazonSns: AmazonSNS): NotificationMessagingTemplate {
+        val notificationMessagingTemplate = NotificationMessagingTemplate(amazonSns)
+        val mappingJackson2MessageConverter = MappingJackson2MessageConverter()
+        mappingJackson2MessageConverter.isStrictContentTypeMatch = false
+        mappingJackson2MessageConverter.serializedPayloadClass = String::class.java
+        mappingJackson2MessageConverter.objectMapper = objectMapper
+        notificationMessagingTemplate.messageConverter = mappingJackson2MessageConverter
+        return notificationMessagingTemplate
+    }
 }
