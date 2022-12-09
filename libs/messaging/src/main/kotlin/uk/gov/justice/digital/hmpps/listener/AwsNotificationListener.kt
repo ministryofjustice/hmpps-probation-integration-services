5008a650d84f1ae3b07d1180565fd990b5fa8ed8
Marcus Aspin <maspin@unilink.com>
2022-12-09 10:19:34 +0000

    PI-610 Updated SQS listener to capture Sentry exceptions

--- libs/messaging/src/main/kotlin/uk/gov/justice/digital/hmpps/listener/AwsNotificationListener.kt
+++ libs/messaging/src/main/kotlin/uk/gov/justice/digital/hmpps/listener/AwsNotificationListener.kt
@@ -1,11 +1,15 @@
 package uk.gov.justice.digital.hmpps.listener
 
 import io.awspring.cloud.sqs.annotation.SqsListener
+import io.awspring.cloud.sqs.listener.AsyncAdapterBlockingExecutionFailedException
+import io.awspring.cloud.sqs.listener.ListenerExecutionFailedException
+import io.sentry.Sentry
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Conditional
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.config.AwsCondition
 import uk.gov.justice.digital.hmpps.messaging.NotificationHandler
+import java.util.concurrent.CompletionException
 
 @Component
 @Conditional(AwsCondition::class)
@@ -15,6 +19,20 @@ class AwsNotificationListener(
 ) {
     @SqsListener("\${messaging.consumer.queue}")
     fun receive(message: String) {
-        handler.handle(message)
+        try {
+            handler.handle(message)
+        } catch (e: Throwable) {
+            Sentry.captureException(unwrapSqsExceptions(e))
+            throw e
+        }
+    }
+
+    fun unwrapSqsExceptions(e: Throwable): Throwable {
+        fun unwrap(e: Throwable) = e.cause ?: e
+        var cause = e
+        if (cause is CompletionException) { cause = unwrap(cause) }
+        if (cause is AsyncAdapterBlockingExecutionFailedException) { cause = unwrap(cause) }
+        if (cause is ListenerExecutionFailedException) { cause = unwrap(cause) }
+        return cause
     }
 }
