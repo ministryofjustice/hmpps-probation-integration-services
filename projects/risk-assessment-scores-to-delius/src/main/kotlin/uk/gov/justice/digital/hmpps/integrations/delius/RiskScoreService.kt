e19553fd5d0488419165256d33bf9daafb48cff3
Marcus Aspin <maspin@unilink.com>
2022-12-15 19:55:27 +0000

    Added known validation message for incorrect event number

--- projects/risk-assessment-scores-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/RiskScoreService.kt
+++ projects/risk-assessment-scores-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/RiskScoreService.kt
@@ -70,6 +70,7 @@ class RiskScoreService(jdbcTemplate: JdbcTemplate) {
         private val KNOWN_VALIDATION_MESSAGES = listOf(
             "The existing CAS Assessment Date is greater than a specified P_ASSESSMENT_DATE value",
             "The Event is Soft Deleted",
+            "The event number does not exist against the specified Offender",
             "CRN/Offender does not exist",
         )
     }
