5945a57c98970d20525ead11e6a94cc988f4582f
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-01-04 12:36:41 +0000

    use lock to write changes to queue

--- libs/dev-tools/src/main/kotlin/uk/gov/justice/digital/hmpps/messaging/HmppsDevQueue.kt
+++ libs/dev-tools/src/main/kotlin/uk/gov/justice/digital/hmpps/messaging/HmppsDevQueue.kt
@@ -11,40 +11,60 @@ import uk.gov.justice.digital.hmpps.message.Notification
 import uk.gov.justice.digital.hmpps.publisher.NotificationPublisher
 import java.time.Duration
 import java.time.LocalDateTime
+import java.util.LinkedList
+import java.util.Queue
 import java.util.UUID
-import java.util.concurrent.BlockingQueue
-import java.util.concurrent.ConcurrentHashMap
-import java.util.concurrent.LinkedBlockingQueue
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.TimeoutException
+import java.util.concurrent.locks.ReentrantLock
 
 abstract class NotificationChannel(
     val name: String
 ) {
-    private val messages: BlockingQueue<Notification<*>> = LinkedBlockingQueue()
-    private val processing: MutableMap<UUID, Notification<*>> = ConcurrentHashMap()
+    private val lock = ReentrantLock()
+    private val messages: Queue<Notification<*>> = LinkedList()
+    private val processing: MutableMap<UUID, Notification<*>> = HashMap()
     fun publish(notification: Notification<*>) {
-        messages.put(notification)
+        lock.lock()
+        try {
+            messages.add(notification)
+        } finally {
+            lock.unlock()
+        }
     }
 
     fun receive(): Notification<*>? {
+        lock.lock()
         val notification = try {
-            messages.poll(30, TimeUnit.SECONDS)
+            val peek = messages.peek()
+            val notification = peek?.let {
+                processing[it.id] = it
+                messages.poll()
+            }
+            notification
         } catch (ignore: InterruptedException) {
             null
+        } finally {
+            lock.unlock()
         }
-        notification?.also { processing[it.id] = it }
         return notification
     }
 
     fun confirm(notificationId: UUID) {
-        processing.remove(notificationId)
+        lock.lock()
+        try {
+            processing.remove(notificationId)
+        } finally {
+            lock.unlock()
+        }
     }
 
     fun fail(notificationId: UUID) {
-        processing[notificationId]?.let {
-            messages.put(it)
-            processing.remove(it.id)
+        lock.lock()
+        try {
+            messages.add(processing.remove(notificationId))
+        } finally {
+            lock.unlock()
         }
     }
 
@@ -92,7 +112,7 @@ class HmppsNotificationListener(
     private val handler: NotificationHandler<*>
 ) {
 
-    @Scheduled(fixedDelay = 10, initialDelay = 1000)
+    @Scheduled(fixedDelay = 100)
     fun receive() {
         val queue = channelManager.getChannel(queueName)
         val notification = queue.receive()
