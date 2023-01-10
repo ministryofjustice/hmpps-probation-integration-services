32e16ff41a868a849cff5b5eff55de75ab567713
Marcus Aspin <maspin@unilink.com>
2023-01-10 09:19:32 +0000

    Add support for auth when testing endpoints in Swagger docs (#674)

--- libs/oauth-server/src/main/kotlin/uk/gov/justice/digital/hmpps/config/security/DocumentationConfig.kt
+++ libs/oauth-server/src/main/kotlin/uk/gov/justice/digital/hmpps/config/security/DocumentationConfig.kt
@@ -1,9 +1,25 @@
 package uk.gov.justice.digital.hmpps.config.security
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.security.SecurityRequirement
+import io.swagger.v3.oas.annotations.security.SecurityScheme
 import io.swagger.v3.oas.annotations.servers.Server
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpHeaders
 
-@OpenAPIDefinition(servers = [Server(url = "/")])
+@OpenAPIDefinition(
+    servers = [Server(url = "/")],
+    security = [SecurityRequirement(name = "hmpps-auth-token")],
+)
+@SecurityScheme(
+    name = "hmpps-auth-token",
+    scheme = "bearer",
+    bearerFormat = "JWT",
+    type = SecuritySchemeType.HTTP,
+    `in` = SecuritySchemeIn.HEADER,
+    paramName = HttpHeaders.AUTHORIZATION
+)
 @Configuration
 class DocumentationConfig
