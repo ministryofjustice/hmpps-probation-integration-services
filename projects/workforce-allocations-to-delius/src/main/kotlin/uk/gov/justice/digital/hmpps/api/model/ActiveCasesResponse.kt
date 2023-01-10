b308050c39bc0d5b54b8014f91ee87471f2c1f72
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2023-01-10 15:39:41 +0000

    PI-653 active cases endpoint

---
+++ projects/workforce-allocations-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/model/ActiveCasesResponse.kt
@@ -0,0 +1,15 @@
+package uk.gov.justice.digital.hmpps.api.model
+
+data class ActiveCasesResponse(
+    val code: String,
+    val name: Name,
+    val grade: String?,
+    val email: String?,
+    val cases: List<Case>,
+)
+
+data class Case(
+    val crn: String,
+    val name: Name,
+    val type: String
+)
