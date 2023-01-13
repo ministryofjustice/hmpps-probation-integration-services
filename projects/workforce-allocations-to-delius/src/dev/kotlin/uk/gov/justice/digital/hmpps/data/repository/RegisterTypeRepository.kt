1e24b0f6a3c598cda2fb32b8bfc91ec855d82237
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2023-01-13 10:17:46 +0000

    added integration test

--- projects/workforce-allocations-to-delius/src/dev/kotlin/uk/gov/justice/digital/hmpps/data/repository/RegisterTypeRepository.kt
+++ projects/workforce-allocations-to-delius/src/dev/kotlin/uk/gov/justice/digital/hmpps/data/repository/RegisterTypeRepository.kt
@@ -2,6 +2,5 @@ package uk.gov.justice.digital.hmpps.data.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
 import uk.gov.justice.digital.hmpps.integrations.delius.event.registration.RegisterType
-import uk.gov.justice.digital.hmpps.integrations.delius.event.requirement.RequirementMainCategory
 
 interface RegisterTypeRepository : JpaRepository<RegisterType, Long>
