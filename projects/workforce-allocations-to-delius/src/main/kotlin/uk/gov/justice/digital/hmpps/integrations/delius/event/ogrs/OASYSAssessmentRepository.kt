d1d032542dca903caf849d6086294b0c90243b7c
carlov20 <carlov20@users.noreply.github.com>
2023-01-25 16:13:22 +0000

    WFP-1697 (#763)
    * WFP-1697 remove event number from risk endpoint as not required
    
    * limit to one result

--- projects/workforce-allocations-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/event/ogrs/OASYSAssessmentRepository.kt
+++ projects/workforce-allocations-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/event/ogrs/OASYSAssessmentRepository.kt
@@ -3,5 +3,5 @@ package uk.gov.justice.digital.hmpps.integrations.delius.event.ogrs
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface OASYSAssessmentRepository : JpaRepository<OASYSAssessment, Long> {
-    fun findByPersonIdOrderByAssessmentDateDesc(personId: Long): OASYSAssessment?
+    fun findFirstByPersonIdOrderByAssessmentDateDesc(personId: Long): OASYSAssessment?
 }
