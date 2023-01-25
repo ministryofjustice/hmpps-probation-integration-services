a05bec2e49a7165508cec429198cd101ec95ef2c
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2023-01-25 15:37:04 +0000

    updated following review

--- projects/unpaid-work-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/controller/personaldetails/PersonalDetailsService.kt
+++ projects/unpaid-work-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/controller/personaldetails/PersonalDetailsService.kt
@@ -1,13 +1,12 @@
 package uk.gov.justice.digital.hmpps.controller.personaldetails
 
-import org.mapstruct.factory.Mappers
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.controller.personaldetails.entity.PersonRepository
 import uk.gov.justice.digital.hmpps.controller.personaldetails.entity.getPerson
 import uk.gov.justice.digital.hmpps.controller.personaldetails.model.PersonalDetails
 
 @Service
-class PersonalDetailsService(val personRepository: PersonRepository, val personMapper: PersonMapper ) {
+class PersonalDetailsService(val personRepository: PersonRepository, val personMapper: PersonMapper) {
     fun getPersonalDetails(crn: String): PersonalDetails {
         val person = personRepository.getPerson(crn)
         return personMapper.convertToModel(person)
