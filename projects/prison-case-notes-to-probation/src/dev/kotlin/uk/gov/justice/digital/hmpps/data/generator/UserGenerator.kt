83e23cf9e5add4d9c960f8a446ccb832e2dee9d2
Marcus Aspin <maspin@unilink.com>
2023-02-01 16:24:06 +0000

    Fix DB username for prison-case-notes-to-probation (#802)
    * Fix DB username for prison-case-notes-to-probation
    
    * Update UserGenerator.kt

--- projects/prison-case-notes-to-probation/src/dev/kotlin/uk/gov/justice/digital/hmpps/data/generator/UserGenerator.kt
+++ projects/prison-case-notes-to-probation/src/dev/kotlin/uk/gov/justice/digital/hmpps/data/generator/UserGenerator.kt
@@ -3,5 +3,5 @@ package uk.gov.justice.digital.hmpps.data.generator
 import uk.gov.justice.digital.hmpps.user.User
 
 object UserGenerator {
-    val APPLICATION_USER = User(IdGenerator.getAndIncrement(), "prison-case-notes-to-probation")
+    val APPLICATION_USER = User(IdGenerator.getAndIncrement(), "PrisonCaseNotesToProbation")
 }
