d6836a44e3fdd86acbc6530b6e572d52f6de7e24
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-02-01 10:27:22 +0000

    PI-765 - fix sql for updated dialect

--- projects/workforce-allocations-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/provider/StaffRepository.kt
+++ projects/workforce-allocations-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/provider/StaffRepository.kt
@@ -16,12 +16,11 @@ interface StaffRepository : JpaRepository<StaffRecord, Long> {
 
     @Query(
         """
-        select case when count(t) > 0 then true else false end
-        from Staff s 
-        left join fetch Team t
-        where s.id = :staffId
-        and t.id = :teamId
-        """
+        select case when count(team_id) > 0 then true else false end
+        from staff_team
+        where staff_id = :staffId and team_id = :teamId
+        """,
+        nativeQuery = true
     )
     fun verifyTeamMembership(staffId: Long, teamId: Long): Boolean
 
