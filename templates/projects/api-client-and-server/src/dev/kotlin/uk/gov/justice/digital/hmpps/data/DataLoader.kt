6740d069054b394d3e2715e17a13b6405516793a
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-02-02 14:39:10 +0000

    PI-770 fix imports/formatting

--- templates/projects/api-client-and-server/src/dev/kotlin/uk/gov/justice/digital/hmpps/data/DataLoader.kt
+++ templates/projects/api-client-and-server/src/dev/kotlin/uk/gov/justice/digital/hmpps/data/DataLoader.kt
@@ -3,12 +3,10 @@ package uk.gov.justice.digital.hmpps.data
 import jakarta.annotation.PostConstruct
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.ApplicationListener
-import UserGenerator
-import org.springframework.boot.CommandLineRunner
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
-import uk.gov.justice.digital.hmpps.security.ServiceContext
 import uk.gov.justice.digital.hmpps.user.UserRepository
+import UserGenerator
 
 @Component
 @Profile("dev", "integration-test")
