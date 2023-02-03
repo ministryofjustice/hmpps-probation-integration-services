a7926ec6a98fa1734866c25a5bfbd155a1a34841
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2023-02-03 08:35:16 +0000

    PI-724

--- projects/unpaid-work-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/controller/casedetails/CaseDetailsService.kt
+++ projects/unpaid-work-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/controller/casedetails/CaseDetailsService.kt
@@ -10,7 +10,7 @@ import uk.gov.justice.digital.hmpps.controller.casedetails.model.CaseDetails
 @Service
 class CaseDetailsService(val caseRepository: CaseRepository, val eventRepository: EventRepository, val caseMapper: CaseMapper) {
     fun getCaseDetails(crn: String, eventId: Long): CaseDetails {
-        val case = caseRepository.getCase(crn, eventId)
+        val case = caseRepository.getCase(crn)
         val event = eventRepository.getEvent(eventId)
         return caseMapper.withAdditionalMappings(case, event)
     }
