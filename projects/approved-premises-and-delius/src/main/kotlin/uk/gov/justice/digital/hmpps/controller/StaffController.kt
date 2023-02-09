3f1082c62559afe76c57733fe8c5061380349296
Matthew Ryall <matthew.ryall@digital.justice.gov.uk>
2023-02-09 10:59:03 +0000

    PI-784: Add documentation for the Keyworker endpoint

--- projects/approved-premises-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/controller/StaffController.kt
+++ projects/approved-premises-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/controller/StaffController.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.controller
 
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
 import org.springframework.security.access.prepost.PreAuthorize
@@ -14,6 +15,16 @@ class StaffController(
     private val staffService: StaffService
 ) {
     @PreAuthorize("hasRole('ROLE_APPROVED_PREMISES_STAFF')")
+    @Operation(
+        summary = "List all members of staff that are keyworkers in the Approved Premises",
+        description = """An Approved Premises is defined in Delius as part of reference data.
+            Probation staff members can be linked to an Approved Premises as either staff
+            members or keyworkers via the "Approved Premises and Teams" screen of the Delius
+            user interface. Keyworkers are defined via the "Approved Premises and Key Workers"
+            screen of the Delius user interface. Respond with a list of all members of staff
+            defined as keyworkers for the Approved Premises identified by the requested code.
+            """,
+    )
     @GetMapping(value = ["/approved-premises/{code}/staff"])
     fun getStaff(
         @PathVariable code: String,
