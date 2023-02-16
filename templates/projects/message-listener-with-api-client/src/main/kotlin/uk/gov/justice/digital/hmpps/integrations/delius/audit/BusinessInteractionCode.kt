8dd4eca7728000e0a013f9c988065f504b495370
Marcus Aspin <maspin@unilink.com>
2023-02-16 19:28:51 +0000

    Added example interaction code

--- templates/projects/message-listener-with-api-client/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/audit/BusinessInteractionCode.kt
+++ templates/projects/message-listener-with-api-client/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/audit/BusinessInteractionCode.kt
@@ -3,6 +3,6 @@ package uk.gov.justice.digital.hmpps.integrations.delius.audit
 import uk.gov.justice.digital.hmpps.audit.InteractionCode
 
 enum class BusinessInteractionCode(override val code: String) : InteractionCode {
-    // TODO Add any Delius interaction codes used by the service here...
-    // e.g. UPLOAD_DOCUMENT("WPBI006"),
+    // TODO Add any Delius interaction codes used by the service here
+    EXAMPLE_INTERACTION("EXAMPLEBI001")
 }
