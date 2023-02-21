da29b3cf766df93a778e9480d87e2ba7855185d7
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-02-21 15:45:00 +0000

    PI-823

--- projects/person-search-index-from-delius/build.gradle.kts
+++ projects/person-search-index-from-delius/build.gradle.kts
@@ -8,7 +8,7 @@ tasks {
     }
     named("docker") {
         doLast {
-            val dir = File("${project.parent?.projectDir}/changed")
+            val dir = File("${project.rootProject.projectDir}/changed")
             if (!dir.exists()) dir.mkdirs()
             val file = File(dir, project.name)
             if (!file.exists()) file.createNewFile()
