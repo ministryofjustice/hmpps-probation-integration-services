70fd079032c03b20726defa5b6de1270155f651d
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-03-08 15:27:42 +0000

    PI-878 allow person-search-index-from-delius docker build/push to be cached

--- projects/person-search-index-from-delius/build.gradle.kts
+++ projects/person-search-index-from-delius/build.gradle.kts
@@ -1,17 +1,61 @@
-apply(plugin = "com.palantir.docker")
+val imageName = "ghcr.io/ministryofjustice/hmpps-probation-integration-services/${project.name}"
+val buildFile = File("${buildDir}/docker", "build")
+val tagFile = File("${buildDir}/docker", "tag")
 
-tasks {
-    configure<com.palantir.gradle.docker.DockerExtension> {
-        name = "ghcr.io/ministryofjustice/hmpps-probation-integration-services/${project.name}:${project.version}"
-        tag("latest", "ghcr.io/ministryofjustice/hmpps-probation-integration-services/${project.name}:latest")
-        files("container")
+val dockerBuild = tasks.create<Exec>("dockerBuild") {
+    doFirst {
+        if (!buildFile.exists()) buildFile.createNewFile()
     }
-    named("docker") {
-        doLast {
-            val dir = File("${project.rootDir}/changed")
-            if (!dir.exists()) dir.mkdirs()
-            val file = File(dir, project.name)
-            if (!file.exists()) file.createNewFile()
+    workingDir = projectDir
+    inputs.dir("container")
+    inputs.dir("deploy")
+
+    commandLine = listOf("docker", "build", "-t", "${imageName}:latest", "container")
+
+    outputs.file(buildFile)
+    outputs.cacheIf { true }
+
+    doLast {
+        buildFile.writeBytes("${project.version}".toByteArray())
+
+        val dir = File("${project.rootDir}/changed")
+        if (!dir.exists()) dir.mkdirs()
+        val file = File(dir, project.name)
+        if (!file.exists()) file.createNewFile()
+    }
+}
+
+val dockerTag = tasks.create<Exec>("dockerTag") {
+    dependsOn(dockerBuild)
+    doFirst {
+        if (!tagFile.exists()) tagFile.createNewFile()
+        commandLine = listOf("docker", "tag", imageName, "$imageName:${project.version}")
+    }
+    workingDir = projectDir
+    commandLine = listOf("docker", "tag", imageName, "${imageName}:latest")
+    inputs.file(buildFile)
+    outputs.file(tagFile)
+    outputs.cacheIf { true }
+}
+
+val dockerPush = tasks.create("dockerPush") {
+    inputs.file(tagFile)
+    outputs.cacheIf { true }
+}
+
+listOf("latest", "${project.version}").forEach {
+    val pushFile = File("${buildDir}/docker", "push-$it")
+    val subTask = tasks.create<Exec>("dockerPush-$it") {
+        doFirst {
+            if (!pushFile.exists()) pushFile.createNewFile()
         }
+        dependsOn(dockerTag)
+        workingDir = projectDir
+        inputs.file(tagFile)
+        commandLine = listOf("docker", "push", "$imageName:latest")
+        outputs.file(pushFile)
+        outputs.cacheIf { true }
     }
+    dockerPush.dependsOn(subTask)
+    dockerPush.outputs.files + pushFile
 }
