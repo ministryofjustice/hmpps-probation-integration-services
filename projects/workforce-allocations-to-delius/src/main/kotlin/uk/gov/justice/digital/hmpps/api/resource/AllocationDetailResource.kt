fd6c366d5674088b56c1c95e17d2998500e55890
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2023-03-14 11:57:04 +0000

    PI-895 - Add allocation detail endpoint (#1030)
    * PI-895
    
    * ktlintFormat

---
+++ projects/workforce-allocations-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/resource/AllocationDetailResource.kt
@@ -0,0 +1,23 @@
+package uk.gov.justice.digital.hmpps.api.resource
+
+import jakarta.validation.Valid
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.validation.annotation.Validated
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.api.model.AllocationDetailRequests
+import uk.gov.justice.digital.hmpps.service.AllocationDemandService
+
+@Validated
+@RestController
+@RequestMapping("/allocation")
+class AllocationDetailResource(private val allocationDemand: AllocationDemandService) {
+    @PreAuthorize("hasRole('ROLE_ALLOCATION_CONTEXT')")
+    @PostMapping("/details")
+    fun getDetails(
+        @Valid @RequestBody
+        detailRequests: AllocationDetailRequests
+    ) = allocationDemand.getDetails(detailRequests)
+}
