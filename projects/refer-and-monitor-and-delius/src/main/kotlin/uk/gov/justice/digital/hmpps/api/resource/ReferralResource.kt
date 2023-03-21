03482ef0b319091bdaccd05bad1569a4357917fb
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-03-21 16:04:56 +0000

    PI-914 change role to avoid conflict with user role

--- projects/refer-and-monitor-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/resource/ReferralResource.kt
+++ projects/refer-and-monitor-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/resource/ReferralResource.kt
@@ -14,7 +14,7 @@ import uk.gov.justice.digital.hmpps.service.NsiService
 @RestController
 @RequestMapping("probation-case/{crn}/referrals")
 class ReferralResource(private val nsiService: NsiService) {
-    @PreAuthorize("hasRole('CRS_REFERRAL')")
+    @PreAuthorize("hasRole('RM_REFERRAL')")
     @PutMapping
     @ResponseStatus(HttpStatus.NO_CONTENT)
     fun referralStarted(@PathVariable crn: String, @RequestBody referralStarted: ReferralStarted) {
