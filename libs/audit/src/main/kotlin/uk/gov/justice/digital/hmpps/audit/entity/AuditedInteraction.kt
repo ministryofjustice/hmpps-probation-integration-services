a64cb7109be87abe7f3b4dcc04b6c81908ab8597
stevomcallister <78762879+stevomcallister@users.noreply.github.com>
2023-03-27 11:10:40 +0100

    PI-925 added column annotation for timestamp (#1222)

--- libs/audit/src/main/kotlin/uk/gov/justice/digital/hmpps/audit/entity/AuditedInteraction.kt
+++ libs/audit/src/main/kotlin/uk/gov/justice/digital/hmpps/audit/entity/AuditedInteraction.kt
@@ -14,6 +14,7 @@ import java.time.ZonedDateTime
 
 data class AuditedInteractionId(
 
+    @Column(columnDefinition = "timestamp")
     val dateTime: ZonedDateTime = ZonedDateTime.now(),
 
     val businessInteractionId: Long = 0,
