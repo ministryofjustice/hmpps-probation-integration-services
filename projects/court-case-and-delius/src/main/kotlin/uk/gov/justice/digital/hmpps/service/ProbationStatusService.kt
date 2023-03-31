acf6088b501670bfb1a8c1b660ea241e4b19f73f
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2023-03-31 09:04:15 +0100

    PI-942 revert to original version of probation status (#1257)

--- projects/court-case-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/ProbationStatusService.kt
+++ projects/court-case-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/ProbationStatusService.kt
@@ -1,14 +1,15 @@
 package uk.gov.justice.digital.hmpps.service
 
 import org.springframework.stereotype.Service
-import uk.gov.justice.digital.hmpps.api.model.ManagedStatus
+import uk.gov.justice.digital.hmpps.api.model.ProbationStatus
 import uk.gov.justice.digital.hmpps.api.model.ProbationStatusDetail
 import uk.gov.justice.digital.hmpps.integrations.delius.person.entity.PersonRepository
 import uk.gov.justice.digital.hmpps.integrations.delius.person.entity.SentenceCounts
 
 @Service
 class ProbationStatusService(private val personRepository: PersonRepository) {
-    fun getProbationStatus(crn: String): ProbationStatusDetail = personRepository.statusOf(crn).detail()
+    fun getProbationStatus(crn: String): ProbationStatusDetail =
+        personRepository.statusOf(crn)?.detail() ?: ProbationStatusDetail.NO_RECORD
 }
 
 fun SentenceCounts.detail() = ProbationStatusDetail(
@@ -19,10 +20,9 @@ fun SentenceCounts.detail() = ProbationStatusDetail(
     awaitingPsrCount > 0
 )
 
-val SentenceCounts.status: ManagedStatus
+val SentenceCounts.status: ProbationStatus
     get() = when {
-        unallocatedCount > 0 && allocatedCount == 0 && previousCount == 0 -> ManagedStatus.NEW_TO_PROBATION
-        allocatedCount > 0 -> ManagedStatus.CURRENTLY_MANAGED
-        previousCount > 0 -> ManagedStatus.PREVIOUSLY_MANAGED
-        else -> ManagedStatus.UNKNOWN
+        currentCount > 0 -> ProbationStatus.CURRENT
+        previousCount > 0 -> ProbationStatus.PREVIOUSLY_KNOWN
+        else -> ProbationStatus.NOT_SENTENCED
     }
