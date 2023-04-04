3ef25c9714242aab6ebddc7af68acc7ea80316f3
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-04-04 08:55:22 +0100

    PI-964 prevent duplicate nsis from being created

---
+++ projects/refer-and-monitor-and-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/NsiServiceTest.kt
@@ -0,0 +1,58 @@
+package uk.gov.justice.digital.hmpps
+
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import uk.gov.justice.digital.hmpps.api.model.ReferralStarted
+import uk.gov.justice.digital.hmpps.data.generator.PersonGenerator
+import uk.gov.justice.digital.hmpps.data.generator.SentenceGenerator
+import uk.gov.justice.digital.hmpps.datetime.EuropeLondon
+import uk.gov.justice.digital.hmpps.integrations.delius.referral.NsiRepository
+import uk.gov.justice.digital.hmpps.service.ContractTypeNsiType
+import uk.gov.justice.digital.hmpps.service.NsiService
+import java.time.LocalDate
+import java.util.UUID
+import java.util.concurrent.CompletableFuture
+
+@AutoConfigureMockMvc
+@ActiveProfiles("integration-test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class NsiServiceTest {
+
+    @Autowired
+    lateinit var nsiService: NsiService
+
+    @Autowired
+    lateinit var nsiRepository: NsiRepository
+
+    @Test
+    fun `does not save duplicate nsis`() {
+        val referralId = UUID.randomUUID()
+        val startedAt = LocalDate.now().atStartOfDay().atZone(EuropeLondon)
+        val contractType = ContractTypeNsiType.MAPPING.entries.first().key
+        val sentenceId = SentenceGenerator.SENTENCE_WITHOUT_NSI.eventId
+        val referralStarted = ReferralStarted(referralId, startedAt, contractType, sentenceId, "Some notes")
+
+        val job1 = CompletableFuture.supplyAsync {
+            nsiService.startNsi(PersonGenerator.SETENCED_WITHOUT_NSI.crn, referralStarted)
+        }
+        val job2 = CompletableFuture.supplyAsync {
+            nsiService.startNsi(PersonGenerator.SETENCED_WITHOUT_NSI.crn, referralStarted)
+        }
+
+        CompletableFuture.allOf(job1, job2).join()
+        job1.get()
+        job2.get()
+
+        val nsi = nsiRepository.findByPersonCrnAndExternalReference(
+            PersonGenerator.SETENCED_WITHOUT_NSI.crn,
+            referralStarted.urn
+        )
+        assertThat(nsi?.externalReference, equalTo(referralStarted.urn))
+        assertThat(nsi?.createdDatetime, equalTo(nsi?.lastUpdatedDatetime))
+    }
+}
--- projects/refer-and-monitor-and-delius/src/test/kotlin/uk/gov/justice/digital/hmpps/service/CreateNsiTest.kt
+++ projects/refer-and-monitor-and-delius/src/test/kotlin/uk/gov/justice/digital/hmpps/service/CreateNsiTest.kt
@@ -1,19 +1,14 @@
 package uk.gov.justice.digital.hmpps.service
 
-import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
-import org.mockito.kotlin.any
 import org.mockito.kotlin.whenever
-import org.springframework.dao.IncorrectResultSizeDataAccessException
 import uk.gov.justice.digital.hmpps.api.model.ReferralStarted
-import uk.gov.justice.digital.hmpps.data.generator.NsiGenerator
 import uk.gov.justice.digital.hmpps.data.generator.PersonGenerator
-import uk.gov.justice.digital.hmpps.data.generator.ProviderGenerator
 import uk.gov.justice.digital.hmpps.data.generator.SentenceGenerator
 import uk.gov.justice.digital.hmpps.integrations.delius.person.entity.PersonRepository
 import uk.gov.justice.digital.hmpps.integrations.delius.provider.entity.ProviderRepository
@@ -22,8 +17,6 @@ import uk.gov.justice.digital.hmpps.integrations.delius.referral.NsiStatusReposi
 import uk.gov.justice.digital.hmpps.integrations.delius.referral.NsiTypeRepository
 import uk.gov.justice.digital.hmpps.integrations.delius.referral.RequirementRepository
 import uk.gov.justice.digital.hmpps.integrations.delius.referral.entity.DisposalRepository
-import uk.gov.justice.digital.hmpps.integrations.delius.referral.entity.Nsi
-import uk.gov.justice.digital.hmpps.integrations.delius.referral.entity.NsiStatus
 import uk.gov.justice.digital.hmpps.integrations.delius.referral.entity.RequirementMainCategory
 import java.time.ZonedDateTime
 import java.util.UUID
@@ -78,41 +71,5 @@ internal class CreateNsiTest {
                 ReferralStarted(referralId, ZonedDateTime.now(), "unknown", sentence.eventId, "Notes")
             ) {}
         }
-    }
-
-    @Test
-    fun `null is returned from create when attempting to create a duplicate`() {
-        val person = PersonGenerator.SETENCED_WITHOUT_NSI
-        val sentence = SentenceGenerator.SENTENCE_WITHOUT_NSI
-        val referralId = UUID.randomUUID()
-        val contractType = ContractTypeNsiType.MAPPING.keys.first()
-        val nsiType = ContractTypeNsiType.MAPPING[contractType]!!
-        val rs = ReferralStarted(
-            referralId,
-            ZonedDateTime.now(),
-            contractType,
-            sentence.eventId,
-            "Notes"
-        )
-
-        whenever(personRepository.findByCrn(person.crn)).thenReturn(person)
-        whenever(disposalRepository.findByPersonIdAndEventId(person.id, sentence.eventId)).thenReturn(sentence)
-        whenever(
-            requirementRepository.findForPersonAndEvent(
-                person.id,
-                sentence.id,
-                RequirementMainCategory.Code.REHAB_ACTIVITY_TYPE.value
-            )
-        ).thenReturn(listOf())
-        whenever(nsiTypeRepository.findByCode(nsiType)).thenReturn(NsiGenerator.TYPES[nsiType])
-        whenever(nsiStatusRepository.findByCode(NsiStatus.Code.IN_PROGRESS.value)).thenReturn(NsiGenerator.INPROG_STATUS)
-        whenever(nsiRepository.findByPersonCrnAndExternalReference(person.crn, rs.urn))
-            .thenThrow(IncorrectResultSizeDataAccessException(1, 2))
-        whenever(providerRepository.findByCode(ProviderGenerator.INTENDED_PROVIDER.code)).thenReturn(ProviderGenerator.INTENDED_PROVIDER)
-        whenever(nsiRepository.save(any<Nsi>())).thenAnswer { it.arguments[0] }
-        whenever(nsiManagerService.createNewManager(any())).thenAnswer { NsiGenerator.generateManager(it.arguments[0] as Nsi) }
-
-        val none = createNsi.new(person.crn, rs) {}
-        assertNull(none)
     }
 }
