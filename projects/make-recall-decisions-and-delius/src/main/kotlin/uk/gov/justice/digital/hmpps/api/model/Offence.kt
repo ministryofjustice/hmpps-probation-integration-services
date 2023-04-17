5c2f73e55ad0f7dbb552722c6e601b209c9c9f75
Marcus Aspin <maspin@unilink.com>
2023-04-17 15:23:14 +0100

    PI-998 Handle null additional_offence.offence_date (#1388)

--- projects/make-recall-decisions-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/model/Offence.kt
+++ projects/make-recall-decisions-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/model/Offence.kt
@@ -3,7 +3,7 @@ package uk.gov.justice.digital.hmpps.api.model
 import java.time.LocalDate
 
 data class Offence(
-    val date: LocalDate,
+    val date: LocalDate?,
     val code: String,
     val description: String
 )
