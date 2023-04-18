2da9696f769aeaa7993bdc86d3de11f8d8ce2dc2
Marcus Aspin <maspin@unilink.com>
2023-04-18 12:40:52 +0100

    PI-577 Return 404 for user access when case not found
    Also makes username case-insensitive. Note: `upper` is used instead of `lower` because there is an index on `upper(distinguished_name)`.

--- projects/make-recall-decisions-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/controller/UserController.kt
+++ projects/make-recall-decisions-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/controller/UserController.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.api.controller
 
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.api.model.Staff
 import uk.gov.justice.digital.hmpps.integrations.delius.user.access.UserAccessRepository
+import uk.gov.justice.digital.hmpps.integrations.delius.user.access.checkUserAccess
 import uk.gov.justice.digital.hmpps.integrations.delius.user.staff.UserStaffRepository
 
 @RestController
@@ -23,7 +25,8 @@ class UserController(
         description = "<p>Returns either an `exclusionMessage` or `restrictionMessage` if the user is not allowed to access the case. " +
             "<p>This can happen if the user is *excluded* from viewing the case (e.g. a family member), or if the case has been *restricted* to a subset of users that the user is not a part of (e.g. high profile cases)."
     )
-    fun checkUserAccess(@PathVariable username: String, @PathVariable crn: String) = userAccessRepository.findByUsernameAndCrn(username, crn)
+    @ApiResponse(responseCode = "404", description = "A case with the provided CRN does not exist in Delius. Note: this could be the result of a merge or a deletion.")
+    fun checkUserAccess(@PathVariable username: String, @PathVariable crn: String) = userAccessRepository.checkUserAccess(username, crn)
 
     @GetMapping("/user/{username}/staff")
     @Operation(summary = "Get the staff code for a user, if it exists")
--- projects/make-recall-decisions-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/user/staff/UserStaffRepository.kt
+++ projects/make-recall-decisions-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/user/staff/UserStaffRepository.kt
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.Query
 import uk.gov.justice.digital.hmpps.integrations.delius.user.staff.entity.Staff
 
 interface UserStaffRepository : JpaRepository<Staff, Long> {
-    @Query("select s.code from Staff s where lower(s.user.username) = lower(:username)")
+    @Query("select s.code from Staff s where upper(s.user.username) = upper(:username)")
     fun findUserStaffCode(username: String): String?
 }
