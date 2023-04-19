7d5130fcd2f7a4d909c2a9bdb8690f3343de72bd
stevomcallister <78762879+stevomcallister@users.noreply.github.com>
2023-04-19 14:10:41 +0100

    PI-961 - API endpoint for Ctrl-O's 'Effective Proposal Framework' system (#1421)
    Co-authored-by: Marcus Aspin <maspin@unilink.com>

---
+++ projects/effective-proposal-framework-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/epf/entity/CourtAppearance.kt
@@ -0,0 +1,65 @@
+package uk.gov.justice.digital.hmpps.epf.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import org.hibernate.annotations.Immutable
+import org.hibernate.annotations.Where
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import java.time.LocalDate
+
+@Entity
+@Immutable
+class Court(
+
+    @Id
+    @Column(name = "court_id")
+    val id: Long,
+
+    @Column(name = "court_name")
+    val name: String
+
+)
+
+@Entity
+@Immutable
+@Table(name = "court_appearance")
+@Where(clause = "soft_deleted = 0")
+class CourtAppearance(
+
+    @Column(name = "appearance_date")
+    val appearanceDate: LocalDate,
+
+    @Id
+    @Column(name = "court_appearance_id")
+    val id: Long,
+
+    @JoinColumn(name = "event_id")
+    @ManyToOne
+    val event: Event,
+
+    @JoinColumn(name = "court_id")
+    @ManyToOne
+    val court: Court,
+
+    @Column(name = "soft_deleted", columnDefinition = "NUMBER", nullable = false)
+    val softDeleted: Boolean = false
+)
+
+interface CourtRepository : JpaRepository<Court, Long>
+interface CourtAppearanceRepository : JpaRepository<CourtAppearance, Long> {
+
+    @Query(
+        """
+        select ca.court.name as name from CourtAppearance ca
+        where ca.event.id = :eventId
+        order by ca.appearanceDate desc
+    """
+    )
+    fun findMostRecentCourtNameByEventId(eventId: Long, page: PageRequest = PageRequest.of(0, 1)): String
+}
---
+++ projects/effective-proposal-framework-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/epf/entity/Person.kt
@@ -0,0 +1,57 @@
+package uk.gov.justice.digital.hmpps.epf.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import org.hibernate.annotations.Immutable
+import org.hibernate.annotations.Where
+import org.springframework.data.jpa.repository.EntityGraph
+import org.springframework.data.jpa.repository.JpaRepository
+import uk.gov.justice.digital.hmpps.exception.NotFoundException
+import java.time.LocalDate
+
+@Immutable
+@Table(name = "offender")
+@Entity
+@Where(clause = "soft_deleted = 0")
+class Person(
+    @Id
+    @Column(name = "offender_id")
+    val id: Long,
+
+    @Column(columnDefinition = "char(7)")
+    val crn: String,
+
+    @ManyToOne
+    @JoinColumn(name = "gender_id")
+    val gender: ReferenceData,
+
+    @Column(name = "date_of_birth_date")
+    val dateOfBirth: LocalDate,
+
+    @Column(name = "first_name", length = 35)
+    val forename: String,
+
+    @Column(name = "second_name", length = 35)
+    val secondName: String? = null,
+
+    @Column(name = "third_name", length = 35)
+    val thirdName: String? = null,
+
+    @Column(name = "surname", length = 35)
+    val surname: String,
+
+    @Column(columnDefinition = "number")
+    val softDeleted: Boolean = false
+
+)
+
+interface PersonRepository : JpaRepository<Person, Long> {
+    @EntityGraph(attributePaths = ["gender"])
+    fun findByCrn(crn: String): Person?
+}
+
+fun PersonRepository.getPerson(crn: String) = findByCrn(crn) ?: throw NotFoundException("Person", "crn", crn)
