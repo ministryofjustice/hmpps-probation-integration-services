85e3aaa9f0ebccde764be90cc80775fd3ab86c69
Marcus Aspin <maspin@unilink.com>
2023-04-24 11:33:50 +0100

    PI-577 Remove staff code endpoint
    MRD now sends the username instead, so we don't need to provide them the staff code.

--- projects/make-recall-decisions-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/controller/UserController.kt
+++ projects/make-recall-decisions-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/controller/UserController.kt
@@ -7,18 +7,13 @@ import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
-import uk.gov.justice.digital.hmpps.api.model.Staff
 import uk.gov.justice.digital.hmpps.integrations.delius.user.access.UserAccessRepository
 import uk.gov.justice.digital.hmpps.integrations.delius.user.access.checkUserAccess
-import uk.gov.justice.digital.hmpps.integrations.delius.user.staff.StaffRepository
 
 @RestController
 @Tag(name = "Users")
 @PreAuthorize("hasRole('ROLE_MAKE_RECALL_DECISIONS_API')")
-class UserController(
-    private val userAccessRepository: UserAccessRepository,
-    private val staffRepository: StaffRepository
-) {
+class UserController(private val userAccessRepository: UserAccessRepository) {
     @GetMapping("/user/{username}/access/{crn}")
     @Operation(
         summary = "Details of any restrictions or exclusions",
@@ -27,8 +22,4 @@ class UserController(
     )
     @ApiResponse(responseCode = "404", description = "A case with the provided CRN does not exist in Delius. Note: this could be the result of a merge or a deletion.")
     fun checkUserAccess(@PathVariable username: String, @PathVariable crn: String) = userAccessRepository.checkUserAccess(username, crn)
-
-    @GetMapping("/user/{username}/staff")
-    @Operation(summary = "Get the staff code for a user, if it exists")
-    fun getStaffCode(@PathVariable username: String) = Staff(staffRepository.findUserStaffCode(username))
 }
