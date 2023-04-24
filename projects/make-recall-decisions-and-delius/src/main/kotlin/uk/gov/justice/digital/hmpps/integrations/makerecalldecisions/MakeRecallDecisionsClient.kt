2267de70e12e68089f182aaafbceebb7537ddda2
Marcus Aspin <maspin@unilink.com>
2023-04-24 15:16:24 +0100

    PI-1022 Fix Feign config for auth token

--- projects/make-recall-decisions-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/makerecalldecisions/MakeRecallDecisionsClient.kt
+++ projects/make-recall-decisions-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/makerecalldecisions/MakeRecallDecisionsClient.kt
@@ -2,10 +2,9 @@ package uk.gov.justice.digital.hmpps.integrations.makerecalldecisions
 
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
-import uk.gov.justice.digital.hmpps.config.OAuth2FeignConfig
 import java.net.URI
 
-@FeignClient(name = "make-recall-decisions", url = "https://dummy-url/to/be/overridden", configuration = [OAuth2FeignConfig::class])
+@FeignClient(name = "make-recall-decisions", url = "https://dummy-url/to/be/overridden")
 interface MakeRecallDecisionsClient {
     @GetMapping
     fun getDetails(url: URI): DecisionDetails
