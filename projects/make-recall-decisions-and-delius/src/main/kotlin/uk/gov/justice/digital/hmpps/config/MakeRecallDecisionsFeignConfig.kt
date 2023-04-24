0ebbc47b4acfe70c93968af570f288588bb8155c
Marcus Aspin <maspin@unilink.com>
2023-04-24 16:44:18 +0100

    PI-1022 Fix Feign config for auth token (#1438)

--- projects/make-recall-decisions-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/config/MakeRecallDecisionsFeignConfig.kt
+++ projects/make-recall-decisions-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/config/MakeRecallDecisionsFeignConfig.kt
@@ -8,9 +8,9 @@ import uk.gov.justice.digital.hmpps.integrations.alfresco.AlfrescoClient
 import uk.gov.justice.digital.hmpps.integrations.makerecalldecisions.MakeRecallDecisionsClient
 
 @Configuration
-@EnableFeignClients(clients = [AlfrescoClient::class, MakeRecallDecisionsClient::class])
-class FeignConfig
-
-class OAuth2FeignConfig(authorizedClientManager: OAuth2AuthorizedClientManager) : FeignConfig(authorizedClientManager) {
+@EnableFeignClients(clients = [MakeRecallDecisionsClient::class, AlfrescoClient::class])
+class MakeRecallDecisionsFeignConfig(
+    authorizedClientManager: OAuth2AuthorizedClientManager
+) : FeignConfig(authorizedClientManager) {
     override fun registrationId(): String = "make-recall-decisions-and-delius"
 }
