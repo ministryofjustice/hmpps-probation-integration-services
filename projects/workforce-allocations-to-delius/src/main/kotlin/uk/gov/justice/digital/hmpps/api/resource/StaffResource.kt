8d04977422509a3b20031718a5b00ed81f3298b0
Matthew Ryall <matthew.ryall@digital.justice.gov.uk>
2023-04-27 16:06:11 +0100

    PI-633: Complete workforce docs (#1451)
    * PI-633: Add AsyncAPI definition for workforce allocation events
    
    * PI-633: Add basic API endpoint documentation
    
    * PI-633: Update workforce allocation context map
    
    * PI-633: API endpoint documentation for workforce-allocations-to-delius

--- projects/workforce-allocations-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/resource/StaffResource.kt
+++ projects/workforce-allocations-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/resource/StaffResource.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.api.resource
 
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
@@ -16,12 +17,33 @@ import uk.gov.justice.digital.hmpps.service.StaffService
 class StaffResource(private val service: StaffService) {
 
     @PreAuthorize("hasRole('ROLE_ALLOCATION_CONTEXT')")
+    @Operation(
+        summary = """Personal and and summary caseload information for
+            the identified probation officer""",
+        description = """Summary information on the probation caseload
+            for the probation officer identified in the request. Used
+            to support the 'Officer View' capability of the HMPPS
+            Workload service which shows statistics on the staff members
+            caseload
+            """
+    )
     @GetMapping("{code}/officer-view")
     fun officerView(
         @PathVariable code: String
     ) = service.getOfficerView(code)
 
     @PreAuthorize("hasRole('ROLE_ALLOCATION_CONTEXT')")
+    @Operation(
+        summary = """Personal information along with a list of active
+            cases for the identified probation practitioner""",
+        description = """Summary information on the full case list of
+            of the probation practitioner identified in the request.
+            Used to support the 'Case Details' capability of the HMPPS
+            Workload service, which show details of the current case load
+            of the staff member for capacity planning
+            """
+
+    )
     @PostMapping("{code}/active-cases")
     fun activeCases(
         @PathVariable code: String,
