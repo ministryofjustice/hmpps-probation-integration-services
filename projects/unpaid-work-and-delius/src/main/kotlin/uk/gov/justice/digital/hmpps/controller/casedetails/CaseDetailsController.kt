ae41b53cc652b0adfd630ce74379ebbca9155d99
Matthew Ryall <matthew.ryall@digital.justice.gov.uk>
2023-05-02 15:36:40 +0100

    PI-1010: Add API docs for unpaid work service

--- projects/unpaid-work-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/controller/casedetails/CaseDetailsController.kt
+++ projects/unpaid-work-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/controller/casedetails/CaseDetailsController.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.controller.casedetails
 
+import io.swagger.v3.oas.annotations.Operation
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -7,8 +8,18 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class CaseDetailsController(val service: CaseDetailsService) {
-    @GetMapping(value = ["/case-data/{crn}/{eventId}"])
+
     @PreAuthorize("hasRole('UPW_DETAILS')")
+    @Operation(
+        summary = "Detailed information on the probation case",
+        description = """Full details of the probation case for the purposes
+            of creating a new unpaid work assessment. The service will return
+            case information for the CRN and Event Number supplied in the
+            request. Only active personal contacts and personal circumstances
+            are included
+        """
+    )
+    @GetMapping(value = ["/case-data/{crn}/{eventId}"])
     fun personDetails(
         @PathVariable crn: String,
         @PathVariable eventId: Long
