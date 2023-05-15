3e9671af9d2d2a632fc3706955747cc1453240e9
stevomcallister <78762879+stevomcallister@users.noreply.github.com>
2023-05-15 14:58:07 +0100

    PI-1073 Probation area endpoint (#1547)

---
+++ projects/pathfinder-and-delius/src/dev/kotlin/uk/gov/justice/digital/hmpps/data/generator/ProbationAreaGenerator.kt
@@ -0,0 +1,28 @@
+package uk.gov.justice.digital.hmpps.data.generator
+
+import IdGenerator
+import uk.gov.justice.digital.hmpps.entity.Borough
+import uk.gov.justice.digital.hmpps.entity.District
+import uk.gov.justice.digital.hmpps.entity.ProbationAreaEntity
+
+object ProbationAreaGenerator {
+    val DEFAULT_PA = ProbationAreaEntity(
+        true,
+        "NPS North East",
+        "N02",
+        IdGenerator.getAndIncrement()
+    )
+    val DEFAULT_BOROUGH = Borough(
+        true,
+        IdGenerator.getAndIncrement(),
+        DEFAULT_PA.id,
+        "N02"
+    )
+    val DEFAULT_LDU = District(
+        true,
+        "D01",
+        "Durham",
+        DEFAULT_BOROUGH,
+        IdGenerator.getAndIncrement()
+    )
+}
