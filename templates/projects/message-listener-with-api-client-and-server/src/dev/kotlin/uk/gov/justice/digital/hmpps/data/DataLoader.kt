14baf80a03586929945095027a7169ee07d0a4f2
Marcus Aspin <maspin@unilink.com>
2023-05-31 09:12:44 +0100

    PI-1066 Fix template imports (#1601)

--- templates/projects/message-listener-with-api-client-and-server/src/dev/kotlin/uk/gov/justice/digital/hmpps/data/DataLoader.kt
+++ templates/projects/message-listener-with-api-client-and-server/src/dev/kotlin/uk/gov/justice/digital/hmpps/data/DataLoader.kt
@@ -1,11 +1,11 @@
 package uk.gov.justice.digital.hmpps.data
 
-import UserGenerator
 import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.ApplicationListener
 import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.data.generator.UserGenerator
 import uk.gov.justice.digital.hmpps.user.UserRepository
 
 @Component
