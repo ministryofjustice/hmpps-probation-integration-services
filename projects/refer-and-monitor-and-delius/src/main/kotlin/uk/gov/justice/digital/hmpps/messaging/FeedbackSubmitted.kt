33b35ac79ba892f638af85cb763fec2da46db597
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-05-31 09:09:44 +0100

    PI-1129 update feedback for initial assessment using domain events

--- projects/refer-and-monitor-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/messaging/FeedbackSubmitted.kt
+++ projects/refer-and-monitor-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/messaging/FeedbackSubmitted.kt
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.exception.UnprocessableException
 import uk.gov.justice.digital.hmpps.integrations.randm.ReferAndMonitorClient
 import uk.gov.justice.digital.hmpps.integrations.randm.ReferralSession
+import uk.gov.justice.digital.hmpps.integrations.randm.SupplierAssessment
 import uk.gov.justice.digital.hmpps.message.HmppsDomainEvent
 import uk.gov.justice.digital.hmpps.messaging.EventProcessingResult.Failure
 import uk.gov.justice.digital.hmpps.service.AppointmentService
@@ -18,8 +19,21 @@ class FeedbackSubmitted(
     private val appointmentService: AppointmentService
 ) : DomainEventHandler {
     override val handledEvents = mapOf(
-        DomainEventType.SessionAppointmentSubmitted as DomainEventType to ::sessionAppointmentSubmitted
+        DomainEventType.InitialAppointmentSubmitted to ::initialAppointmentSubmitted,
+        DomainEventType.SessionAppointmentSubmitted to ::sessionAppointmentSubmitted
     )
+
+    fun initialAppointmentSubmitted(event: HmppsDomainEvent): EventProcessingResult = handle(event) {
+        val appointment = ramClient.getSupplierAssessment(URI(event.detailUrl!!))?.appointmentOutcome(
+            event.personReference.findCrn()!!,
+            event.referralReference(),
+            event.contractType(),
+            event.providerName(),
+            event.url(),
+            event.deliusId()
+        )
+        updateAppointment(appointment, event)
+    }
 
     fun sessionAppointmentSubmitted(event: HmppsDomainEvent): EventProcessingResult = handle(event) {
         val appointment = ramClient.getSession(URI(event.detailUrl!!))?.appointmentOutcome(
@@ -30,13 +44,17 @@ class FeedbackSubmitted(
             event.providerName(),
             event.url()
         )
+        updateAppointment(appointment, event)
+    }
+
+    private fun updateAppointment(appointment: UpdateAppointmentOutcome?, event: HmppsDomainEvent) =
         if (appointment == null) {
-            Failure(IllegalArgumentException("Unable to retrieve session: ${event.detailUrl}"))
+            Failure(IllegalArgumentException("Unable to retrieve appointment: ${event.detailUrl}"))
         } else {
             try {
                 appointmentService.updateOutcome(appointment)
                 EventProcessingResult.Success(
-                    DomainEventType.SessionAppointmentSubmitted,
+                    DomainEventType.of(event.eventType),
                     mapOf(
                         "appointmentId" to appointment.id.toString(),
                         "crn" to appointment.crn,
@@ -47,7 +65,6 @@ class FeedbackSubmitted(
                 EventProcessingResult.Rejected(e, e.properties)
             }
         }
-    }
 }
 
 private fun HmppsDomainEvent.contractType() = additionalInformation["contractTypeName"] as String
@@ -55,6 +72,7 @@ private fun HmppsDomainEvent.referralId() = additionalInformation["referralId"] 
 private fun HmppsDomainEvent.referralReference() = additionalInformation["referralReference"] as String
 private fun HmppsDomainEvent.providerName() = additionalInformation["primeProviderName"] as String
 private fun HmppsDomainEvent.url() = additionalInformation["referralProbationUserURL"] as String
+private fun HmppsDomainEvent.deliusId() = (additionalInformation["deliusAppointmentId"] as String).toLong()
 
 private fun ReferralSession.appointmentOutcome(
     crn: String,
@@ -75,3 +93,28 @@ private fun ReferralSession.appointmentOutcome(
     ),
     url
 )
+
+private fun SupplierAssessment.appointmentOutcome(
+    crn: String,
+    referralReference: String,
+    contractType: String,
+    providerName: String,
+    url: String,
+    deliusId: Long?
+): UpdateAppointmentOutcome {
+    val sessionFeedback = checkNotNull(currentAppointment?.sessionFeedback) {
+        "No Session Feedback for appointment $currentAppointmentId"
+    }
+    return UpdateAppointmentOutcome(
+        id,
+        deliusId,
+        crn,
+        referralReference,
+        Referral(referralId.toString(), Provider(providerName), contractType),
+        Outcome(
+            Attended.of(sessionFeedback.attendance.attended),
+            sessionFeedback.behaviour.notifyProbationPractitioner ?: true
+        ),
+        url
+    )
+}
