2869a2df4e2d0f3155f83ede84c3006e1f86cf39
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-06-01 11:12:26 +0100

    ktlintFormat

--- projects/manage-pom-cases-and-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/RegistrationRepositoryTest.kt
+++ projects/manage-pom-cases-and-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/RegistrationRepositoryTest.kt
@@ -14,6 +14,7 @@ import java.time.LocalDate
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 internal class RegistrationRepositoryTest {
     @Autowired lateinit var personRepository: PersonRepository
+
     @Autowired lateinit var registrationRepository: RegistrationRepository
 
     @Test
