a3228cfb9c907e2bb54b2e5e9de40d93d014cff6
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2023-06-08 14:10:15 +0100

    updated pathfinder to allow multiple crns

--- projects/pathfinder-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/controller/DetailController.kt
+++ projects/pathfinder-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/controller/DetailController.kt
@@ -2,12 +2,14 @@ package uk.gov.justice.digital.hmpps.controller
 
 import jakarta.validation.Valid
 import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.model.BatchRequest
 import uk.gov.justice.digital.hmpps.service.DetailService
 
+@Validated
 @RestController
 class DetailController(private val detailService: DetailService) {
 
--- projects/pathfinder-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/model/BatchRequest.kt
+++ projects/pathfinder-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/model/BatchRequest.kt
@@ -1,2 +1,5 @@
 package uk.gov.justice.digital.hmpps.model
-data class BatchRequest(val crns: List<String>)
+
+import jakarta.validation.constraints.Size
+
+data class BatchRequest(@field:Size(min=1, max=500) val crns: List<String>)
