ee070ab396094966ace10e8e35eeb407b569e1de
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-06-13 08:10:09 +0100

    PI-1138 revert sonar upgrade

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.google.cloud.tools.jib") version "3.3.2" apply false
-    id("org.sonarqube") version "4.2.1.3168" apply false
+    id("org.sonarqube") version "4.0.0.2929" apply false
    `kotlin-dsl`
 }
 
@@ -12,5 +12,5 @@ repositories {
 
 dependencies {
     implementation("com.google.cloud.tools:jib-gradle-plugin:3.3.2")
-    implementation("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:4.2.0.3129")
+    implementation("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:4.0.0.2929")
 }
