0ce496fc63c2c51f6fd2364e2df2df3959d0a9d6
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2023-06-21 13:22:05 +0100

    PI-1168 (#1774)
    * PI-1168
    
    * PI-1168
    
    * PI-1168 add feature flag

--- projects/prison-custody-status-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/recall/RecallService.kt
+++ projects/prison-custody-status-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/recall/RecallService.kt
@@ -45,7 +45,9 @@ import java.time.ZonedDateTime
 import java.time.temporal.ChronoUnit.DAYS
 
 enum class RecallOutcome {
+    MultipleEventsRecalled,
     PrisonerRecalled,
+    MultipleDetailsUpdated,
     CustodialDetailsUpdated,
     NoCustodialUpdates
 }
@@ -86,7 +88,7 @@ class RecallService(
 
         return eventService.getActiveCustodialEvents(nomsNumber)
             .map { addRecallToEvent(it, institution, getRecallReason, recallDateTime, "TRANSFERRED" == reason) }
-            .minBy { it.ordinal } // return the most relevant outcome
+            .combined()
     }
 
     private fun addRecallToEvent(
@@ -312,4 +314,13 @@ class RecallService(
     private fun ReferenceData.canChange() = !NO_CHANGE_STATUSES.map { it.code }.contains(code)
 
     private fun ReferenceData.isTerminated() = TERMINATED_STATUSES.map { it.code }.contains(code)
+}
+
+fun List<RecallOutcome>.combined(): RecallOutcome {
+    if (size == 1) return first()
+    return when (val outcome = minBy { it.ordinal }) {
+        RecallOutcome.PrisonerRecalled -> RecallOutcome.MultipleEventsRecalled
+        RecallOutcome.CustodialDetailsUpdated -> RecallOutcome.MultipleDetailsUpdated
+        else -> outcome
+    }
 }
