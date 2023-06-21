6c9999ac5e8c0bbd70957d15bf0ddfc06c0c6af5
Marcus Aspin <maspin@unilink.com>
2023-06-21 09:20:09 +0100

    PI-1092 Added domain-events-and-delius service (#1786)

--- libs/commons/src/main/kotlin/uk/gov/justice/digital/hmpps/telemetry/TelemetryService.kt
+++ libs/commons/src/main/kotlin/uk/gov/justice/digital/hmpps/telemetry/TelemetryService.kt
@@ -4,6 +4,7 @@ import com.microsoft.applicationinsights.TelemetryClient
 import org.slf4j.LoggerFactory
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Service
+import java.lang.Exception
 
 @Service
 class TelemetryService(private val telemetryClient: TelemetryClient = TelemetryClient()) {
@@ -13,7 +14,13 @@ class TelemetryService(private val telemetryClient: TelemetryClient = TelemetryC
 
     @Async
     fun trackEvent(name: String, properties: Map<String, String> = mapOf(), metrics: Map<String, Double> = mapOf()) {
-        log.debug("$name $properties $metrics")
+        log.debug("{} {} {}", name, properties, metrics)
         telemetryClient.trackEvent(name, properties, metrics)
+    }
+
+    @Async
+    fun trackException(exception: Exception, properties: Map<String, String> = mapOf(), metrics: Map<String, Double> = mapOf()) {
+        log.debug("{} {} {}", exception.message, properties, metrics)
+        telemetryClient.trackException(exception, properties, metrics)
     }
 }
