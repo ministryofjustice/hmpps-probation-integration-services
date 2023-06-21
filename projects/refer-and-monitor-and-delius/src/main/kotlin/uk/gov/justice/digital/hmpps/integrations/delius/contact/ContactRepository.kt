2bf678b0a16e2c1bdd8624d20a1725677f50fc46
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-06-21 15:13:50 +0100

    PI-1201 fix conflict logic

--- projects/refer-and-monitor-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/contact/ContactRepository.kt
+++ projects/refer-and-monitor-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/contact/ContactRepository.kt
@@ -85,7 +85,7 @@ interface ContactRepository : JpaRepository<Contact, Long> {
             from contact c
             join r_contact_type ct on c.contact_type_id = ct.contact_type_id
             where c.offender_id = :personId and ct.attendance_contact = 'Y'
-            and c.external_reference <> :externalReference
+            and (c.external_reference is null or c.external_reference <> :externalReference)
             and (:previousExternalReference is null or c.external_reference <> :previousExternalReference)
             and to_char(c.contact_date, 'YYYY-MM-DD') = :date
             and to_char(c.contact_start_time, 'HH24:MI') < :endTime 
