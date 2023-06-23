a6f6a3f45df1213af7fb422db921e29e2b88a606
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2023-06-23 10:35:26 +0100

    PI-1204 Add User Details Endpoint (#1824)

--- projects/refer-and-monitor-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/ldap/entity/LdapUserDetails.kt
+++ projects/refer-and-monitor-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/ldap/entity/LdapUserDetails.kt
@@ -7,13 +7,19 @@ import org.springframework.ldap.odm.annotations.Id
 import javax.naming.Name
 
 @Entry(objectClasses = ["inetOrgPerson", "top"], base = "ou=Users")
-class LdapUser(
+class LdapUserDetails(
     @Id
     val dn: Name,
 
     @Attribute(name = "cn")
     @DnAttribute(value = "cn", index = 1)
     val username: String,
+
+    @Attribute(name = "sn")
+    val surname: String,
+
+    @Attribute(name = "givenname")
+    val forename: String,
 
     @Attribute(name = "mail")
     val email: String?
--- projects/refer-and-monitor-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/UserService.kt
+++ projects/refer-and-monitor-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/UserService.kt
@@ -1,13 +1,22 @@
 package uk.gov.justice.digital.hmpps.service
 
+import org.springframework.ldap.core.LdapTemplate
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.api.model.CaseAccess
+import uk.gov.justice.digital.hmpps.api.model.Name
 import uk.gov.justice.digital.hmpps.api.model.UserAccess
+import uk.gov.justice.digital.hmpps.api.model.UserDetail
 import uk.gov.justice.digital.hmpps.integrations.delius.limitedaccess.entity.PersonAccess
 import uk.gov.justice.digital.hmpps.integrations.delius.limitedaccess.entity.UserAccessRepository
+import uk.gov.justice.digital.hmpps.integrations.ldap.entity.LdapUserDetails
+import uk.gov.justice.digital.hmpps.ldap.findByUsername
 
 @Service
-class UserAccessService(private val uar: UserAccessRepository) {
+class UserService(private val uar: UserAccessRepository, private val ldapTemplate: LdapTemplate) {
+
+    fun userDetails(username: String): UserDetail? = ldapTemplate.findByUsername<LdapUserDetails>(username)?.let {
+        UserDetail(it.username, Name(it.forename, it.surname), it.email)
+    }
 
     fun userAccessFor(username: String, crns: List<String>): UserAccess {
         val limitations: Map<String, List<PersonAccess>> = uar.getAccessFor(username, crns).groupBy { it.crn }
