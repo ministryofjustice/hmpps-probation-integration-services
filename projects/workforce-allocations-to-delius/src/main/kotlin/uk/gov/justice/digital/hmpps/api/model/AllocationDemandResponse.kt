0a0289c2a598fbbfe1e5ce40338ca8c2bca5ae03
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-06-26 09:57:09 +0100

    PI-1193 Add Court Name to allocation demand

--- projects/workforce-allocations-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/model/AllocationDemandResponse.kt
+++ projects/workforce-allocations-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/model/AllocationDemandResponse.kt
@@ -6,9 +6,11 @@ data class AllocationResponse(
     val event: Event,
     val sentence: Sentence?,
     val initialAppointment: InitialAppointment?,
+    val court: NamedCourt,
     val type: CaseType = CaseType.UNKNOWN,
     val probationStatus: ProbationStatus,
     val communityPersonManager: Manager?
 )
 
 data class AllocationDemandResponse(val cases: List<AllocationResponse>)
+data class NamedCourt(val name: String)
