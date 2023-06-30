2d0598ff6afe8cb59f9547ca7f192f2a7b90354c
Marcus Aspin <maspin@unilink.com>
2023-06-30 12:19:56 +0100

    PI-1236 Add super linter action
    This job validates, [among other things](https://github.com/github/super-linter#supported-linters), that we are not vulnerable to script injection attacks in GitHub Actions workflows.

--- .github/actions/check-changes/parse-changes.sh
+++ .github/actions/check-changes/parse-changes.sh
@@ -11,12 +11,12 @@ function topLevelChanges() {
 if [ "$COMMONS_CHANGED" == "true" ]; then
   projects=$(cd projects && echo * | xargs -n1 | toJsonArray)
   echo "Changes detected in common files, rebuild/deploy everything"
-  echo "projects=$projects" >> $GITHUB_OUTPUT
+  echo "projects=$projects" | tee -a "$GITHUB_OUTPUT"
 elif [ -n "$PROJECT_FILES" ]; then
-  projects=$(echo $PROJECT_FILES | xargs -n1 | topLevelChanges | toJsonArray)
+  projects=$(echo "$PROJECT_FILES" | xargs -n1 | topLevelChanges | toJsonArray)
   echo "Changes detected in: $projects"
-  echo "projects=$projects" >> $GITHUB_OUTPUT
+  echo "projects=$projects" | tee -a "$GITHUB_OUTPUT"
 else
   echo "No changes detected"
-  echo "projects=[]" >> $GITHUB_OUTPUT
+  echo "projects=[]" | tee -a "$GITHUB_OUTPUT"
 fi
