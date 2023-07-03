d7b9babd8281126143010ea6422ba6196f0125c3
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-07-03 13:27:58 +0100

    PI-1241 ldap pooling

--- libs/commons/src/main/kotlin/uk/gov/justice/digital/hmpps/config/LdapConfig.kt
+++ libs/commons/src/main/kotlin/uk/gov/justice/digital/hmpps/config/LdapConfig.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps.config
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
@@ -8,6 +9,7 @@ import org.springframework.ldap.core.support.LdapContextSource
 import org.springframework.ldap.pool.factory.PoolingContextSource
 
 @Configuration
+@ConditionalOnClass(name = ["org.springframework.ldap.core.support.LdapContextSource"])
 class LdapConfig {
     @Bean
     @Primary
