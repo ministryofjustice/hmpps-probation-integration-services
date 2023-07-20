5081825d71efd397794c754860d5266c609c9135
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-07-20 17:13:44 +0100

    PI-1316 Remove dependency between client and server libs

--- libs/commons/src/main/kotlin/uk/gov/justice/digital/hmpps/config/security/SecurityConfig.kt
+++ libs/commons/src/main/kotlin/uk/gov/justice/digital/hmpps/config/security/SecurityConfig.kt
@@ -8,35 +8,21 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher
+
+fun interface SecurityConfigurer {
+    fun configure(http: HttpSecurity): HttpSecurity
+}
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 @ConditionalOnDefaultWebSecurity
-class SecurityConfig {
-
-    fun filterChain(http: HttpSecurity): HttpSecurity {
-        http
-            .authorizeHttpRequests {
-                it.requestMatchers(
-                    AntPathRequestMatcher("/actuator/**"),
-                    AntPathRequestMatcher("/health/**"),
-                    AntPathRequestMatcher("/info/**"),
-                    AntPathRequestMatcher("/swagger-ui/**"),
-                    AntPathRequestMatcher("/v3/api-docs.yaml"),
-                    AntPathRequestMatcher("/v3/api-docs/**")
-                ).permitAll().anyRequest().authenticated()
-            }
-            .csrf { it.disable() }
-            .cors { it.disable() }
-            .httpBasic { it.disable() }
-            .formLogin { it.disable() }
-            .logout { it.disable() }
-        return http
-    }
+class SecurityConfig(private val configurers: List<SecurityConfigurer>) {
 
     @Bean
     @ConditionalOnMissingBean
-    fun configure(http: HttpSecurity): SecurityFilterChain = filterChain(http).build()
+    fun configure(http: HttpSecurity): SecurityFilterChain {
+        configurers.forEach { it.configure(http) }
+        return http.build()
+    }
 }
