dbf8d33fcada90d785ac020a6bb25127fe9cff27
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2023-07-20 17:21:19 +0100

    PI-1316 Remove dependency between client and server libs (#1974)

--- libs/oauth-client/src/main/kotlin/uk/gov/justice/digital/hmpps/config/security/OAuth2ClientSecurityConfig.kt
+++ libs/oauth-client/src/main/kotlin/uk/gov/justice/digital/hmpps/config/security/OAuth2ClientSecurityConfig.kt
@@ -3,26 +3,16 @@ package uk.gov.justice.digital.hmpps.config.security
 import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.annotation.Primary
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager
 import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
-import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 @ConditionalOnDefaultWebSecurity
-class OAuth2ClientSecurityConfig : SecurityConfig() {
-
-    @Bean
-    @Primary
-    override fun configure(http: HttpSecurity): SecurityFilterChain {
-        filterChain(http).oauth2Client { }
-        return http.build()
-    }
-
+class OAuth2ClientSecurityConfig {
     @Bean
     fun authorizedClientManager(
         clientRegistration: ClientRegistrationRepository
@@ -36,5 +26,13 @@ class OAuth2ClientSecurityConfig : SecurityConfig() {
             .build()
         authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider)
         return authorizedClientManager
+    }
+}
+
+@Configuration
+class OAuthClientSecurityConfigurer : SecurityConfigurer {
+    override fun configure(http: HttpSecurity): HttpSecurity {
+        http.oauth2Client {}
+        return http
     }
 }
