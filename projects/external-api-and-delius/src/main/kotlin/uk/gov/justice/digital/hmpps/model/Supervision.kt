c164b242ab6068288085d99a0b6144fd8bcf2c8e
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2023-07-27 15:54:35 +0100

    PI-1347 allow supervisions without a conviction date to be displayed (#2019)

--- projects/external-api-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/model/Supervision.kt
+++ projects/external-api-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/model/Supervision.kt
@@ -5,7 +5,7 @@ import java.time.LocalDate
 data class Supervision(
     val number: Int,
     val active: Boolean,
-    val date: LocalDate,
+    val date: LocalDate?,
     val sentence: Sentence?,
     val mainOffence: Offence?,
     val additionalOffences: List<Offence>?,
