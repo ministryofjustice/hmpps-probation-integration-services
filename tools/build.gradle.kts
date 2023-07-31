64cdae7cec4e6e0fb2075e8a8541ddc1126e9d38
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-07-31 14:51:06 +0100

    PI-1333 remove unused tools and automated build job

--- tools/build.gradle.kts
+++
@@ -1,3 +0,0 @@
-plugins {
-    id("com.palantir.docker") version "0.34.0" apply false
-}
--- tools/settings.gradle.kts
+++
@@ -1,6 +0,0 @@
-include(
-    "eclipse-temurin:17-jre-alpine",
-    "eclipse-temurin:17-jre-jammy",
-    "eclipse-temurin:19-jre-jammy",
-    "opensearch-logstash:8"
-)
