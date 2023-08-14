3a5452b47e95c714a3448cdb436b93a8b0674d9f
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2023-08-14 15:12:10 +0100

    PI-1383 Fixed bug where lower case crn has been provided

--- projects/risk-assessment-scores-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/entity/Person.kt
+++ projects/risk-assessment-scores-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/entity/Person.kt
@@ -56,4 +56,4 @@ interface PersonRepository : JpaRepository<Person, Long> {
     fun findByCrn(crn: String): Person?
 }
 fun PersonRepository.getByCrn(crn: String) =
-    findByCrn(crn) ?: throw NotFoundException("Person", "crn", crn)
+    findByCrn(crn.uppercase()) ?: throw NotFoundException("Person", "crn", crn)
