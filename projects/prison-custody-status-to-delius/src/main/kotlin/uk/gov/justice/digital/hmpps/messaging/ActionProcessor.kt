e279dc1abb9665fd46f6b99ee3382ee67f932a15
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-08-18 16:47:13 +0100

    Update to ignore custodial events with a status of terminated/auto-terminated before processing

--- projects/prison-custody-status-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/messaging/ActionProcessor.kt
+++ projects/prison-custody-status-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/messaging/ActionProcessor.kt
@@ -3,7 +3,6 @@ package uk.gov.justice.digital.hmpps.messaging
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import uk.gov.justice.digital.hmpps.exception.IgnorableMessageException
-import uk.gov.justice.digital.hmpps.integrations.delius.custody.entity.isTerminated
 import uk.gov.justice.digital.hmpps.integrations.delius.event.EventService
 
 @Service
@@ -15,9 +14,6 @@ class ActionProcessor(actionsList: List<PrisonerMovementAction>, private val eve
         try {
             eventService.getActiveCustodialEvents(prisonerMovement.nomsId)
                 .flatMap { event ->
-                    if (event.disposal?.custody?.status?.isTerminated() == true) {
-                        return listOf(ActionResult.Ignored("CustodyTerminated", prisonerMovement.telemetryProperties()))
-                    }
                     actionNames.map {
                         try {
                             actions[it]?.accept(PrisonerMovementContext(prisonerMovement, event.disposal!!.custody!!))
