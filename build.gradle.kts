9bf71727b314bc95708088850c2c459ce9a6b0ff
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2023-09-08 16:41:01 +0100

    Revert update for deprecation (#2252)

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ dependencies {
 
 val copyAgentTask = project.tasks.register<Copy>("copyAgent") {
     from(agentDeps)
-    into("${project.layout.buildDirectory}/agent")
+    into("${project.buildDir}/agent")
     rename("applicationinsights-agent(.+).jar", "agent.jar")
 }
 
