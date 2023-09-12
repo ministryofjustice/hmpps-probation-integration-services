36d30ae79c126d3c449f9f7f6e33dd8eb944e88f
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2023-09-12 16:41:31 +0100

    PI-1440 basic message processing

--- projects/opd-and-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/IntegrationTest.kt
+++ projects/opd-and-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/IntegrationTest.kt
@@ -1,18 +1,15 @@
 package uk.gov.justice.digital.hmpps
 
+import com.github.tomakehurst.wiremock.WireMockServer
 import org.junit.jupiter.api.Test
-import org.mockito.Mockito.atLeastOnce
 import org.mockito.kotlin.verify
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
-import uk.gov.justice.digital.hmpps.data.generator.MessageGenerator
-import uk.gov.justice.digital.hmpps.message.Notification
 import uk.gov.justice.digital.hmpps.messaging.HmppsChannelManager
 import uk.gov.justice.digital.hmpps.telemetry.TelemetryService
 import uk.gov.justice.digital.hmpps.telemetry.notificationReceived
-import java.util.concurrent.TimeoutException
 
 @SpringBootTest
 internal class IntegrationTest {
@@ -23,19 +20,16 @@ internal class IntegrationTest {
 
     @MockBean lateinit var telemetryService: TelemetryService
 
+    @Autowired
+    lateinit var wireMockServer: WireMockServer
+
     @Test
-    fun `message is logged to telemetry`() {
-        // Given a message
-        val notification = Notification(message = MessageGenerator.EXAMPLE)
+    fun `process opd assessment`() {
+        val message = prepMessage("opd-assessment", wireMockServer.port())
 
-        // When it is received
-        try {
-            channelManager.getChannel(queueName).publishAndWait(notification)
-        } catch (_: TimeoutException) {
-            // Note: Remove this try/catch when the MessageListener logic has been implemented
-        }
+        channelManager.getChannel(queueName).publishAndWait(message)
 
-        // Then it is logged to telemetry
-        verify(telemetryService, atLeastOnce()).notificationReceived(notification)
+        verify(telemetryService).notificationReceived(message)
+
     }
 }
--- projects/opd-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/messaging/Handler.kt
+++ projects/opd-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/messaging/Handler.kt
@@ -2,18 +2,27 @@ package uk.gov.justice.digital.hmpps.messaging
 
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.converter.NotificationConverter
+import uk.gov.justice.digital.hmpps.integrations.opd.OPDClient
+import uk.gov.justice.digital.hmpps.integrations.opd.OPDService
+import uk.gov.justice.digital.hmpps.integrations.opd.telemetryProperties
 import uk.gov.justice.digital.hmpps.message.HmppsDomainEvent
 import uk.gov.justice.digital.hmpps.message.Notification
 import uk.gov.justice.digital.hmpps.telemetry.TelemetryService
 import uk.gov.justice.digital.hmpps.telemetry.notificationReceived
+import java.net.URI
 
 @Component
 class Handler(
     override val converter: NotificationConverter<HmppsDomainEvent>,
-    private val telemetryService: TelemetryService
+    private val telemetryService: TelemetryService,
+    private val opdClient: OPDClient,
+    private val opdService: OPDService
 ) : NotificationHandler<HmppsDomainEvent> {
     override fun handle(notification: Notification<HmppsDomainEvent>) {
         telemetryService.notificationReceived(notification)
-        TODO("Not yet implemented")
+        val crn = notification.message.personReference.findCrn()!!
+        val opdAssessment = opdClient.getOPDAssessment(URI.create(notification.message.detailUrl!!))
+        opdService.processAssessment(crn, opdAssessment)
+        telemetryService.trackEvent("TierUpdateSuccess", opdAssessment.telemetryProperties(crn))
     }
 }
