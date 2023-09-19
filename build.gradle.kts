b4815874fa5b14fd0149c4e0dd4531760753e636
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-09-19 08:37:27 +0100

    PI-1457 update to 3.4.17

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ plugins {
 val agentDeps: Configuration by configurations.creating
 
 dependencies {
-    agentDeps("com.microsoft.azure:applicationinsights-agent:3.4.15")
+    agentDeps("com.microsoft.azure:applicationinsights-agent:3.4.17")
 }
 
 val copyAgentTask = project.tasks.register<Copy>("copyAgent") {
