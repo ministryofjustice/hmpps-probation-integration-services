21b22d2c37c0f766fbfcacc79562ce6a06f87305
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-11-01 16:15:12 +0000

    PI-1566 OPD Assessment

--- projects/opd-and-delius/src/dev/kotlin/uk/gov/justice/digital/hmpps/data/generator/MessageGenerator.kt
+++
@@ -1,8 +0,0 @@
-package uk.gov.justice.digital.hmpps.data.generator
-
-import uk.gov.justice.digital.hmpps.message.HmppsDomainEvent
-import uk.gov.justice.digital.hmpps.resourceloader.ResourceLoader
-
-object MessageGenerator {
-    val OPD_ASSESSMENT_NEW = ResourceLoader.message<HmppsDomainEvent>("opd-assessment")
-}
