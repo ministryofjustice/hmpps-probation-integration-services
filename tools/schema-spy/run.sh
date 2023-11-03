3d99a18ff86ca5b4791fdab5c71c4e9bc699b623
Marcus Aspin <maspin@unilink.com>
2023-11-03 08:54:25 +0000

    PI-1513 Stop using the self-hosted runner for SchemaSpy (#2461)
    * PI-1513 Stop using the self-hosted runner for SchemaSpy
    
    * Increase stack size
    
    * Keep container running
    
    * Increase stack size even further
    
    * Catch errors
    
    * Add -norows

---
+++ tools/schema-spy/run.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+pod_name=schemaspy
+
+# Delete pod on script exit
+function delete_pod() { kubectl delete pod "$pod_name"; }
+trap delete_pod ERR SIGTERM SIGINT
+
+# Start pod
+kubectl run "$pod_name" \
+  --image=schemaspy/schemaspy:6.2.4 \
+  --restart=Never --stdin=true --tty=true \
+  --overrides='{
+    "spec": {
+      "containers": [
+        {
+          "name": "schemaspy",
+          "image": "schemaspy/schemaspy:6.2.4",
+          "command": ["sh"],
+          "stdin": true,
+          "tty": true,
+          "securityContext": { "runAsNonRoot": true, "runAsUser": 1000 },
+          "resources": { "limits": { "cpu": "2000m", "memory": "8000Mi" } },
+          "env": [ { "name": "JAVA_TOOL_OPTIONS", "value": "-Xss1g -XX:MaxRAMPercentage=75" } ]
+        }
+      ]
+    }
+  }' -- sh & sleep 5
+kubectl wait --for=condition=ready pod "$pod_name"
+
+# Upload Oracle driver
+kubectl cp ./tools/schema-spy/ojdbc10.jar "$pod_name:/drivers_inc/ojdbc10.jar"
+
+# Generate report
+excludes='^(^Z.*$|^.*[0-9]$|^PRF_.*$|^PERF_.*$|^MIS_.*$|^.*_MV$|^.*\\$.*$|^.*TRAINING.*$|^PDT_THREAD$|^CHANGE_CAPTURE$)$'
+kubectl exec "$pod_name" -- /usr/local/bin/schemaspy -db "${DB}" -host "${HOST}" -port "${PORT}" -s "${SCHEMA}" -u "${USERNAME}" -p "${PASSWORD}" -cat "${SCHEMA}" -t orathin-service -I "$excludes" -vizjs -norows -noviews
+
+# Download report
+kubectl cp "$pod_name:/output" schema-spy-report
+
+# Clean up
+delete_pod
