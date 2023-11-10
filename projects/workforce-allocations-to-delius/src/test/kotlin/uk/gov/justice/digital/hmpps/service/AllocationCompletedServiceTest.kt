54b059aba6cae18df65ffd1e8b2edd2af8733762
stevomcallister <stephen.mcallister@digital.justice.gov.uk>
2023-11-10 12:28:13 +0000

    PI-1631 updated to return the order manager for a given crn/event number

--- projects/workforce-allocations-to-delius/src/test/kotlin/uk/gov/justice/digital/hmpps/service/AllocationCompletedServiceTest.kt
+++ projects/workforce-allocations-to-delius/src/test/kotlin/uk/gov/justice/digital/hmpps/service/AllocationCompletedServiceTest.kt
@@ -18,7 +18,7 @@ import uk.gov.justice.digital.hmpps.data.generator.StaffGenerator
 import uk.gov.justice.digital.hmpps.exception.NotFoundException
 import uk.gov.justice.digital.hmpps.integrations.delius.contact.ContactRepository
 import uk.gov.justice.digital.hmpps.integrations.delius.event.EventRepository
-import uk.gov.justice.digital.hmpps.integrations.delius.person.PersonManagerRepository
+import uk.gov.justice.digital.hmpps.integrations.delius.event.OrderManagerRepository
 import uk.gov.justice.digital.hmpps.integrations.delius.person.PersonRepository
 import uk.gov.justice.digital.hmpps.integrations.delius.provider.StaffRepository
 import java.time.LocalDate
@@ -35,7 +35,7 @@ class AllocationCompletedServiceTest {
 
     @Mock lateinit var contactRepository: ContactRepository
 
-    @Mock lateinit var personManagerRepository: PersonManagerRepository
+    @Mock lateinit var orderManagerRepository: OrderManagerRepository
 
     @InjectMocks lateinit var allocationCompletedService: AllocationCompletedService
 
