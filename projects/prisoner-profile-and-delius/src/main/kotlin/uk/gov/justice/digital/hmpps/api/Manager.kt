bec32838cddafa02da5f6f32ae45fcb11d7cb527
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-11-10 12:55:24 +0000

    PI-1641

--- projects/prisoner-profile-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/Manager.kt
+++ projects/prisoner-profile-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/Manager.kt
@@ -3,11 +3,13 @@ package uk.gov.justice.digital.hmpps.api
 data class Manager(
     val code: String,
     val name: Name,
-    val team: Team
+    val team: Team,
+    val email: String?,
+    val telephone: String?
 ) {
     val unallocated = code.endsWith("U")
 }
 
 data class Name(val forename: String, val surname: String)
 
-data class Team(val code: String, val description: String)
+data class Team(val code: String, val description: String, val email: String?, val telephone: String?)
--- projects/prisoner-profile-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/manager/entity/CommunityManager.kt
+++ projects/prisoner-profile-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/manager/entity/CommunityManager.kt
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
+import jakarta.persistence.OneToOne
 import jakarta.persistence.Table
 import org.hibernate.annotations.Immutable
 import org.hibernate.annotations.Where
@@ -70,6 +71,10 @@ class Team(
 
     val description: String,
 
+    @Column(name = "email_address")
+    val email: String?,
+    val telephone: String?,
+
     @Id
     @Column(name = "team_id")
     val id: Long
@@ -86,10 +91,36 @@ class Staff(
     val forename: String,
     val surname: String,
 
+    @OneToOne(mappedBy = "staff")
+    val user: StaffUser?,
+
     @Id
     @Column(name = "staff_id")
     val id: Long
 )
+
+@Entity
+@Immutable
+@Table(name = "user_")
+class StaffUser(
+
+    @Column(name = "distinguished_name")
+    val username: String,
+
+    @OneToOne
+    @JoinColumn(name = "staff_id")
+    val staff: Staff?,
+
+    @Id
+    @Column(name = "user_id")
+    val id: Long
+) {
+    @Transient
+    var email: String? = null
+
+    @Transient
+    var telephone: String? = null
+}
 
 interface CommunityManagerRepository : JpaRepository<CommunityManager, Long> {
     fun findByPersonNomsId(nomsId: String): CommunityManager?
