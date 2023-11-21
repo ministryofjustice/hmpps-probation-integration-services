548c2359f237e7ef2ebe6abee5a6b7990409bdf1
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2023-11-21 10:51:19 +0000

    PI-1633 updates (#2721)

--- projects/prison-custody-status-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/messaging/actions/UpdateStatusAction.kt
+++ projects/prison-custody-status-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/messaging/actions/UpdateStatusAction.kt
@@ -56,8 +56,7 @@ class UpdateStatusAction(
     private fun outboundStatusChange(context: PrisonerMovementContext): ActionResult {
         val (prisonerMovement, custody) = context
         val statusCode = when {
-            prisonerMovement.isAbsconded() -> CustodialStatusCode.IN_CUSTODY
-            prisonerMovement.isHospitalRelease() || prisonerMovement.isIrcRelease() -> custody.nextStatus()
+            prisonerMovement.isHospitalRelease() || prisonerMovement.isIrcRelease() || prisonerMovement.isAbsconded() -> custody.nextStatus()
             else -> if (custody.canBeReleased()) {
                 CustodialStatusCode.RELEASED_ON_LICENCE
             } else {
@@ -71,7 +70,7 @@ class UpdateStatusAction(
             when {
                 prisonerMovement.isHospitalRelease() -> "Transfer to/from Hospital"
                 prisonerMovement.isIrcRelease() -> "Transfer to Immigration Removal Centre"
-                prisonerMovement.isAbsconded() -> if (custody.canBeRecalled()) "Recall added unlawfully at large " else "Absconded unlawfully at large "
+                prisonerMovement.isAbsconded() -> "Recall added unlawfully at large "
                 else -> "Released on Licence"
             }
         )
