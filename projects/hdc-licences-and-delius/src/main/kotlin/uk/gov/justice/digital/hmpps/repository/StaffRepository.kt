73be282b3d4aad160f242be11781b92d21f8c218
Marcus Aspin <maspin@unilink.com>
2023-11-24 11:16:17 +0000

    PI-1686 Case-insensitive username lookup

--- projects/hdc-licences-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/repository/StaffRepository.kt
+++ projects/hdc-licences-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/repository/StaffRepository.kt
@@ -2,13 +2,23 @@ package uk.gov.justice.digital.hmpps.repository
 
 import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import uk.gov.justice.digital.hmpps.entity.StaffEntity
 
 interface StaffRepository : JpaRepository<StaffEntity, Long> {
     @EntityGraph(attributePaths = ["user", "teams.district.borough"])
     fun findByCode(code: String): StaffEntity?
 
-    @EntityGraph(attributePaths = ["user", "teams.district.borough"])
+    @Query(
+        """
+            select s from StaffEntity s
+            join fetch s.user u
+            left join fetch s.teams t
+            left join fetch t.district d
+            left join fetch d.borough
+            where upper(u.username) = upper(:username)
+        """
+    )
     fun findByUserUsername(username: String): StaffEntity?
 
     @EntityGraph(attributePaths = ["user", "teams.district.borough"])
