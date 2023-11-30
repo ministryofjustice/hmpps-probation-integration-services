5d31b15fb0f29709f6cff54985aa82dd74d57d4b
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-11-30 10:15:31 +0000

    revert accidental removal of spring docs from court case and delius

--- projects/court-case-and-delius/build.gradle.kts
+++ projects/court-case-and-delius/build.gradle.kts
@@ -17,6 +17,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation(libs.springdoc)
 
     dev(project(":libs:dev-tools"))
     dev("com.h2database:h2")
