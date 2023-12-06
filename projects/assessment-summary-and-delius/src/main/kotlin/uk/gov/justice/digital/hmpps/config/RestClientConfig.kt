dd36a80629ee30db1a51f2514e9c712000214b3c
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-12-06 09:08:35 +0000

    initial project setup

--- projects/assessment-summary-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/config/RestClientConfig.kt
+++ projects/assessment-summary-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/config/RestClientConfig.kt
@@ -5,17 +5,12 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestClient
 import uk.gov.justice.digital.hmpps.config.security.createClient
-import uk.gov.justice.digital.hmpps.integrations.example.ExampleClient
+import uk.gov.justice.digital.hmpps.integrations.oasys.OrdsClient
 
 @Configuration
 class RestClientConfig(private val oauth2Client: RestClient) {
 
     @Bean
-    fun exampleClient(@Value("\${integrations.example.url}") apiBaseUrl: String): ExampleClient {
-        return createClient(
-            oauth2Client.mutate()
-                .baseUrl(apiBaseUrl)
-                .build()
-        )
-    }
+    fun ordsClient(@Value("\${integrations.ords.url}") apiBaseUrl: String): OrdsClient =
+        createClient(oauth2Client.mutate().baseUrl(apiBaseUrl).build())
 }
--- projects/assessment-summary-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/oasys/OrdsClient.kt
+++ projects/assessment-summary-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/oasys/OrdsClient.kt
@@ -1,9 +1,9 @@
-package uk.gov.justice.digital.hmpps.integrations.example
+package uk.gov.justice.digital.hmpps.integrations.oasys
 
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.service.annotation.GetExchange
 
-interface ExampleClient {
+interface OrdsClient {
     @GetExchange(value = "/example/{inputId}")
     fun getExampleAPICall(@PathVariable("inputId") inputId: String): String
 }
