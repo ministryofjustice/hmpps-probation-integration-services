ae19542ea5d15659f412b3a9ea83b73226f4fc14
Marcus Aspin <maspin@unilink.com>
2023-12-07 11:29:01 +0000

    Exclude cases with no noms_number

--- projects/hdc-licences-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/repository/PersonRepository.kt
+++ projects/hdc-licences-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/repository/PersonRepository.kt
@@ -16,6 +16,7 @@ interface PersonRepository : JpaRepository<Person, Long> {
                 select offender_id from prison_offender_manager 
                 where allocation_staff_id = :staffId and active_flag = 1 and soft_deleted = 0
             )
+            and noms_number is not null
         """,
         nativeQuery = true
     )
@@ -34,6 +35,7 @@ interface PersonRepository : JpaRepository<Person, Long> {
                 join staff s on s.staff_id = pom.allocation_staff_id
                 where s.officer_code = :code and pom.active_flag = 1 and pom.soft_deleted = 0
             )
+            and noms_number is not null
         """,
         nativeQuery = true
     )
