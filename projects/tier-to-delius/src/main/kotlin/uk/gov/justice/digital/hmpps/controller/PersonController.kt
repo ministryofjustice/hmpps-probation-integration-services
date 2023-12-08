13b04c1d7007a29d71bdf025ba97315b8c52becd
Marcus Aspin <maspin@unilink.com>
2023-12-08 09:17:19 +0000

    PI-1620 Add endpoint for retrieving person details (#2893)

---
+++ projects/tier-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/controller/PersonController.kt
@@ -0,0 +1,24 @@
+package uk.gov.justice.digital.hmpps.controller
+
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.controller.model.Name
+import uk.gov.justice.digital.hmpps.controller.model.PersonDetails
+import uk.gov.justice.digital.hmpps.integrations.delius.person.PersonRepository
+
+@RestController
+class PersonController(private val personRepository: PersonRepository) {
+    @GetMapping(value = ["/person/{crn}"])
+    @PreAuthorize("hasRole('TIER_DETAILS')")
+    fun personDetails(@PathVariable crn: String) = personRepository.findByCrnAndSoftDeletedIsFalse(crn)?.let {
+        PersonDetails(
+            crn = it.crn,
+            name = Name(
+                forenames = listOfNotNull(it.forename, it.secondName, it.thirdName).joinToString(" "),
+                surname = it.surname
+            )
+        )
+    }
+}
