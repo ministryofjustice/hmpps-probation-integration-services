0aa164bfc1a53aa65ec9f2eecdf9e6f4919ccebf
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-12-18 09:31:15 +0000

    remove unused class

--- projects/assessment-summary-and-delius/src/dev/kotlin/uk/gov/justice/digital/hmpps/data/generator/MessageGenerator.kt
+++
@@ -1,8 +0,0 @@
-package uk.gov.justice.digital.hmpps.data.generator
-
-import uk.gov.justice.digital.hmpps.message.HmppsDomainEvent
-import uk.gov.justice.digital.hmpps.resourceloader.ResourceLoader
-
-object MessageGenerator {
-    val NO_REGISTRATIONS = ResourceLoader.message<HmppsDomainEvent>("assessment-summary-produced-N123456")
-}
