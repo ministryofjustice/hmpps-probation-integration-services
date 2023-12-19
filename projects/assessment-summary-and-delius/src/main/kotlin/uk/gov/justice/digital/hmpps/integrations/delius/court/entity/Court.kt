c78d51cb8ddf76ff5f2c98731408112768783c7e
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-12-19 14:43:35 +0000

    PI-1746

--- projects/assessment-summary-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/court/entity/Court.kt
+++ projects/assessment-summary-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/court/entity/Court.kt
@@ -1,9 +1,11 @@
 package uk.gov.justice.digital.hmpps.integrations.delius.court.entity
 
 import jakarta.persistence.Column
+import jakarta.persistence.Convert
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import org.hibernate.annotations.Immutable
+import org.hibernate.type.YesNoConverter
 import org.springframework.data.jpa.repository.JpaRepository
 import uk.gov.justice.digital.hmpps.exception.NotFoundException
 
@@ -12,14 +14,18 @@ import uk.gov.justice.digital.hmpps.exception.NotFoundException
 class Court(
     @Column(columnDefinition = "char(6)")
     val code: String,
+
+    @Convert(converter = YesNoConverter::class)
+    val selectable: Boolean,
+
     @Id
     @Column(name = "court_id")
     val id: Long
 )
 
 interface CourtRepository : JpaRepository<Court, Long> {
-    fun findByCode(code: String): Court?
+    fun findByCodeAndSelectableTrue(code: String): Court?
 }
 
 fun CourtRepository.getByCode(code: String) =
-    findByCode(code) ?: throw NotFoundException("Court", "code", code)
+    findByCodeAndSelectableTrue(code) ?: throw NotFoundException("Court", "code", code)
