17dfd67be0f33f6b7d3b5091bf712c02759e1739
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2023-12-19 11:26:47 +0000

    PI-1736 remove dlqs

--- projects/person-search-index-from-delius/localstack/sqs-setup.sh
+++ projects/person-search-index-from-delius/localstack/sqs-setup.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 echo creating queues...
 awslocal sqs create-queue --queue-name person-queue
-awslocal sqs create-queue --queue-name person-dlq
 awslocal sqs create-queue --queue-name contact-queue
-awslocal sqs create-queue --queue-name contact-dlq
 
 # Testing:
 # awslocal sqs send-message '{"Message": "{\"offenderId\": 1}"}'
