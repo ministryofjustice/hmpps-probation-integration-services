d0ec264eb619121307d77fab0edaa0f7ad6f21ec
Marcus Aspin <maspin@unilink.com>
2024-01-30 16:16:16 +0000

    PI-1827 Add user access endpoint for Tier UI

---
+++ projects/tier-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/controller/UserController.kt
@@ -0,0 +1,21 @@
+package uk.gov.justice.digital.hmpps.controller
+
+import org.springframework.security.access.prepost.PreAuthorize
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import uk.gov.justice.digital.hmpps.service.CaseAccess
+import uk.gov.justice.digital.hmpps.service.UserAccessService
+
+@RestController
+@RequestMapping("users")
+@PreAuthorize("hasRole('TIER_DETAILS')")
+class UserController(private val userAccessService: UserAccessService) {
+
+    @GetMapping("/{username}/access/{crn}")
+    fun userAccessCheck(
+        @PathVariable username: String,
+        @PathVariable crn: String,
+    ): CaseAccess = userAccessService.caseAccessFor(username, crn)
+}
