ef960dcb3551f44f75c303cd8d7807dc834da973
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2024-02-07 15:23:36 +0000

    set up project

--- projects/manage-supervision-and-oasys/src/dev/kotlin/uk/gov/justice/digital/hmpps/data/generator/UserGenerator.kt
+++
@@ -1,7 +0,0 @@
-package uk.gov.justice.digital.hmpps.data.generator
-
-import uk.gov.justice.digital.hmpps.user.AuditUser
-
-object UserGenerator {
-    val AUDIT_USER = AuditUser(IdGenerator.getAndIncrement(), "ManageSupervisionAndOasys")
-}
