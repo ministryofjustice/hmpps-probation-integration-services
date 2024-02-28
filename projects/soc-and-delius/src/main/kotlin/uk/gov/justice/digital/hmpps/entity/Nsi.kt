aee1f3682db5ab623cc27a4f521948d66d060b61
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2024-02-28 13:12:20 +0000

    PI-1967 (#3339)

--- projects/soc-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/entity/Nsi.kt
+++ projects/soc-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/entity/Nsi.kt
@@ -1,16 +1,13 @@
 package uk.gov.justice.digital.hmpps.entity
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.Id
-import jakarta.persistence.JoinColumn
-import jakarta.persistence.ManyToOne
-import jakarta.persistence.Table
+import jakarta.persistence.*
+import org.hibernate.annotations.Immutable
 import org.hibernate.annotations.SQLRestriction
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import java.time.LocalDate
 
+@Immutable
 @Entity
 @Table(name = "nsi")
 @SQLRestriction("soft_deleted = 0 and active_flag = 1")
@@ -19,9 +16,9 @@ class Nsi(
     @Column(name = "offender_id")
     val personId: Long,
 
-    @JoinColumn(name = "nsi_outcome_id")
     @ManyToOne
-    var outcome: ReferenceData? = null,
+    @JoinColumn(name = "nsi_type_id")
+    val type: NsiType,
 
     val referralDate: LocalDate,
 
@@ -36,38 +33,39 @@ class Nsi(
     val softDeleted: Boolean = false
 )
 
+@Immutable
+@Entity
+@Table(name = "r_nsi_type")
+class NsiType(val code: String, @Id @Column(name = "nsi_type_id") val id: Long)
+
 interface NsiRepository : JpaRepository<Nsi, Long> {
 
     @Query(
         """
            with latest_breach as (select nsi.referral_date as breachdate
                        from nsi nsi
-                                join r_standard_reference_list ref
-                                     on nsi.nsi_outcome_id = ref.standard_reference_list_id
+                                join r_nsi_type ref on nsi.nsi_type_id = ref.nsi_type_id
                        where nsi.offender_id = :personId
                          and nsi.active_flag = 1
                          and nsi.soft_deleted = 0
-                         and ref.code_value in (
-                                                'BRE01', 'BRE02', 'BRE03', 'BRE04', 'BRE05', 'BRE06', 'BRE07',
-                                                'BRE08', 'BRE10', 'BRE13', 'BRE14', 'BRE16'
-                           )
+                         and ref.code = 'BRE'
                        order by nsi.referral_date desc
                            fetch next 1 row only),
-            latest_recall as (select nsi.referral_date as recalldate
-                       from nsi nsi
-                                join r_standard_reference_list ref
-                                     on nsi.nsi_outcome_id = ref.standard_reference_list_id
-                       where nsi.offender_id = :personId
-                         and nsi.active_flag = 1
-                         and nsi.soft_deleted = 0
-                         and ref.code_value in (
-                                                'REC01', 'REC02'
-                           )
-                       order by nsi.referral_date desc
-                           fetch next 1 row only)
-            select breachdate as referralDate, 'breach' as name from latest_breach
+                 latest_recall as (select nsi.referral_date as recalldate
+                                   from nsi nsi
+                                            join r_nsi_type ref
+                                                 on nsi.nsi_type_id = ref.nsi_type_id
+                                   where nsi.offender_id = :personId
+                                     and nsi.active_flag = 1
+                                     and nsi.soft_deleted = 0
+                                     and ref.code = 'REC'
+                                   order by nsi.referral_date desc
+                                       fetch next 1 row only)
+            select breachdate as referralDate, 'breach' as name
+            from latest_breach
             union all
-            select recalldate as referralDate, 'recall' as name from latest_recall  
+            select recalldate as referralDate, 'recall' as name
+            from latest_recall  
         """,
         nativeQuery = true
     )
