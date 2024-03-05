974476cc0bd98b27b63ee4b1f660275b7c2e9c68
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2024-03-05 17:16:32 +0000

    add test case to retrieve all high prioity data

--- projects/workforce-allocations-to-delius/src/test/kotlin/uk/gov/justice/digital/hmpps/service/RegistrationServiceTest.kt
+++ projects/workforce-allocations-to-delius/src/test/kotlin/uk/gov/justice/digital/hmpps/service/RegistrationServiceTest.kt
@@ -1,5 +1,7 @@
 package uk.gov.justice.digital.hmpps.service
 
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.equalTo
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
@@ -7,6 +9,7 @@ import org.mockito.Mock
 import org.mockito.Mockito
 import org.mockito.junit.jupiter.MockitoExtension
 import org.mockito.kotlin.whenever
+import uk.gov.justice.digital.hmpps.api.model.Colour
 import uk.gov.justice.digital.hmpps.data.generator.RegistrationsRisksGenerator
 import uk.gov.justice.digital.hmpps.integrations.delius.registration.entity.RegistrationRepository
 
@@ -20,12 +23,14 @@ internal class RegistrationServiceTest {
     lateinit var registrationService: RegistrationService
 
     @Test
-    fun `get ROSH red cases`() {
+    fun `get all high priority cases`() {
         val flags = RegistrationsRisksGenerator.generate()
         whenever(registrationRepository.findAllByPersonCrn(Mockito.anyString())).thenReturn(flags)
 
-        registrationService.findActiveRegistrations("123")
+        val response = registrationService.findActiveRegistrations("123")
 
+        assertThat(response.rosh?.description , equalTo("RoSH"))
+        assertThat(response.rosh?.colour?.name, equalTo(Colour.RED.name))
     }
 
 }
