acdaf76cfc065171d0d537dc4e64ecfb536799d8
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2024-03-06 13:10:51 +0000

    refactor test

--- projects/workforce-allocations-to-delius/src/test/kotlin/uk/gov/justice/digital/hmpps/service/RegistrationServiceTest.kt
+++ projects/workforce-allocations-to-delius/src/test/kotlin/uk/gov/justice/digital/hmpps/service/RegistrationServiceTest.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.service
 
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.equalTo
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
@@ -10,6 +11,8 @@ import org.mockito.Mockito
 import org.mockito.junit.jupiter.MockitoExtension
 import org.mockito.kotlin.whenever
 import uk.gov.justice.digital.hmpps.api.model.Colour
+import uk.gov.justice.digital.hmpps.api.model.RiskItem
+import uk.gov.justice.digital.hmpps.api.model.RiskSummary
 import uk.gov.justice.digital.hmpps.data.generator.RegistrationsRisksGenerator
 import uk.gov.justice.digital.hmpps.integrations.delius.registration.entity.RegistrationRepository
 
@@ -30,20 +33,14 @@ internal class RegistrationServiceTest {
 
         val response = registrationService.findActiveRegistrations(CRN)
 
-        assertThat(response.rosh?.description , equalTo("Rosh"))
-        assertThat(response.rosh?.colour?.name, equalTo(Colour.RED.name))
+        val expected = RiskSummary(rosh = RiskItem("Rosh", Colour.RED),
+            alerts = RiskItem("Alerts", Colour.RED),
+            safeguarding = RiskItem("Safeguarding", Colour.RED),
+            information = RiskItem("Information", Colour.RED),
+            publicProtection = RiskItem("Public Protection", Colour.RED)
+        )
 
-        assertThat(response.alerts?.description, equalTo("Alerts"))
-        assertThat(response.alerts?.colour?.name, equalTo(Colour.RED.name))
-
-        assertThat(response.safeguarding?.description, equalTo("Safeguarding"))
-        assertThat(response.safeguarding?.colour?.name, equalTo(Colour.RED.name))
-
-        assertThat(response.information?.description, equalTo("Information"))
-        assertThat(response.information?.colour?.name, equalTo(Colour.RED.name))
-
-        assertThat(response.publicProtection?.description, equalTo("Public Protection"))
-        assertThat(response.publicProtection?.colour?.name, equalTo(Colour.RED.name))
+        assertEquals(expected, response)
     }
 
     @Test
@@ -53,19 +50,8 @@ internal class RegistrationServiceTest {
 
         val response = registrationService.findActiveRegistrations(CRN)
 
-        assertThat(response.rosh?.description , equalTo(null))
-        assertThat(response.rosh?.colour?.name, equalTo(null))
+        val expected = RiskSummary()
 
-        assertThat(response.alerts?.description, equalTo(null))
-        assertThat(response.alerts?.colour?.name, equalTo(null))
-
-        assertThat(response.safeguarding?.description, equalTo(null))
-        assertThat(response.safeguarding?.colour?.name, equalTo(null))
-
-        assertThat(response.information?.description, equalTo(null))
-        assertThat(response.information?.colour?.name, equalTo(null))
-
-        assertThat(response.publicProtection?.description, equalTo(null))
-        assertThat(response.publicProtection?.colour?.name, equalTo(null))
+        assertEquals(expected, response)
     }
 }
