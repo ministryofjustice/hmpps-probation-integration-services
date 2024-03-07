315b80f1a661d77e2bc0b38d6f495517b1ecc6c7
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2024-03-07 09:53:15 +0000

    PI-1978 - remove unused import

--- projects/workforce-allocations-to-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/RegistrationIntegrationTest.kt
+++ projects/workforce-allocations-to-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/RegistrationIntegrationTest.kt
@@ -1,6 +1,5 @@
 package uk.gov.justice.digital.hmpps
 
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
