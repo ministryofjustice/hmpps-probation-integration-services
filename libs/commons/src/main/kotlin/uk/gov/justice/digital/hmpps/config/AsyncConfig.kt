c34b6ae305b1dc6f2ca990b6f877fd3a6daf6bad
Marcus Aspin <maspin@unilink.com>
2024-03-26 17:20:14 +0000

    PI-2032 Publish messages from a dedicated pod to improve memory usage (#3544)
    * PI-2032 Publish messages from a dedicated pod
    
    * Exclude Helm templates from yamllint

---
+++ libs/commons/src/main/kotlin/uk/gov/justice/digital/hmpps/config/AsyncConfig.kt
@@ -0,0 +1,8 @@
+package uk.gov.justice.digital.hmpps.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableAsync
+
+@EnableAsync
+@Configuration
+class AsyncConfig
