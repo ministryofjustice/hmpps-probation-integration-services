9863dfe13d31d769495091e47a9bb2eea5a9ebe2
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2024-04-03 09:54:36 +0100

    PI-2049: Added Risk flag endpoints

---
+++ projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/model/risk/PersonRiskFlags.kt
@@ -0,0 +1,8 @@
+package uk.gov.justice.digital.hmpps.api.model.risk
+
+import uk.gov.justice.digital.hmpps.api.model.PersonSummary
+
+data class PersonRiskFlags (
+    val personSummary: PersonSummary,
+    val riskFlags: List<RiskFlag>
+)
