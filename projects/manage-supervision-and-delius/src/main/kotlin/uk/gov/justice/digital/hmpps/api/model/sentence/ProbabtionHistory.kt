816705099dda4daf0de15d5e35ef4b3109690b01
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2024-04-05 17:34:19 +0100

    PI-1939 - add probation history
    Signed-off-by: Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>

---
+++ projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/model/sentence/ProbabtionHistory.kt
@@ -0,0 +1,6 @@
+package uk.gov.justice.digital.hmpps.api.model.sentence
+
+data class ProbabtionHistory(
+    val numberOfTerminatedEvents: Number?,
+    val numberOfTerminatedEventBreaches: Number?
+)
