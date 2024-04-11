1d6799000a61125b976405986c3cef5e7709989d
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2024-04-11 11:21:01 +0100

    PI-2061

--- projects/manage-offences-and-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/IntegrationTest.kt
+++ projects/manage-offences-and-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/IntegrationTest.kt
@@ -1,11 +1,15 @@
 package uk.gov.justice.digital.hmpps
 
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.verify
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.mock.mockito.MockBean
+import uk.gov.justice.digital.hmpps.entity.OffenceRepository
 import uk.gov.justice.digital.hmpps.message.Notification
 import uk.gov.justice.digital.hmpps.messaging.HmppsChannelManager
 import uk.gov.justice.digital.hmpps.resourceloader.ResourceLoader
@@ -22,6 +26,9 @@ internal class IntegrationTest {
     @MockBean
     lateinit var telemetryService: TelemetryService
 
+    @Autowired
+    lateinit var offenceRepository: OffenceRepository
+
     @Test
     fun `update offence code`() {
         val notification = Notification(ResourceLoader.event("offence-changed"))
@@ -29,5 +36,12 @@ internal class IntegrationTest {
         channelManager.getChannel(queueName).publishAndWait(notification)
 
         verify(telemetryService).trackEvent("OffenceCodeUpdated", mapOf("offenceCode" to "AB06001"), mapOf())
+
+        val referenceOffence = offenceRepository.findOffenceByCode("09155")
+        assertNotNull(referenceOffence)
+
+        assertThat(referenceOffence?.description).isEqualTo("Obstructing a person home office description")
+        assertThat(referenceOffence?.mainCategoryCode).isEqualTo("091")
+        assertThat(referenceOffence?.subCategoryCode).isEqualTo("55")
     }
 }
