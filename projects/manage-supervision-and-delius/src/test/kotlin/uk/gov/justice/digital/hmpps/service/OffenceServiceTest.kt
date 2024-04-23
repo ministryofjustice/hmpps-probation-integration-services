6434d11159509d4a918fd530b1b69c05b3c880dc
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2024-04-23 14:10:19 +0100

    PI-2000 update service test
    Signed-off-by: Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>

--- projects/manage-supervision-and-delius/src/test/kotlin/uk/gov/justice/digital/hmpps/service/OffenceServiceTest.kt
+++ projects/manage-supervision-and-delius/src/test/kotlin/uk/gov/justice/digital/hmpps/service/OffenceServiceTest.kt
@@ -8,6 +8,7 @@ import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
 import org.mockito.kotlin.whenever
 import uk.gov.justice.digital.hmpps.api.model.Name
+import uk.gov.justice.digital.hmpps.api.model.offence.Offence
 import uk.gov.justice.digital.hmpps.api.model.offence.OffenceDetails
 import uk.gov.justice.digital.hmpps.data.generator.PersonGenerator
 import uk.gov.justice.digital.hmpps.integrations.delius.overview.entity.PersonRepository
@@ -25,7 +26,7 @@ class OffenceServiceTest {
     lateinit var service: OffenceService
 
     @Test
-    fun `no previous offences`() {
+    fun `no additional offences`() {
         whenever(personRepository.findByCrn(PersonGenerator.OVERVIEW.crn)).thenReturn(PersonGenerator.OVERVIEW)
         whenever(
             eventRepository.getEventByPersonIdAndEventNumberAndActiveIsTrue(
@@ -37,6 +38,51 @@ class OffenceServiceTest {
         )
 
         val expected = OffenceDetails(Name("Forename", "Middle1", "Surname"), null, null, listOf())
+        val response = service.getOffencesForPerson(PersonGenerator.OVERVIEW.crn, PersonGenerator.EVENT_1.eventNumber)
+
+        assertEquals(expected, response)
+    }
+
+    @Test
+    fun `return additional offences`() {
+        val event = PersonGenerator.generateEvent(
+            person = PersonGenerator.OVERVIEW,
+            active = false,
+            inBreach = true,
+            disposal = PersonGenerator.INACTIVE_ORDER_1,
+            eventNumber = "123457",
+            mainOffence = PersonGenerator.MAIN_OFFENCE_1,
+            notes = "overview",
+            additionalOffences = listOf(PersonGenerator.ADDITIONAL_OFFENCE_1)
+        )
+
+        val name = Name(
+            PersonGenerator.OVERVIEW.forename,
+            PersonGenerator.OVERVIEW.secondName,
+            PersonGenerator.OVERVIEW.surname
+        )
+        val mainOffence = Offence(
+            PersonGenerator.MAIN_OFFENCE_1.offence.description,
+            PersonGenerator.MAIN_OFFENCE_1.offence.category,
+            PersonGenerator.MAIN_OFFENCE_1.date
+        )
+        val additionalOffence = Offence(
+            PersonGenerator.ADDITIONAL_OFFENCE_1.offence.description,
+            PersonGenerator.ADDITIONAL_OFFENCE_1.offence.category,
+            PersonGenerator.ADDITIONAL_OFFENCE_1.date
+        )
+
+        whenever(personRepository.findByCrn(PersonGenerator.OVERVIEW.crn)).thenReturn(PersonGenerator.OVERVIEW)
+        whenever(
+            eventRepository.getEventByPersonIdAndEventNumberAndActiveIsTrue(
+                PersonGenerator.OVERVIEW.id,
+                PersonGenerator.EVENT_1.eventNumber
+            )
+        ).thenReturn(
+            event
+        )
+
+        val expected = OffenceDetails(name, mainOffence, event.notes, listOf(additionalOffence))
         val response = service.getOffencesForPerson(PersonGenerator.OVERVIEW.crn, PersonGenerator.EVENT_1.eventNumber)
 
         assertEquals(expected, response)
