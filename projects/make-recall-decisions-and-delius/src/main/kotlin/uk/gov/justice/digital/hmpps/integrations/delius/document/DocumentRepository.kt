750fde3f7730b6b8f0e754ea198f5753439384ac
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2024-04-24 14:59:21 +0100

    PI-2124: Added endpoint to find AP documents

--- projects/make-recall-decisions-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/document/DocumentRepository.kt
+++ projects/make-recall-decisions-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/document/DocumentRepository.kt
@@ -5,6 +5,32 @@ import org.springframework.data.jpa.repository.Query
 import uk.gov.justice.digital.hmpps.integrations.delius.document.entity.Document
 
 interface DocumentRepository : JpaRepository<Document, Long> {
-    @Query("select d.name from Document d where d.person.crn = :crn and d.alfrescoId = :alfrescoId")
+    @Query("select d.name from DocumentEntity d where d.person.crn = :crn and d.alfrescoId = :alfrescoId")
     fun findNameByPersonCrnAndAlfrescoId(crn: String, alfrescoId: String): String?
+
+    @Query(
+        """
+        select document.alfresco_document_id as "alfrescoId",
+               document.document_name as name,
+               document.document_type as type,
+               document.table_name as "tableName",
+               document.created_datetime as "createdAt",
+               case
+                when created_by.user_id is not null then created_by.forename || ' ' || created_by.surname
+                when updated_by.user_id is not null then updated_by.forename || ' ' || updated_by.surname
+               end as author,
+               'Approved premises referral on ' || to_char(approved_premises_referral.referral_date, 'dd/MM/yyyy') as description,
+               approved_premises_referral.event_id as "eventId"
+        from document
+        join offender on offender.offender_id = document.offender_id
+        join approved_premises_referral on document.table_name = 'APPROVED_PREMISES_REFERRAL' and document.primary_key_id = approved_premises_referral.approved_premises_referral_id
+        left join user_ created_by on created_by.user_id = document.created_by_user_id
+        left join user_ updated_by on updated_by.user_id = document.last_updated_user_id
+        where offender.crn = :crn
+        and document.alfresco_document_id is not null
+        and document.soft_deleted = 0
+        """,
+        nativeQuery = true
+    )
+    fun getApprovedPremisesDocuments(crn: String): List<Document>
 }
