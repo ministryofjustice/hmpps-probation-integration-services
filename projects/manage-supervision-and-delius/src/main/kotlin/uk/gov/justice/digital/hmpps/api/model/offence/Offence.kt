52e9aa667c493ec3a9330925067846de2cc4dd57
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2024-04-24 18:54:09 +0100

    PI-2000 further refactor
    Signed-off-by: Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>

--- projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/model/offence/Offence.kt
+++ projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/model/offence/Offence.kt
@@ -3,9 +3,9 @@ package uk.gov.justice.digital.hmpps.api.model.offence
 import java.time.LocalDate
 
 data class Offence(
-    val description: String?,
-    val category: String?,
-    val code: String?,
+    val description: String,
+    val category: String,
+    val code: String,
     val dateOfOffence: LocalDate?,
     val count: Long?
 )
