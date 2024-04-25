4e755f052f486a3dedcd59160bed2207472227a6
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2024-04-25 13:10:47 +0100

    PI-2124: AP doc

--- projects/make-recall-decisions-and-delius/src/dev/kotlin/uk/gov/justice/digital/hmpps/data/entity/Entities.kt
+++ projects/make-recall-decisions-and-delius/src/dev/kotlin/uk/gov/justice/digital/hmpps/data/entity/Entities.kt
@@ -4,14 +4,6 @@ import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import jakarta.persistence.Table
-import java.time.LocalDate
-
-@Entity
-class ApprovedPremisesReferral(
-    @Id val approvedPremisesReferralId: Long,
-    val referralDate: LocalDate,
-    val eventId: Long?
-)
 
 @Entity(name = "TestUser")
 @Table(name = "user_")
