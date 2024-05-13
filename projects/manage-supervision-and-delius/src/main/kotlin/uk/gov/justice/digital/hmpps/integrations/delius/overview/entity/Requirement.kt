2d7da0d91a5b949e28a12103b61fa72f39fe73c1
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2024-05-13 17:45:57 +0100

    PI-2158 refactor sql to no longer be a native query
    Signed-off-by: Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>

--- projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/overview/entity/Requirement.kt
+++ projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/overview/entity/Requirement.kt
@@ -158,19 +158,6 @@ interface RequirementRepository : JpaRepository<Requirement, Long> {
     )
     fun getRequirements(id: Long, eventNumber: String): List<RequirementDetails>
 
-    //    @Query(
-//        """
-//            SELECT 	sum(r."LENGTH")
-//            FROM rqmnt r
-//            JOIN r_rqmnt_type_main_category rrtmc
-//            ON r.rqmnt_type_main_category_id = rrtmc.rqmnt_type_main_category_id
-//            JOIN disposal d
-//            ON r.disposal_id = d.disposal_id
-//            WHERE r.DISPOSAL_ID = :id
-//            AND d.soft_deleted = 0
-//            AND rrtmc.CODE  = 'W'
-//        """, nativeQuery = true
-//    )
     @Query(
         "SELECT SUM(r.length) " +
             "FROM Requirement r " +
