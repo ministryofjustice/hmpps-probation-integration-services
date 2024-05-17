740e603cddd05daaf3f97d9b83e989fd5c9637f4
achimber-moj <161360519+achimber-moj@users.noreply.github.com>
2024-05-17 11:00:50 +0100

    Bug/pi 2204 approved premises and delius booking cancellation (#3805)
    * PI-2204 delete approved_premises_preferred record if one exists
    
    Signed-off-by: Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
    
    * PI-2204 update integration test
    
    Signed-off-by: Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
    
    * PI-2204 update integration test
    
    Signed-off-by: Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
    
    * Formatting changes
    
    * Empty-Commit
    
    ---------
    
    Signed-off-by: Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ projects/approved-premises-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/approvedpremises/referral/entity/PreferredResidence.kt
@@ -0,0 +1,24 @@
+package uk.gov.justice.digital.hmpps.integrations.delius.approvedpremises.referral.entity
+
+import jakarta.persistence.*
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import org.springframework.data.jpa.repository.JpaRepository
+
+@Entity
+@Table(name = "approved_premises_preferred")
+@EntityListeners(AuditingEntityListener::class)
+@SequenceGenerator(name = "ap_preferred_id_seq", sequenceName = "ap_preferred_id_seq", allocationSize = 1)
+class PreferredResidence(
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ap_preferred_id_seq")
+    @Column(name = "approved_premises_preferred_id")
+    val id: Long = 0,
+
+    val approvedPremisesResidenceId: Long
+)
+
+interface PreferredResidenceRepository : JpaRepository<PreferredResidence, Long> {
+    fun existsByApprovedPremisesResidenceId(approvedPremisesResidenceId: Long): Boolean
+
+    fun deleteByApprovedPremisesResidenceId(approvedPremisesResidenceId: Long)
+}
