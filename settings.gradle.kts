e63387083dbda175610491e57e67d800aa09f58c
Marcus Aspin <maspin@unilink.com>
2024-05-21 17:04:51 +0100

    Update WireMock co-ordinates and bump to 3.5.4 (#3793)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -86,7 +86,7 @@ dependencyResolutionManagement {
             )
             bundle("telemetry", listOf("insights", "opentelemetry-annotations", "sentry"))
             library("springdoc", "org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
-            library("wiremock", "com.github.tomakehurst:wiremock-jre8-standalone:3.0.1")
+            library("wiremock", "org.wiremock:wiremock-standalone:3.5.4")
             library("mapstruct", "org.mapstruct:mapstruct:1.5.5.Final")
             library("mapstructprocessor", "org.mapstruct:mapstruct-processor:1.5.5.Final")
             library("flipt", "io.flipt:flipt-java:1.1.1")
