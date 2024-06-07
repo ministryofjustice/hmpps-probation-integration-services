a4b68819463e0c594c6f693f078d2f0664541c33
Marcus Aspin <maspin@unilink.com>
2024-06-07 11:43:15 +0100

    PI-2252 Add global admin role for all API endpoints (#3879)
    Main changes are in SecurityConfig.kt, which sets up a role hierarchy so that the PROBATION_INTEGRATION_ADMIN role implies all other roles required by the service.

--- libs/commons/src/main/kotlin/uk/gov/justice/digital/hmpps/config/security/SecurityConfig.kt
+++ libs/commons/src/main/kotlin/uk/gov/justice/digital/hmpps/config/security/SecurityConfig.kt
@@ -2,8 +2,13 @@ package uk.gov.justice.digital.hmpps.config.security
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl.withDefaultRolePrefix
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
@@ -15,8 +20,9 @@ fun interface SecurityConfigurer {
 
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 @ConditionalOnDefaultWebSecurity
+@EnableConfigurationProperties(SecurityConfig.OAuth2Properties::class)
 class SecurityConfig(private val configurers: List<SecurityConfigurer>) {
 
     @Bean
@@ -24,5 +30,19 @@ class SecurityConfig(private val configurers: List<SecurityConfigurer>) {
     fun configure(http: HttpSecurity): SecurityFilterChain {
         configurers.forEach { it.configure(http) }
         return http.build()
+    }
+
+    @Bean
+    fun roleHierarchy(oauth2: OAuth2Properties): RoleHierarchy = withDefaultRolePrefix().also {
+        if (oauth2.roles.isNotEmpty()) it.role("PROBATION_INTEGRATION_ADMIN").implies(*oauth2.roles.toTypedArray())
+    }.build()
+
+    @Bean
+    fun methodSecurityExpressionHandler(roleHierarchy: RoleHierarchy) =
+        DefaultMethodSecurityExpressionHandler().also { it.setRoleHierarchy(roleHierarchy) }
+
+    @ConfigurationProperties(prefix = "oauth2")
+    class OAuth2Properties {
+        var roles: List<String> = emptyList()
     }
 }
--- libs/dev-tools/src/main/kotlin/uk/gov/justice/digital/hmpps/security/TokenHelper.kt
+++ libs/dev-tools/src/main/kotlin/uk/gov/justice/digital/hmpps/security/TokenHelper.kt
@@ -1,7 +1,5 @@
 package uk.gov.justice.digital.hmpps.security
 
-import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
@@ -11,25 +9,19 @@ import java.time.Duration
 import java.time.Instant
 
 @Configuration
-@EnableConfigurationProperties(TokenHelper.JwtProperties::class)
 class TokenHelper {
     companion object {
         const val TOKEN = "token"
     }
 
-    @ConfigurationProperties(prefix = "jwt")
-    class JwtProperties {
-        var authorities: List<String> = emptyList()
-    }
-
     @Bean
-    fun jwt(jwtProperties: JwtProperties): Jwt = Jwt
+    fun jwt(): Jwt = Jwt
         .withTokenValue(TOKEN)
         .issuedAt(Instant.now())
         .expiresAt(Instant.now().plus(Duration.ofDays(1)))
         .header("alg", "none")
         .claim("sub", "probation-integration-dev")
-        .claim("authorities", jwtProperties.authorities)
+        .claim("authorities", listOf("ROLE_PROBATION_INTEGRATION_ADMIN"))
         .build()
 
     @Bean
