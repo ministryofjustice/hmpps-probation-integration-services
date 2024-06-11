38d8db4ed3cee8a70e6087dbff8035ac0a8a493d
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2024-06-11 12:16:35 +0100

    PI-2258 refactor test to assert logs
    Signed-off-by: Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>

--- projects/prison-custody-status-to-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/PcstdIntegrationTransactionRollbackTest.kt
+++ projects/prison-custody-status-to-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/PcstdIntegrationTransactionRollbackTest.kt
@@ -1,9 +1,13 @@
 package uk.gov.justice.digital.hmpps
 
 import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.containsString
 import org.hamcrest.Matchers.equalTo
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.system.CapturedOutput
+import org.springframework.boot.test.system.OutputCaptureExtension
 import org.springframework.test.context.TestPropertySource
 import uk.gov.justice.digital.hmpps.data.generator.BookingGenerator
 import uk.gov.justice.digital.hmpps.data.generator.InstitutionGenerator
@@ -11,11 +15,12 @@ import uk.gov.justice.digital.hmpps.data.generator.NotificationGenerator
 import uk.gov.justice.digital.hmpps.data.generator.PersonGenerator
 
 @TestPropertySource(properties = ["logging.level.org.springframework.transaction=DEBUG", "logging.level.org.hibernate.engine.transaction.internal.TransactionImpl=DEBUG"])
+@ExtendWith(OutputCaptureExtension::class)
 class PcstdIntegrationTransactionRollbackTest : PcstdIntegrationTestBase() {
     private val releaseOnLicence = "Released on Licence"
 
     @Test
-    fun `when a prisoner is matched with more than one pom`() {
+    fun `when a prisoner is matched with more than one pom`(output: CapturedOutput) {
 
         val notification = NotificationGenerator.PRISONER_MATCHED_WITH_POM
         val person = PersonGenerator.MATCHABLE_WITH_POM
@@ -35,5 +40,8 @@ class PcstdIntegrationTransactionRollbackTest : PcstdIntegrationTestBase() {
         verifyCustodyHistory(
             custody
         )
+
+        assertThat(output.toString(), containsString("rolling back"))
+        assertThat(output.toString(), containsString("Query did not return a unique result: 2 results were returned"))
     }
 }
