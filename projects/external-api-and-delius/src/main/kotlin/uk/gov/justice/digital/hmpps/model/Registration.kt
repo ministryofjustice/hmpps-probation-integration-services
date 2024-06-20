41e359d4d5484e6ec29e2ddd4ab8c0230a31cd90
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2024-06-20 11:12:39 +0100

    PI-2288: Added Schema example to registration code

--- projects/external-api-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/model/Registration.kt
+++ projects/external-api-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/model/Registration.kt
@@ -1,8 +1,10 @@
 package uk.gov.justice.digital.hmpps.model
 
+import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDate
 
 data class Registration(
+    @Schema(example = "RCCO")
     val code: String,
     val description: String,
     val startDate: LocalDate,
