2713a02bee39fa39cc340bd808b6556460b85f3a
pmcphee77 <150798161+pmcphee77@users.noreply.github.com>
2024-06-20 11:52:19 +0100

    PI-2288: Added Schema example to registration code (#3928)
    * PI-2288: Added Schema example to registration code
    
    * PI-2288: Added Schema example to registration code

--- projects/external-api-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/model/Registration.kt
+++ projects/external-api-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/model/Registration.kt
@@ -1,9 +1,12 @@
 package uk.gov.justice.digital.hmpps.model
 
+import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDate
 
 data class Registration(
+    @Schema(example = "RCCO")
     val code: String,
+    @Schema(example = "Child Concerns")
     val description: String,
     val startDate: LocalDate,
     val reviewDate: LocalDate?,
