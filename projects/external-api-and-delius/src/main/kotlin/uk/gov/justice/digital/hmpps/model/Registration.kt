cfac119d9128a6154428727a657c7a582fa3d0ee
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2024-06-27 17:32:40 +0100

    PI-2300: Changed Registration types to cater for different examples

--- projects/external-api-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/model/Registration.kt
+++ projects/external-api-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/model/Registration.kt
@@ -3,10 +3,20 @@ package uk.gov.justice.digital.hmpps.model
 import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDate
 
-data class Registration(
+data class DynamicRiskRegistration(
     @Schema(example = "RCCO")
     val code: String,
     @Schema(example = "Child Concerns")
+    val description: String,
+    val startDate: LocalDate,
+    val reviewDate: LocalDate?,
+    val notes: String?
+)
+
+data class PersonStatusRegistration(
+    @Schema(example = "ASFO")
+    val code: String,
+    @Schema(example = "Serious Further Offence - Subject to SFO review/investigation")
     val description: String,
     val startDate: LocalDate,
     val reviewDate: LocalDate?,
