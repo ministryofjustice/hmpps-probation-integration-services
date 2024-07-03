826f5ac8988259b53b96bcba6ac86419c09b992b
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2024-07-03 16:05:23 +0100

    PI-2102 - revert incorrect updates

--- projects/soc-and-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/DetailsIntegrationTest.kt
+++ projects/soc-and-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/DetailsIntegrationTest.kt
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
-import org.springframework.test.context.TestPropertySource
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
