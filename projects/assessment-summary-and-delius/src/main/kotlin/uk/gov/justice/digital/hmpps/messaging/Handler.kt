da9c28d006a49fbf249d09566e93a6f7032e4813
pmcphee77 <150798161+pmcphee77@users.noreply.github.com>
2024-07-04 13:05:18 +0100

    PI-2335: Log notification received to appinsights (#3986)
    PI-2335: Log notifcation received to appinsights

--- projects/assessment-summary-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/messaging/Handler.kt
+++ projects/assessment-summary-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/messaging/Handler.kt
@@ -13,6 +13,7 @@ import uk.gov.justice.digital.hmpps.message.HmppsDomainEvent
 import uk.gov.justice.digital.hmpps.message.Notification
 import uk.gov.justice.digital.hmpps.service.AssessmentSubmitted
 import uk.gov.justice.digital.hmpps.telemetry.TelemetryService
+import uk.gov.justice.digital.hmpps.telemetry.notificationReceived
 import java.net.URI
 
 const val AssessmentSummaryProduced = "assessment.summary.produced"
@@ -30,6 +31,7 @@ class Handler(
     override fun handle(notification: Notification<HmppsDomainEvent>) {
         try {
             if (notification.message.eventType == AssessmentSummaryProduced && featureFlags.enabled("assessment-summary-produced")) {
+                telemetryService.notificationReceived(notification)
                 notification.message.detailUrl
                     ?.let { ordsClient.getAssessmentSummary(URI.create(it)) }
                     ?.let { assessmentSubmitted.assessmentSubmitted(it.crn, it.assessments.first()) }
