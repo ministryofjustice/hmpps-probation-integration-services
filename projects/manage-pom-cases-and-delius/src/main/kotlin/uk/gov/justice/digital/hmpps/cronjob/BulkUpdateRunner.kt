0f5384ff2b6f6c6ce1784a6b56b804ae9a897846
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2024-07-09 17:51:36 +0100

    PI-2249 - add bulk update job for pom cases

---
+++ projects/manage-pom-cases-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/cronjob/BulkUpdateRunner.kt
@@ -0,0 +1,25 @@
+package uk.gov.justice.digital.hmpps.cronjob
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.SpringApplication.exit
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.context.event.ApplicationStartedEvent
+import org.springframework.context.ApplicationContext
+import org.springframework.context.ApplicationListener
+import org.springframework.stereotype.Component
+import uk.gov.justice.digital.hmpps.messaging.Notifier
+import kotlin.system.exitProcess
+
+@Component
+@ConditionalOnProperty("bulk.update.enabled")
+class BulkUpdateRunner(
+    @Value("\${bulk.update.dry-run:true}") private val dryRun: Boolean,
+    private val applicationContext: ApplicationContext,
+    private val notifier: Notifier,
+) : ApplicationListener<ApplicationStartedEvent> {
+
+    override fun onApplicationEvent(applicationStartedEvent: ApplicationStartedEvent) {
+        notifier.requestBulkUpdate(dryRun)
+        exitProcess(exit(applicationContext, { 0 }))
+    }
+}
