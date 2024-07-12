df624bb603b84b322d8c9f1ae984fe5e62f5fcfa
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2024-07-12 16:47:45 +0100

    PI-2344 - add api

---
+++ projects/subject-access-requests-and-delius/src/dev/kotlin/uk/gov/justice/digital/hmpps/data/generator/PersonGenerator.kt
@@ -0,0 +1,15 @@
+package uk.gov.justice.digital.hmpps.data.generator
+
+import uk.gov.justice.digital.hmpps.integration.delius.person.entity.Person
+
+object PersonGenerator {
+
+    val DEFAULT: Person = generate("A123456", "Jon", surname = "Smith")
+    fun generate(
+        crn: String,
+        forename: String,
+        secondName: String? = null,
+        thirdName: String? = null,
+        surname: String,
+    ) = Person(IdGenerator.getAndIncrement(), crn, forename, secondName, thirdName, surname)
+}
---
+++ projects/subject-access-requests-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/model/Person.kt
@@ -0,0 +1,5 @@
+package uk.gov.justice.digital.hmpps.api.model
+
+data class Person (
+    val fullName: String
+)
