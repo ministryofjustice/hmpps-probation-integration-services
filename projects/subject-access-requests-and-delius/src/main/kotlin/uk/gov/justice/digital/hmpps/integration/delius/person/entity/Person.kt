7050535c4e62bccf07bb133f9effa232e658a646
achimber-moj <161360519+achimber-moj@users.noreply.github.com>
2024-07-15 17:27:17 +0100

    PI-2344 - add api (#4031)
    * PI-2344 - add api
    
    * PI-2344 - update test
    
    * Formatting changes
    
    * Empty-Commit
    
    * PI-2344 - correct role details
    
    * PI-2344 - small refactor
    
    * PI-2344 - apply review comments
    
    * Formatting changes
    
    * Empty-Commit
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

---
+++ projects/subject-access-requests-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integration/delius/person/entity/Person.kt
@@ -0,0 +1,44 @@
+package uk.gov.justice.digital.hmpps.integration.delius.person.entity
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import org.hibernate.annotations.Immutable
+import org.hibernate.annotations.SQLRestriction
+import org.springframework.data.jpa.repository.JpaRepository
+import uk.gov.justice.digital.hmpps.exception.NotFoundException
+
+@Immutable
+@Entity
+@Table(name = "offender")
+@SQLRestriction("soft_deleted = 0")
+class Person(
+    @Id
+    @Column(name = "offender_id")
+    val id: Long,
+
+    @Column(columnDefinition = "char(7)")
+    val crn: String,
+
+    @Column(name = "first_name", length = 35)
+    val forename: String,
+
+    @Column(name = "second_name", length = 35)
+    val secondName: String? = null,
+
+    @Column(name = "third_name", length = 35)
+    val thirdName: String? = null,
+
+    @Column(name = "surname", length = 35)
+    val surname: String,
+
+    @Column(columnDefinition = "number")
+    val softDeleted: Boolean = false,
+)
+
+interface PersonRepository : JpaRepository<Person, Long> {
+    fun findByCrn(crn: String): Person?
+}
+
+fun PersonRepository.getPerson(crn: String) = findByCrn(crn) ?: throw NotFoundException("Person", "crn", crn)
---
+++ projects/subject-access-requests-and-delius/src/test/kotlin/uk/gov/justice/digital/hmpps/service/SubjectAccessRequestsServiceTest.kt
@@ -0,0 +1,50 @@
+package uk.gov.justice.digital.hmpps.service
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.whenever
+import uk.gov.justice.digital.hmpps.data.generator.PersonGenerator
+import uk.gov.justice.digital.hmpps.integration.delius.person.entity.Person
+import uk.gov.justice.digital.hmpps.integration.delius.person.entity.PersonRepository
+
+@ExtendWith(MockitoExtension::class)
+class SubjectAccessRequestsServiceTest {
+
+    @Mock
+    lateinit var personRepository: PersonRepository
+
+    @InjectMocks
+    lateinit var subjectAccessRequestsService: SubjectAccessRequestsService
+
+    val person1 = PersonGenerator.PERSON1
+
+    @ParameterizedTest
+    @CsvSource(
+        "A123456,Jon Harry Fred Smith, 1",
+        "A123456,Jon Harry Smith, 2",
+        "A123456,Jon Fred Smith, 3",
+        "A123456,Jon Smith, 4",
+    )
+    fun `test returned name`(crn: String, fullName: String, person: Int) {
+
+        whenever(personRepository.findByCrn(crn)).thenReturn(getPerson(person))
+
+        val response = subjectAccessRequestsService.getPersonDetailsByCrn(crn)
+
+        assertEquals(fullName, response.fullName)
+    }
+
+    private fun getPerson(person: Int): Person {
+        return when (person) {
+            1 -> PersonGenerator.PERSON1
+            2 -> PersonGenerator.PERSON2
+            3 -> PersonGenerator.PERSON3
+            else -> PersonGenerator.PERSON4
+        }
+    }
+}
