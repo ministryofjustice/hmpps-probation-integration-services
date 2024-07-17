6ca064dbe2c17c87e60edb567a42eb84c474b15b
pmcphee77 <150798161+pmcphee77@users.noreply.github.com>
2024-07-17 16:41:54 +0100

    PI-2365: Added feature flag for detail and summary (#4043)
    * PI-2365: Added feature flag for detail and summary
    
    * Formatting changes
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- projects/court-case-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/proxy/CommunityApiController.kt
+++ projects/court-case-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/proxy/CommunityApiController.kt
@@ -7,18 +7,42 @@ import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.HttpStatusCodeException
+import uk.gov.justice.digital.hmpps.api.resource.ProbationRecordResource
+import uk.gov.justice.digital.hmpps.flags.FeatureFlags
 import java.net.URI
 
 @RestController
-@RequestMapping("secure/**")
+@RequestMapping("secure")
 class CommunityApiController(
     @Value("\${community-api.url}") private val communityApiUrl: String,
+    private val probationRecordResource: ProbationRecordResource,
+    private val featureFlags: FeatureFlags,
     private val communityApiClient: CommunityApiClient
 ) {
-    @GetMapping
+
+    @GetMapping("/offenders/crn/{crn}/all")
+    fun offenderDetail(request: HttpServletRequest, response: HttpServletResponse, @PathVariable crn: String): Any {
+
+        if (featureFlags.enabled("ccd-offender-detail-enabled")) {
+            return probationRecordResource.getOffenderDetail(crn)
+        }
+        return proxy(request, response)
+    }
+
+    @GetMapping("/offenders/crn/{crn}")
+    fun offenderSummary(request: HttpServletRequest, response: HttpServletResponse, @PathVariable crn: String): Any {
+
+        if (featureFlags.enabled("ccd-offender-summary-enabled")) {
+            return probationRecordResource.getOffenderDetailSummary(crn)
+        }
+        return proxy(request, response)
+    }
+
+    @GetMapping("/**")
     fun proxy(request: HttpServletRequest, response: HttpServletResponse): ResponseEntity<String> {
 
         val headers = request.headerNames.asSequence().associateWith { request.getHeader(it) }.toMutableMap()
