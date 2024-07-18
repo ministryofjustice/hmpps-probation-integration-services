36fd8f987704b2a9182a2cbf1e5ce1300d3b922a
pmcphee77 <150798161+pmcphee77@users.noreply.github.com>
2024-07-18 13:32:14 +0100

    PI-2365: Bypass get token for proxy requests (#4045)
    * PI-2365: Bypass get token for proxy requests

--- projects/court-case-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/config/RestClientConfig.kt
+++ projects/court-case-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/config/RestClientConfig.kt
@@ -8,11 +8,14 @@ import uk.gov.justice.digital.hmpps.config.security.createClient
 import uk.gov.justice.digital.hmpps.integrations.courtcase.CourtCaseClient
 
 @Configuration
-class RestClientConfig(private val oauth2Client: RestClient) {
+class RestClientConfig(private val oauth2Client: RestClient, private val restClientBuilder: RestClient.Builder) {
 
     @Bean
     fun courtCaseClient() = createClient<CourtCaseClient>(oauth2Client)
 
     @Bean
-    fun communityApiClient() = createClient<CommunityApiClient>(oauth2Client)
+    fun proxyClient() = restClientBuilder.build()
+
+    @Bean
+    fun communityApiClient() = createClient<CommunityApiClient>(proxyClient())
 }
