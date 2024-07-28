c740ad2048dc826b518dbd0408a406dad86f189d
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2024-07-28 19:07:23 +0100

    PI-2391: Fixed case sensitive issue with wiremocks

--- projects/court-case-and-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/ProxyIntegrationTest.kt
+++ projects/court-case-and-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/ProxyIntegrationTest.kt
@@ -157,6 +157,30 @@ internal class ProxyIntegrationTest {
     }
 
     @Test
+    fun `compare new endpoints with community api endpoints with an array of string as a parameter`() {
+
+        val res = mockMvc.perform(
+            post("/secure/compare")
+                .contentType("application/json;charset=utf-8")
+                .content(
+                    """
+                    {
+                        "params": {
+                            "crn": "C123456",
+                            "convictionId": ${SentenceGenerator.CURRENTLY_MANAGED.id},
+                            "nsiCodes": "${NSI_TYPE.code},${NSI_TYPE.code}"
+                        },
+                        "uri": "CONVICTION_BY_ID_NSIS"
+                    }
+                """
+                )
+                .withToken()
+        ).andExpect(status().is2xxSuccessful).andReturn().response.contentAsJson<CompareReport>()
+
+        assertThat(res.endPointName, equalTo("CONVICTION_BY_ID_NSIS"))
+    }
+
+    @Test
     fun `compare new endpoints with community api endpoints with json arrays`() {
 
         val res = mockMvc.perform(
@@ -205,6 +229,10 @@ internal class ProxyIntegrationTest {
                                 "activeOnly": true,
                                 "excludeSoftDeleted": true
                             },
+                            "CONVICTION_BY_ID_NSIS": {
+                                "convictionId": "?",
+                                "nsiCodes": "?"
+                            },
                             "CONVICTION_BY_ID_PSS": {
                                 "convictionId": "?"
                             }
@@ -215,7 +243,7 @@ internal class ProxyIntegrationTest {
                 .withToken()
         ).andExpect(status().is2xxSuccessful).andReturn().response.contentAsJson<CompareAllReport>()
 
-        assertThat(res.totalNumberOfRequests, equalTo(7))
+        assertThat(res.totalNumberOfRequests, equalTo(8))
         assertThat(res.totalNumberOfCrns, equalTo(1))
         assertThat(res.currentPageNumber, equalTo(1))
     }
