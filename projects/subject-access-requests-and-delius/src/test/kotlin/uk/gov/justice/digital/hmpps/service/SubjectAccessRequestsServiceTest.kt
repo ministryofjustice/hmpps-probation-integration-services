941214504972eb9e4f1df98b8c0ef7d99e399bc0
achimber-moj <161360519+achimber-moj@users.noreply.github.com>
2024-07-29 15:35:45 +0100

    PI-2395 split name into separate fields (#4077)
    * PI-2395 split name into separate fields
    
    * Formatting changes
    
    * Empty-Commit
    
    ---------
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- projects/subject-access-requests-and-delius/src/test/kotlin/uk/gov/justice/digital/hmpps/service/SubjectAccessRequestsServiceTest.kt
+++ projects/subject-access-requests-and-delius/src/test/kotlin/uk/gov/justice/digital/hmpps/service/SubjectAccessRequestsServiceTest.kt
@@ -8,8 +8,10 @@ import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
 import org.mockito.kotlin.whenever
+import uk.gov.justice.digital.hmpps.api.model.Name
+import uk.gov.justice.digital.hmpps.api.model.Person
 import uk.gov.justice.digital.hmpps.data.generator.PersonGenerator
-import uk.gov.justice.digital.hmpps.integration.delius.person.entity.Person
+import uk.gov.justice.digital.hmpps.integration.delius.person.entity.Person as PersonEntity
 import uk.gov.justice.digital.hmpps.integration.delius.person.entity.PersonRepository
 
 @ExtendWith(MockitoExtension::class)
@@ -36,15 +38,26 @@ class SubjectAccessRequestsServiceTest {
 
         val response = subjectAccessRequestsService.getPersonDetailsByCrn(crn)
 
-        assertEquals(fullName, response.fullName)
+        val expectedResponse = Person(getName(fullName))
+
+        assertEquals(expectedResponse, response)
     }
 
-    private fun getPerson(person: Int): Person {
+    private fun getPerson(person: Int): PersonEntity {
         return when (person) {
             1 -> PersonGenerator.PERSON1
             2 -> PersonGenerator.PERSON2
             3 -> PersonGenerator.PERSON3
             else -> PersonGenerator.PERSON4
+        }
+    }
+
+    private fun getName(fullName: String): Name {
+        val name = fullName.split(" ")
+        return when (name.size) {
+            4 -> Name(name[0], name[1] + " " + name[2], name[3])
+            3 -> Name(name[0], name[1], name[2])
+            else -> Name(name[0], surname = name[1])
         }
     }
 }
