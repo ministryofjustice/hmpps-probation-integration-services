e00dfdcae1945857e2575ac5249529d6c29e361e
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2024-08-22 17:14:20 +0100

    PI-2443: Handle exceptions when calling service for comparison

--- projects/court-case-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/proxy/DataNotAvailableException.kt
+++ projects/court-case-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/proxy/DataNotAvailableException.kt
@@ -2,3 +2,6 @@ package uk.gov.justice.digital.hmpps.api.proxy
 
 class DataNotAvailableException(param: String) :
     RuntimeException("No Data available for param $param")
+
+class ComparisonException(message: String?) :
+    RuntimeException("Calling new api failed with $message")
