1aab4c3f32507ff5dbd20a2b9972df186695398e
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2024-08-28 15:09:55 +0100

    PI-2457: Added mappa endpoint to MPC

--- projects/manage-pom-cases-and-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/RegistrationRepositoryTest.kt
+++ projects/manage-pom-cases-and-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/RegistrationRepositoryTest.kt
@@ -5,10 +5,13 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import uk.gov.justice.digital.hmpps.data.generator.PersonGenerator
+import uk.gov.justice.digital.hmpps.data.generator.ProviderGenerator
 import uk.gov.justice.digital.hmpps.data.generator.RegistrationGenerator
 import uk.gov.justice.digital.hmpps.integrations.delius.person.entity.Person
 import uk.gov.justice.digital.hmpps.integrations.delius.person.entity.PersonRepository
 import uk.gov.justice.digital.hmpps.integrations.delius.person.entity.registration.entity.RegistrationRepository
+import uk.gov.justice.digital.hmpps.integrations.delius.provider.entity.Staff
+import uk.gov.justice.digital.hmpps.integrations.delius.provider.entity.StaffRepository
 import java.time.LocalDate
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -19,10 +22,14 @@ internal class RegistrationRepositoryTest {
     @Autowired
     lateinit var registrationRepository: RegistrationRepository
 
+    @Autowired
+    lateinit var staffRepository: StaffRepository
+
     @Test
     fun `correctly identifies when a vlo is assigned`() {
         val person = givenPerson()
-        hasQualifyingRegistration(person)
+        val staff = givenStaff()
+        hasQualifyingRegistration(person, staff)
 
         val vloAssigned = registrationRepository.hasVloAssigned(person.id)
         assertTrue(vloAssigned)
@@ -31,13 +38,17 @@ internal class RegistrationRepositoryTest {
     private fun givenPerson() =
         personRepository.save(PersonGenerator.generate("V123456", "V1234LO"))
 
-    private fun hasQualifyingRegistration(person: Person) =
+    private fun givenStaff() =
+        staffRepository.save(ProviderGenerator.generateStaff("TEST", "TEST", "TEST"))
+
+    private fun hasQualifyingRegistration(person: Person, staff: Staff) =
         registrationRepository.save(
             RegistrationGenerator.generate(
                 RegistrationGenerator.TYPE_DASO,
                 null,
                 LocalDate.now().minusDays(1),
-                person = person
+                person = person,
+                staff = staff
             )
         )
 }
