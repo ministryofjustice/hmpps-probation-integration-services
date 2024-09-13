6dac1e7cb4f8e4562c975367205b5e1d7eb6b836
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2024-09-13 12:03:13 +0100

    PI-2521: Set proxy timeouts the same as court case

--- projects/court-case-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/config/RestClientConfig.kt
+++ projects/court-case-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/config/RestClientConfig.kt
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestClient
 import uk.gov.justice.digital.hmpps.api.proxy.CommunityApiClient
 import uk.gov.justice.digital.hmpps.config.security.createClient
+import uk.gov.justice.digital.hmpps.config.security.withTimeouts
 import uk.gov.justice.digital.hmpps.integrations.courtcase.CourtCaseClient
+import java.time.Duration
 
 @Configuration
 class RestClientConfig(private val oauth2Client: RestClient, private val restClientBuilder: RestClient.Builder) {
@@ -14,7 +16,8 @@ class RestClientConfig(private val oauth2Client: RestClient, private val restCli
     fun courtCaseClient() = createClient<CourtCaseClient>(oauth2Client)
 
     @Bean
-    fun proxyClient() = restClientBuilder.build()
+    fun proxyClient() =
+        restClientBuilder.requestFactory(withTimeouts(Duration.ofSeconds(20), Duration.ofSeconds(5))).build()
 
     @Bean
     fun communityApiClient() = createClient<CommunityApiClient>(proxyClient())
