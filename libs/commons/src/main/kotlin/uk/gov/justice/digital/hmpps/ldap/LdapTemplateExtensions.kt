86f88c615ff790bdee07b79803b2b89f1ae96957
pmcphee77 <150798161+pmcphee77@users.noreply.github.com>
2024-09-17 16:27:12 +0100

    PI-2479: Return not found when LDAP user not found (#4241)

--- libs/commons/src/main/kotlin/uk/gov/justice/digital/hmpps/ldap/LdapTemplateExtensions.kt
+++ libs/commons/src/main/kotlin/uk/gov/justice/digital/hmpps/ldap/LdapTemplateExtensions.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps.ldap
 
 import io.opentelemetry.instrumentation.annotations.SpanAttribute
 import io.opentelemetry.instrumentation.annotations.WithSpan
+import org.springframework.ldap.NameNotFoundException
 import org.springframework.ldap.core.AttributesMapper
 import org.springframework.ldap.core.LdapTemplate
 import org.springframework.ldap.query.LdapQuery
@@ -25,26 +26,32 @@ inline fun <reified T> LdapTemplate.findByUsername(@SpanAttribute username: Stri
 fun LdapTemplate.findEmailByUsername(@SpanAttribute username: String) = findAttributeByUsername(username, "mail")
 
 @WithSpan
-fun LdapTemplate.findAttributeByUsername(@SpanAttribute username: String, @SpanAttribute attribute: String) = search(
-    query()
+fun LdapTemplate.findAttributeByUsername(@SpanAttribute username: String, @SpanAttribute attribute: String) = try {
+    search(query()
         .attributes(attribute)
         .searchScope(SearchScope.ONELEVEL)
         .where("objectclass").`is`("inetOrgPerson")
         .and("objectclass").`is`("top")
         .and("cn").`is`(username),
-    AttributesMapper { it[attribute]?.get()?.toString() }
-).singleOrNull()
+        AttributesMapper { it[attribute]?.get()?.toString() }
+    ).singleOrNull()
+} catch (_: NameNotFoundException) {
+    throw NotFoundException("NDeliusUser of $username not found")
+}
 
 @WithSpan
-fun LdapTemplate.getRoles(@SpanAttribute username: String) = search(
-    query()
+fun LdapTemplate.getRoles(@SpanAttribute username: String) = try {
+    search(query()
         .attributes("cn")
         .base(LdapNameBuilder.newInstance().add("cn", username).build())
         .searchScope(SearchScope.ONELEVEL)
         .where("objectclass").`is`("NDRole")
         .or("objectclass").`is`("NDRoleAssociation"),
-    AttributesMapper { it["cn"]?.get()?.toString() }
-).filterNotNull()
+        AttributesMapper { it["cn"]?.get()?.toString() }
+    ).filterNotNull()
+} catch (_: NameNotFoundException) {
+    throw NotFoundException("NDeliusUser of $username not found")
+}
 
 @WithSpan
 fun LdapTemplate.addRole(@SpanAttribute username: String, @SpanAttribute role: DeliusRole) {
@@ -56,12 +63,20 @@ fun LdapTemplate.addRole(@SpanAttribute username: String, @SpanAttribute role: D
         put(listOf("NDRoleAssociation", "alias", "top").asAttribute("objectclass"))
     }
     val userRole = role.context(username)
-    rebind(userRole, null, attributes)
+    try {
+        rebind(userRole, null, attributes)
+    } catch (_: NameNotFoundException) {
+        throw NotFoundException("NDeliusUser of $username not found")
+    }
 }
 
 @WithSpan
 fun LdapTemplate.removeRole(@SpanAttribute username: String, @SpanAttribute role: DeliusRole) =
-    unbind(role.context(username))
+    try {
+        unbind(role.context(username))
+    } catch (_: NameNotFoundException) {
+        throw NotFoundException("NDeliusUser of $username not found")
+    }
 
 private fun DeliusRole.context(username: String? = null) =
     LdapNameBuilder.newInstance()
