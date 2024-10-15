91159b929704ecb552acdb09f9cb7f5e10b3fe99
Marcus Aspin <maspin@unilink.com>
2024-10-15 18:31:19 +0100

    PI-2556 Added LAO endpoints for allocations service (#4318)
    * PI-2556 Added LAO endpoints for allocations service
    
    * Formatting changes
    
    ---------
    
    Co-authored-by: probation-integration-bot[bot] <177347787+probation-integration-bot[bot]@users.noreply.github.com>

--- projects/workforce-allocations-to-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/UserIntegrationTest.kt
+++ projects/workforce-allocations-to-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/UserIntegrationTest.kt
@@ -2,25 +2,39 @@ package uk.gov.justice.digital.hmpps
 
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.equalTo
+import org.hamcrest.Matchers.equalToIgnoringCase
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import uk.gov.justice.digital.hmpps.api.model.CaseAccess
+import uk.gov.justice.digital.hmpps.api.model.CaseAccessList
+import uk.gov.justice.digital.hmpps.api.model.User
 import uk.gov.justice.digital.hmpps.api.model.UserAccess
 import uk.gov.justice.digital.hmpps.data.generator.PersonGenerator
 import uk.gov.justice.digital.hmpps.data.generator.UserGenerator
+import uk.gov.justice.digital.hmpps.test.MockMvcExtensions.andExpectJson
 import uk.gov.justice.digital.hmpps.test.MockMvcExtensions.contentAsJson
 import uk.gov.justice.digital.hmpps.test.MockMvcExtensions.withJson
 import uk.gov.justice.digital.hmpps.test.MockMvcExtensions.withToken
 
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class LimitedAccessIntegrationTest {
+class UserIntegrationTest {
     @Autowired
     lateinit var mockMvc: MockMvc
+
+    @Test
+    fun `get all users`() {
+        mockMvc.perform(get("/users").withToken())
+            .andExpect(status().is2xxSuccessful)
+            .andExpect(jsonPath("$[0].username", equalToIgnoringCase("JoeBloggs")))
+    }
 
     @Test
     fun `limited access controls are correctly returned`() {
@@ -108,6 +122,51 @@ class LimitedAccessIntegrationTest {
         assertThat(
             result.access.first { it.crn == PersonGenerator.DEFAULT.crn },
             equalTo(CaseAccess(PersonGenerator.DEFAULT.crn, userExcluded = false, userRestricted = false))
+        )
+    }
+
+    @Test
+    fun `get all access limitations`() {
+        mockMvc.perform(get("/person/${PersonGenerator.RESTRICTION_EXCLUSION.crn}/limited-access/all").withToken())
+            .andExpectJson(
+                CaseAccessList(
+                    crn = PersonGenerator.RESTRICTION_EXCLUSION.crn,
+                    excludedFrom = listOf(User(UserGenerator.LIMITED_ACCESS_USER.username)),
+                    restrictedTo = listOf(User(UserGenerator.AUDIT_USER.username)),
+                    exclusionMessage = PersonGenerator.RESTRICTION_EXCLUSION.exclusionMessage,
+                    restrictionMessage = PersonGenerator.RESTRICTION_EXCLUSION.restrictionMessage,
+                )
+            )
+    }
+
+    @Test
+    fun `get all access limitations filtered by username`() {
+        mockMvc.perform(
+            post("/person/${PersonGenerator.RESTRICTION_EXCLUSION.crn}/limited-access")
+                .withToken()
+                .withJson(listOf(UserGenerator.AUDIT_USER.username))
+        ).andExpectJson(
+            CaseAccessList(
+                crn = PersonGenerator.RESTRICTION_EXCLUSION.crn,
+                excludedFrom = emptyList(),
+                restrictedTo = listOf(User(UserGenerator.AUDIT_USER.username)),
+                exclusionMessage = PersonGenerator.RESTRICTION_EXCLUSION.exclusionMessage,
+                restrictionMessage = PersonGenerator.RESTRICTION_EXCLUSION.restrictionMessage,
+            )
+        )
+
+        mockMvc.perform(
+            post("/person/${PersonGenerator.RESTRICTION_EXCLUSION.crn}/limited-access")
+                .withToken()
+                .withJson(listOf("SomeOtherUsername"))
+        ).andExpectJson(
+            CaseAccessList(
+                crn = PersonGenerator.RESTRICTION_EXCLUSION.crn,
+                excludedFrom = emptyList(),
+                restrictedTo = emptyList(),
+                exclusionMessage = PersonGenerator.RESTRICTION_EXCLUSION.exclusionMessage,
+                restrictionMessage = PersonGenerator.RESTRICTION_EXCLUSION.restrictionMessage,
+            )
         )
     }
 }
