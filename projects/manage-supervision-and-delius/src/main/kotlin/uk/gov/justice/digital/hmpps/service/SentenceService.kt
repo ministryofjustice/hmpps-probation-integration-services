69df04bd3f6c274dc2e209d2ce9079b717528525
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2024-10-17 00:22:57 +0100

    MAN-28 - remove characters not needed

--- projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/SentenceService.kt
+++ projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/SentenceService.kt
@@ -94,7 +94,7 @@ class SentenceService(
             val splitParam = "---------------------------------------------------------" + System.lineSeparator()
             return notes.split(splitParam).map { note ->
                 val addedBy = Regex(
-                    "^Comment added by .+? on \\d{2}\\/\\d{2}\\/\\d{4} at \\d{2}:\\d{2}"
+                    "^Comment added by .+? on \\d{2}/\\d{2}/\\d{4} at \\d{2}:\\d{2}"
                         + System.lineSeparator()
                 ).find(note)?.value
                 val noteText = addedBy?.let { note.removePrefix(addedBy) } ?: note
