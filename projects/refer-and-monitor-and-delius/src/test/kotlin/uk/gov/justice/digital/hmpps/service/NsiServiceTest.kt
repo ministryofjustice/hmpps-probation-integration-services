0a4b89e59173c3ed7270d2cf71d1ebc69fff3665
Marcus Aspin <maspin@unilink.com>
2024-10-21 10:26:46 +0000

    PI-2585 Remove unused feature flags

--- projects/refer-and-monitor-and-delius/src/test/kotlin/uk/gov/justice/digital/hmpps/service/NsiServiceTest.kt
+++ projects/refer-and-monitor-and-delius/src/test/kotlin/uk/gov/justice/digital/hmpps/service/NsiServiceTest.kt
@@ -13,7 +13,6 @@ import uk.gov.justice.digital.hmpps.api.model.ReferralStarted
 import uk.gov.justice.digital.hmpps.audit.service.AuditedInteractionService
 import uk.gov.justice.digital.hmpps.data.generator.ContactGenerator
 import uk.gov.justice.digital.hmpps.data.generator.NsiGenerator
-import uk.gov.justice.digital.hmpps.flags.FeatureFlags
 import uk.gov.justice.digital.hmpps.integrations.delius.contact.ContactOutcomeRepository
 import uk.gov.justice.digital.hmpps.integrations.delius.contact.ContactRepository
 import uk.gov.justice.digital.hmpps.integrations.delius.contact.ContactTypeRepository
@@ -58,9 +57,6 @@ internal class NsiServiceTest {
 
     @Mock
     lateinit var telemetryService: TelemetryService
-
-    @Mock
-    lateinit var featureFlags: FeatureFlags
 
     @InjectMocks
     lateinit var nsiService: NsiService
