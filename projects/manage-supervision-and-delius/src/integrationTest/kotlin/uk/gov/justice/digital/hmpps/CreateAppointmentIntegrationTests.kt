14870882c069b3dfb45cb925d22e1c63b13245b2
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2024-11-01 12:47:59 +0000

    MAN-109 - update integration test

--- projects/manage-supervision-and-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/CreateAppointmentIntegrationTests.kt
+++ projects/manage-supervision-and-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/CreateAppointmentIntegrationTests.kt
@@ -105,23 +105,14 @@ class CreateAppointmentIntegrationTests {
         appointmentRepository.delete(appointment)
     }
 
-    @Test
-    fun `create multiple appointments`() {
+    @ParameterizedTest
+    @MethodSource("createMultipleAppointments")
+    fun `create multiple appointments`(createAppointment: CreateAppointment) {
         val person = PersonGenerator.PERSON_1
-
-        val uuid = UUID.randomUUID()
         val response = mockMvc.perform(
             post("/appointments/${person.crn}")
                 .withToken()
-                .withJson(
-                    CreateAppointment(
-                        CreateAppointment.Type.HomeVisitToCaseNS,
-                        ZonedDateTime.now(),
-                        numberOfAppointments = 3,
-                        eventId = PersonGenerator.EVENT_1.id,
-                        uuid = uuid
-                    )
-                ))
+                .withJson(createAppointment))
             .andDo(print())
             .andExpect(MockMvcResultMatchers.status().isCreated)
             .andReturn().response.contentAsJson<AppointmentDetail>()
@@ -135,7 +126,7 @@ class CreateAppointmentIntegrationTests {
         assertThat(appointments[2].date, equalTo(LocalDate.now().plusDays(2)))
 
         //check for unique external reference
-        val externalRef = "urn:uk:gov:hmpps:manage-supervision-service:appointment:$uuid"
+        val externalRef = "urn:uk:gov:hmpps:manage-supervision-service:appointment:${createAppointment.uuid}"
         assertThat(appointments[0].externalReference, equalTo(externalRef))
         assertNotEquals(externalRef, appointments[1].externalReference)
         assertNotEquals(externalRef, appointments[2].externalReference)
@@ -159,6 +150,24 @@ class CreateAppointmentIntegrationTests {
                 ZonedDateTime.now().plusDays(1),
                 null,
                 CreateAppointment.Interval.DAY,
+                eventId = PersonGenerator.EVENT_1.id,
+                uuid = UUID.randomUUID()
+            )
+        )
+
+        @JvmStatic
+        fun createMultipleAppointments() = listOf(
+            CreateAppointment(
+                CreateAppointment.Type.HomeVisitToCaseNS,
+                ZonedDateTime.now(),
+                numberOfAppointments = 3,
+                eventId = PersonGenerator.EVENT_1.id,
+                uuid = UUID.randomUUID()
+            ),
+            CreateAppointment(
+                CreateAppointment.Type.HomeVisitToCaseNS,
+                ZonedDateTime.now(),
+                until = ZonedDateTime.now().plusDays(3),
                 eventId = PersonGenerator.EVENT_1.id,
                 uuid = UUID.randomUUID()
             )
