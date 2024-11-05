e3e722ab6f41df066a5ca761a5b5501c10b19608
Marcus Aspin <maspin@unilink.com>
2024-11-05 15:28:37 +0000

    PI-2584 Add missing LDAP username + add more Telemetry

--- projects/justice-email-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/MailboxService.kt
+++ projects/justice-email-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/MailboxService.kt
@@ -9,20 +9,24 @@ import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.message.Notification
 import uk.gov.justice.digital.hmpps.messaging.EmailMessage
 import uk.gov.justice.digital.hmpps.publisher.NotificationPublisher
+import uk.gov.justice.digital.hmpps.telemetry.TelemetryService
 
 @Service
 class MailboxService(
     @Value("\${microsoft-graph.email-address}")
     private val emailAddress: String,
     private val graphServiceClient: GraphServiceClient,
-    private val notificationPublisher: NotificationPublisher
+    private val notificationPublisher: NotificationPublisher,
+    private val telemetryService: TelemetryService,
 ) {
     @WithSpan("POLL mailbox", kind = SpanKind.SERVER)
     fun publishUnreadMessagesToQueue() {
-        getUnreadMessages().forEach { message ->
-            notificationPublisher.publish(message.asNotification())
-            message.markAsRead()
-        }
+        getUnreadMessages()
+            .also { telemetryService.trackEvent("ReceivedMessages", mapOf("count" to it.size.toString())) }
+            .forEach {
+                notificationPublisher.publish(it.asNotification())
+                it.markAsRead()
+            }
     }
 
     private fun getUnreadMessages() = graphServiceClient
@@ -36,7 +40,7 @@ class MailboxService(
             request.queryParameters.filter = "isRead ne true"
             request.queryParameters.select = arrayOf("subject", "from", "id", "receivedDateTime", "body", "isRead")
             request.queryParameters.orderby = arrayOf("receivedDateTime DESC")
-        }.value
+        }.value ?: emptyList()
 
     private fun Message.markAsRead() {
         graphServiceClient
