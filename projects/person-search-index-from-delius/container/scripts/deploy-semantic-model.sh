159700f4a92cda1b2e37ad6bd2e9b7df7d878cd8
Marcus Aspin <maspin@unilink.com>
2025-01-17 13:17:24 +0000

    PI-2526 Switch to cross-account SageMaker connector (#4541)
    * PI-2526 Switch to cross-account SageMaker connector
    
    * Fix embedding of text chunks
    
    * Set endpoint name for all envs
    
    * Switch to environment-specific endpoint names

--- projects/person-search-index-from-delius/container/scripts/deploy-semantic-model.sh
+++ projects/person-search-index-from-delius/container/scripts/deploy-semantic-model.sh
@@ -5,12 +5,12 @@ eval "$(sentry-cli bash-hook --no-environ)"
 
 ## Create model group if it doesn't exist
 echo Searching for existing model group...
-model_group_id=$(curl_json -XPOST "${SEARCH_INDEX_HOST}/_plugins/_ml/model_groups/_search" --data '{"query":{"match":{"name":"bedrock_model_group"}}}' | jq -r '.hits.hits[0]._id // ""')
+model_group_id=$(curl_json -XPOST "${SEARCH_INDEX_HOST}/_plugins/_ml/model_groups/_search" --data '{"query":{"match":{"name":"sagemaker_model_group"}}}' | jq -r '.hits.hits[0]._id // ""')
 if [ -z "$model_group_id" ]; then
   echo Creating model group...
   model_group=$(curl_json -XPOST "${SEARCH_INDEX_HOST}/_plugins/_ml/model_groups/_register" --data '{
-    "name": "bedrock_model_group",
-    "description": "A model group for Bedrock models"
+    "name": "sagemaker_model_group",
+    "description": "A model group for SageMaker models"
   }')
   if [ "$(jq -r '.status' <<<"$model_group")" != "CREATED" ]; then fail "Failed to create model group: $model_group"; fi
   model_group_id=$(jq -r '.model_group_id' <<<"$model_group")
@@ -18,12 +18,12 @@ else
   echo "Found model group with id=$model_group_id"
 fi
 
-## Create Bedrock connector if it doesn't exist
+## Create SageMaker connector if it doesn't exist
 echo Searching for existing connector...
-connector_id=$(curl_json -XPOST "${SEARCH_INDEX_HOST}/_plugins/_ml/connectors/_search" --data "{\"query\":{\"match\":{\"name.keyword\":\"bedrock-${BEDROCK_MODEL_NAME}\"}}}" | jq -r '.hits.hits[0]._id // ""')
+connector_id=$(curl_json -XPOST "${SEARCH_INDEX_HOST}/_plugins/_ml/connectors/_search" --data "{\"query\":{\"match\":{\"name.keyword\":\"sagemaker-embeddings\"}}}" | jq -r '.hits.hits[0]._id // ""')
 if [ -z "$connector_id" ]; then
   echo Creating connector...
-  connector_body=$(envsubst < /pipelines/contact/index/bedrock-connector.json)
+  connector_body=$(envsubst < /pipelines/contact/index/sagemaker-connector.json)
   connector_id=$(curl_json -XPOST "${SEARCH_INDEX_HOST}/_plugins/_ml/connectors/_create" --data "$connector_body" | jq -r '.connector_id')
 else
   echo "Found connector with id=$connector_id"
@@ -31,13 +31,13 @@ fi
 
 ## Register model if it doesn't exist
 model_body="{
-  \"name\": \"bedrock-${BEDROCK_MODEL_NAME}\",
-  \"description\": \"Bedrock embedding model\",
+  \"name\": \"sagemaker-embeddings\",
+  \"description\": \"SageMaker embedding model\",
   \"function_name\": \"remote\",
   \"model_group_id\": \"${model_group_id}\",
   \"connector_id\": \"${connector_id}\"
 }"
-model_id=$(curl_json -XPOST "${SEARCH_INDEX_HOST}/_plugins/_ml/models/_search" --data "{\"query\":{\"match\":{\"name.keyword\":\"bedrock-${BEDROCK_MODEL_NAME}\"}}}" | jq -r '.hits.hits[0]._id // ""')
+model_id=$(curl_json -XPOST "${SEARCH_INDEX_HOST}/_plugins/_ml/models/_search" --data "{\"query\":{\"match\":{\"name.keyword\":\"sagemaker-embeddings\"}}}" | jq -r '.hits.hits[0]._id // ""')
 if [ -z "$model_id" ]; then
   echo Registering model...
   curl_json -XPOST "${SEARCH_INDEX_HOST}/_plugins/_ml/models/_register" --data "${model_body}"
