6effce911a46fa7497d201f2a1effef9e5930861
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2025-01-21 14:13:22 +0000

    MAN-291 - include last termination date to response

--- projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/ComplianceService.kt
+++ projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/ComplianceService.kt
@@ -79,6 +79,7 @@ class ComplianceService(
             previousOrders = PreviousOrders(
                 breaches = previousOrders.map { breachesForSentence(it.id) }.count(),
                 count = previousOrders.size,
+                lastEndedDate = previousOrders.first().disposal?.terminationDate,
                 orders = previousOrders.mapNotNull {
                     it.disposal?.toOrder(
                         it.mainOffence?.offence?.description,
