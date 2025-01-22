eef79fd1868daaa065e8c0e68a49c681e2aee456
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2025-01-22 14:17:57 +0000

    MAN-319 PI-2749: MPOP Activity Search API

--- projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/model/activity/PersonActivity.kt
+++ projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/model/activity/PersonActivity.kt
@@ -1,8 +1,28 @@
 package uk.gov.justice.digital.hmpps.api.model.activity
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import uk.gov.justice.digital.hmpps.api.model.PersonSummary
+import java.time.LocalDate
 
 data class PersonActivity(
     val personSummary: PersonSummary,
     val activities: List<Activity>
-) 
+)
+
+data class PersonActivitySearchResponse(
+    val size: Int,
+    val page: Int,
+    val totalResults: Long,
+    val totalPages: Int,
+    val personSummary: PersonSummary,
+    val activities: List<Activity>
+)
+
+data class PersonActivitySearchRequest(
+    val keywords: String? = "",
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    val dateFrom: LocalDate? = null,
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    val dateTo: LocalDate? = null,
+    val filters: List<String> = emptyList(),
+)
---
+++ projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/client/ProbationSearchClient.kt
@@ -0,0 +1,40 @@
+package uk.gov.justice.digital.hmpps.client
+
+import com.fasterxml.jackson.annotation.JsonFormat
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.service.annotation.PostExchange
+import java.time.LocalDate
+
+interface ProbationSearchClient {
+    @PostExchange(url = "/search/activity")
+    fun contactSearch(
+        @RequestBody body: ActivitySearchRequest,
+        @RequestParam page: Int = 0,
+        @RequestParam size: Int = 10): ContactSearchResponse
+}
+
+data class ActivitySearchRequest(
+    val crn: String,
+    val keywords: String? = "",
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    val dateFrom: LocalDate? = null,
+    @JsonFormat(pattern = "yyyy-MM-dd")
+    val dateTo: LocalDate? = null,
+    val filters: List<String> = emptyList(),
+)
+
+
+data class ContactSearchResponse(
+    val size: Int,
+    val page: Int,
+    val totalResults: Long,
+    val totalPages: Int,
+    val results: List<ContactSearchResult>,
+)
+
+data class ContactSearchResult(
+    val crn: String,
+    val id: Long,
+    val highlights: Map<String, List<String>> = mapOf()
+)
