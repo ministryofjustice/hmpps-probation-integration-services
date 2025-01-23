d88f4f759c322d647d977007d254dc7ec343d7b4
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2025-01-23 08:54:25 +0000

    reduce select queries

--- projects/prison-case-notes-to-probation/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/repository/CaseNoteRepository.kt
+++ projects/prison-case-notes-to-probation/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/repository/CaseNoteRepository.kt
@@ -1,9 +1,12 @@
 package uk.gov.justice.digital.hmpps.integrations.delius.repository
 
+import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
 import uk.gov.justice.digital.hmpps.integrations.delius.entity.CaseNote
 
 interface CaseNoteRepository : JpaRepository<CaseNote, Long> {
+    @EntityGraph(attributePaths = ["type"])
     fun findByNomisId(nomisId: Long): CaseNote?
+    @EntityGraph(attributePaths = ["type"])
     fun findByExternalReference(externalReference: String): CaseNote?
 }
