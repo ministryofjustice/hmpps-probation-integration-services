4132f9e836367c1aa1b3dd1a4f2857732b23dedf
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2025-01-24 09:39:03 +0000

    MAN-36 - include inactive requirements

--- projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/overview/entity/Requirement.kt
+++ projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/overview/entity/Requirement.kt
@@ -168,7 +168,6 @@ interface RequirementRepository : JpaRepository<Requirement, Long> {
             AND e.event_number = :eventNumber
             AND e.soft_deleted = 0 
             AND e.active_flag = 1
-            AND r.active_flag = 1
             AND r.soft_deleted = 0
             ORDER BY rrtmc.description
         """, nativeQuery = true
@@ -207,7 +206,6 @@ interface RequirementRepository : JpaRepository<Requirement, Long> {
             WHERE r.rqmnt_id = :id
             AND e.soft_deleted = 0 
             AND e.active_flag = 1
-            AND r.active_flag = 1
             AND r.soft_deleted = 0            
         """, nativeQuery = true
     )
