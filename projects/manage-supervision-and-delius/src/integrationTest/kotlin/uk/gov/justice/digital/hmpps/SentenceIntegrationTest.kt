e5eae451b1bd383211e4499317ac8560356995b6
achimber-moj <161360519+achimber-moj@users.noreply.github.com>
2025-02-03 09:46:18 +0000

    PI-362 - add event number to compliance api previous orders (#4610)
    * PI-362 - add event number to compliance api previous orders
    
    * Formatting changes
    
    ---------
    
    Co-authored-by: probation-integration-bot[bot] <177347787+probation-integration-bot[bot]@users.noreply.github.com>

--- projects/manage-supervision-and-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/SentenceIntegrationTest.kt
+++ projects/manage-supervision-and-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/SentenceIntegrationTest.kt
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.TestPropertySource
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
@@ -115,9 +114,9 @@ class SentenceIntegrationTest {
                     listOf(AdditionalSentence(3, null, null, "Disqualified from Driving"))
                 ),
                 Order(
-                    "Default Sentence Type",
-                    12,
-                    null,
+                    description = "Default Sentence Type",
+                    length = 12,
+                    endDate = null,
                     releaseDate = RELEASE_3.date.toLocalDate(),
                     startDate = LocalDate.now().minusDays(14)
                 ),
