02d29dcdda3f2485983b105c8d36b6227638b3bd
pmcphee77 <150798161+pmcphee77@users.noreply.github.com>
2025-02-06 15:56:53 +0000

    PI-2783: Added audit and domain events for person updates (#4631)
    * PI-2783: Added audit and domain events for person updates

--- projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/audit/BusinessInteractionCode.kt
+++ projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/audit/BusinessInteractionCode.kt
@@ -3,5 +3,8 @@ package uk.gov.justice.digital.hmpps.integrations.delius.audit
 import uk.gov.justice.digital.hmpps.audit.InteractionCode
 
 enum class BusinessInteractionCode(override val code: String) : InteractionCode {
-    ADD_CONTACT("CLBI003")
+    ADD_CONTACT("CLBI003"),
+    INSERT_ADDRESS("OIBI029"),
+    UPDATE_ADDRESS("OIBI032"),
+    UPDATE_PERSON("OIBI027"),
 }
