f526c9381c869c6dd281acd5294593f48a8384f9
james.macbeth <james.macbeth@justice.gov.uk>
2025-02-19 10:23:25 +0000

    PI-2812 - Mirror incoming messages into OpenSearch.

---
+++ projects/common-platform-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/config/OpenSearchConfiguration.kt
@@ -0,0 +1,30 @@
+package uk.gov.justice.digital.hmpps.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.apache.http.HttpHost
+import org.opensearch.client.RestClient
+import org.opensearch.client.RestHighLevelClient
+import org.opensearch.client.json.jackson.JacksonJsonpMapper
+import org.opensearch.data.client.orhlc.OpenSearchRestTemplate
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+
+@Configuration
+class OpenSearchConfiguration {
+    @Value("\${opensearch.uris}")
+    private val url: String? = null
+
+    @Bean
+    fun openSearchClient() = RestHighLevelClient(RestClient.builder(HttpHost.create(url)))
+
+    @Bean(name = ["elasticsearchTemplate", "elasticsearchOperations"])
+    @ConditionalOnMissingBean(OpenSearchRestTemplate::class)
+    fun openSearchRestTemplate(client: RestHighLevelClient) = OpenSearchRestTemplate(client)
+
+    @Bean
+    @Primary
+    fun jsonpMapper(objectMapper: ObjectMapper) = JacksonJsonpMapper(objectMapper)
+}
