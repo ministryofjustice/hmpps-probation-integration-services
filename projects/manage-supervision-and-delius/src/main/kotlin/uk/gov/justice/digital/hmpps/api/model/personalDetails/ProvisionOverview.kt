e47aa70dfb679c42c3897c3f3a7f51e81ba29f7c
achimber-moj <161360519+achimber-moj@users.noreply.github.com>
2025-02-20 16:59:10 +0000

    PI-2853 format notes for personal provisions (#4694)
    * PI-2853 format notes for personal provisions
    
    * Formatting changes
    
    ---------
    
    Co-authored-by: probation-integration-bot[bot] <177347787+probation-integration-bot[bot]@users.noreply.github.com>

--- projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/model/personalDetails/ProvisionOverview.kt
+++ projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/model/personalDetails/ProvisionOverview.kt
@@ -2,17 +2,24 @@ package uk.gov.justice.digital.hmpps.api.model.personalDetails
 
 import uk.gov.justice.digital.hmpps.api.model.Name
 import uk.gov.justice.digital.hmpps.api.model.PersonSummary
+import uk.gov.justice.digital.hmpps.api.model.sentence.NoteDetail
 import java.time.LocalDate
 
 data class ProvisionOverview(
     val personSummary: PersonSummary,
-    val provisions: List<Provision>,
+    val provisions: List<Provision>
+)
 
-    )
+data class ProvisionOverviewSummary(
+    val personSummary: PersonSummary,
+    val provision: Provision?
+)
 
 data class Provision(
+    val id: Long,
     val description: String,
-    val notes: String?,
+    val provisionNotes: List<NoteDetail>? = null,
+    val provisionNote: NoteDetail? = null,
     val startDate: LocalDate,
     val lastUpdated: LocalDate,
     val lastUpdatedBy: Name
