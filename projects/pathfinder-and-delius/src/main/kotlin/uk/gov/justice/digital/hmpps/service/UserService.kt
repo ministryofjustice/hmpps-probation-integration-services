4ab80700cc0b1db4f5d74d93d053ad6f1d3572f5
Marcus Aspin <maspin@unilink.com>
2025-02-20 16:58:50 +0000

    PI-2858 Add user details endpoint for pathfinder

--- projects/pathfinder-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/UserService.kt
+++ projects/pathfinder-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/UserService.kt
@@ -2,14 +2,21 @@ package uk.gov.justice.digital.hmpps.service
 
 import org.springframework.ldap.core.LdapTemplate
 import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.entity.LdapUser
+import uk.gov.justice.digital.hmpps.exception.NotFoundException
 import uk.gov.justice.digital.hmpps.ldap.addRole
+import uk.gov.justice.digital.hmpps.ldap.findByUsername
 import uk.gov.justice.digital.hmpps.ldap.removeRole
 import uk.gov.justice.digital.hmpps.model.DeliusRole
+import uk.gov.justice.digital.hmpps.model.UserDetails
 
 @Service
 class UserService(private val ldapTemplate: LdapTemplate) {
-
     fun addRole(username: String, role: DeliusRole) = ldapTemplate.addRole(username, role)
 
     fun removeRole(username: String, role: DeliusRole) = ldapTemplate.removeRole(username, role)
+
+    fun getUserDetails(username: String) = ldapTemplate.findByUsername<LdapUser>(username)
+        ?.let { UserDetails(it.username, it.forename, it.surname, it.email) }
+        ?: throw NotFoundException("User", "username", username)
 }
