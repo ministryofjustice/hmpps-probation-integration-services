7ff6bf730a1981421294af56b796e0909b9a4b75
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2025-02-24 13:26:52 +0000

    PI-2864: apply review comment

--- projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/overview/entity/Event.kt
+++ projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/overview/entity/Event.kt
@@ -60,7 +60,7 @@ class Event(
     @Column(name = "created_datetime")
     val dateCreated: ZonedDateTime,
 ) {
-    fun isInactiveEvent(): Boolean = !active || (disposal?.active == false)
+    fun isInactiveEvent(): Boolean = !active || disposal?.active == false
 }
 
 interface EventRepository : JpaRepository<Event, Long> {
