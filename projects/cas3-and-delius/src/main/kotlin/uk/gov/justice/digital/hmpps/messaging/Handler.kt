861c7a7e77f692bf6ecc7b85eed7d18c33703f1c
Marcus Aspin <maspin@unilink.com>
2025-02-26 16:46:42 +0000

    PI-2875 Ignore missing domain events for CAS1 and CAS3 in dev (#4727)

--- projects/cas3-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/messaging/Handler.kt
+++ projects/cas3-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/messaging/Handler.kt
@@ -4,7 +4,10 @@ import com.asyncapi.kotlinasyncapi.annotation.Schema
 import com.asyncapi.kotlinasyncapi.annotation.channel.Channel
 import com.asyncapi.kotlinasyncapi.annotation.channel.Message
 import com.asyncapi.kotlinasyncapi.annotation.channel.Publish
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
+import org.springframework.web.client.HttpStatusCodeException
 import uk.gov.justice.digital.hmpps.converter.NotificationConverter
 import uk.gov.justice.digital.hmpps.message.HmppsDomainEvent
 import uk.gov.justice.digital.hmpps.message.Notification
@@ -20,6 +23,7 @@ class Handler(
     private val telemetryService: TelemetryService,
     private val cas3Service: Cas3Service,
     private val notifier: Notifier,
+    @Value("\${event.exception.throw-not-found:true}") private val throwNotFound: Boolean,
 ) : NotificationHandler<HmppsDomainEvent> {
     @Publish(
         messages = [
@@ -64,59 +68,64 @@ class Handler(
     override fun handle(notification: Notification<HmppsDomainEvent>) {
         telemetryService.notificationReceived(notification)
         val event = notification.message
-        when (event.eventType) {
-            "accommodation.cas3.referral.submitted" -> {
-                cas3Service.referralSubmitted(event)
-                telemetryService.trackEvent("ApplicationSubmitted", event.telemetryProperties())
-            }
-
-            "accommodation.cas3.booking.cancelled" -> {
-                cas3Service.bookingCancelled(event)
-                telemetryService.trackEvent("BookingCancelled", event.telemetryProperties())
-            }
-
-            "accommodation.cas3.booking.confirmed" -> {
-                cas3Service.bookingConfirmed(event)
-                telemetryService.trackEvent("BookingConfirmed", event.telemetryProperties())
-            }
-
-            "accommodation.cas3.booking.provisionally-made" -> {
-                cas3Service.bookingProvisionallyMade(event)
-                telemetryService.trackEvent("BookingProvisionallyMade", event.telemetryProperties())
-            }
-
-            "accommodation.cas3.person.arrived" -> {
-                val (previousAddress, newAddress) = cas3Service.personArrived(event)
-                notifier.addressCreated(event.crn(), newAddress.id, newAddress.status.description)
-                previousAddress?.let { notifier.addressUpdated(event.crn(), it.id, it.status.description) }
-                telemetryService.trackEvent("PersonArrived", event.telemetryProperties())
-            }
-
-            "accommodation.cas3.person.departed" -> {
-                cas3Service.personDeparted(event)?.let { updatedAddress ->
-                    notifier.addressUpdated(event.crn(), updatedAddress.id, updatedAddress.status.description)
+        try {
+            when (event.eventType) {
+                "accommodation.cas3.referral.submitted" -> {
+                    cas3Service.referralSubmitted(event)
+                    telemetryService.trackEvent("ApplicationSubmitted", event.telemetryProperties())
                 }
-                telemetryService.trackEvent("PersonDeparted", event.telemetryProperties())
-            }
 
-            "accommodation.cas3.person.arrived.updated" -> {
-                cas3Service.personArrivedUpdated(event)?.let { updatedAddress ->
-                    notifier.addressUpdated(event.crn(), updatedAddress.id, updatedAddress.status.description)
+                "accommodation.cas3.booking.cancelled" -> {
+                    cas3Service.bookingCancelled(event)
+                    telemetryService.trackEvent("BookingCancelled", event.telemetryProperties())
                 }
-                telemetryService.trackEvent("PersonArrivedUpdated", event.telemetryProperties())
-            }
 
-            "accommodation.cas3.person.departed.updated" -> {
-                cas3Service.personDepartedUpdated(event)
-                telemetryService.trackEvent("PersonDepartedUpdated", event.telemetryProperties())
-            }
+                "accommodation.cas3.booking.confirmed" -> {
+                    cas3Service.bookingConfirmed(event)
+                    telemetryService.trackEvent("BookingConfirmed", event.telemetryProperties())
+                }
 
-            "accommodation.cas3.booking.cancelled.updated" -> {
-                cas3Service.bookingCancelledUpdated(event)
-                telemetryService.trackEvent("BookingCancelledUpdated", event.telemetryProperties())
-            }
+                "accommodation.cas3.booking.provisionally-made" -> {
+                    cas3Service.bookingProvisionallyMade(event)
+                    telemetryService.trackEvent("BookingProvisionallyMade", event.telemetryProperties())
+                }
 
-            else -> throw IllegalArgumentException("Unexpected event type ${event.eventType}")
+                "accommodation.cas3.person.arrived" -> {
+                    val (previousAddress, newAddress) = cas3Service.personArrived(event)
+                    notifier.addressCreated(event.crn(), newAddress.id, newAddress.status.description)
+                    previousAddress?.let { notifier.addressUpdated(event.crn(), it.id, it.status.description) }
+                    telemetryService.trackEvent("PersonArrived", event.telemetryProperties())
+                }
+
+                "accommodation.cas3.person.departed" -> {
+                    cas3Service.personDeparted(event)?.let { updatedAddress ->
+                        notifier.addressUpdated(event.crn(), updatedAddress.id, updatedAddress.status.description)
+                    }
+                    telemetryService.trackEvent("PersonDeparted", event.telemetryProperties())
+                }
+
+                "accommodation.cas3.person.arrived.updated" -> {
+                    cas3Service.personArrivedUpdated(event)?.let { updatedAddress ->
+                        notifier.addressUpdated(event.crn(), updatedAddress.id, updatedAddress.status.description)
+                    }
+                    telemetryService.trackEvent("PersonArrivedUpdated", event.telemetryProperties())
+                }
+
+                "accommodation.cas3.person.departed.updated" -> {
+                    cas3Service.personDepartedUpdated(event)
+                    telemetryService.trackEvent("PersonDepartedUpdated", event.telemetryProperties())
+                }
+
+                "accommodation.cas3.booking.cancelled.updated" -> {
+                    cas3Service.bookingCancelledUpdated(event)
+                    telemetryService.trackEvent("BookingCancelledUpdated", event.telemetryProperties())
+                }
+
+                else -> throw IllegalArgumentException("Unexpected event type ${event.eventType}")
+            }
+        } catch (ex: HttpStatusCodeException) {
+            if (ex.statusCode != HttpStatus.NOT_FOUND || throwNotFound) throw ex
+            telemetryService.trackEvent("Domain event details not found", event.telemetryProperties())
         }
     }
 
