ea1f1f7d0971a0d94bbaae3d94db2f551d14ee8b
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2025-03-03 12:35:44 +0000

    PI-2887: new api to get user roles

--- projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/sentence/entity/LdapUser.kt
+++ projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/sentence/entity/LdapUser.kt
@@ -4,6 +4,10 @@ import org.springframework.ldap.odm.annotations.Attribute
 import org.springframework.ldap.odm.annotations.DnAttribute
 import org.springframework.ldap.odm.annotations.Entry
 import org.springframework.ldap.odm.annotations.Id
+import org.springframework.ldap.odm.annotations.Transient
+import java.time.LocalDate
+import java.time.LocalDate.now
+import java.time.format.DateTimeFormatter.ofPattern
 import javax.naming.Name
 
 @Entry(objectClasses = ["inetOrgPerson", "top"])
@@ -15,9 +19,24 @@ class LdapUser(
     @DnAttribute(value = "cn", index = 0)
     val username: String,
 
+    @Attribute(name = "givenName")
+    val forename: String,
+
+    @Attribute(name = "sn")
+    val surname: String,
+
     @Attribute(name = "mail")
     val email: String?,
 
     @Attribute(name = "telephoneNumber")
-    val telephone: String?
-)
+    val telephone: String?,
+
+    @Attribute(name = "endDate")
+    val endDate: String?,
+
+    @Transient
+    var roles: List<String>
+) {
+    val enabled: Boolean
+        get() = endDate == null || LocalDate.parse(endDate.substring(0, 8), ofPattern("yyyyMMdd")).isAfter(now())
+}
