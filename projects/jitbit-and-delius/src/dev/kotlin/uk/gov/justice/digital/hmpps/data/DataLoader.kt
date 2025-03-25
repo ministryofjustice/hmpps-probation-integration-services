dbaf088c2c8b08c1cdfa4045d8c88593b525668d
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2025-03-25 12:32:04 +0000

    PI-2937 LAO for jitbit (#4852)

--- projects/jitbit-and-delius/src/dev/kotlin/uk/gov/justice/digital/hmpps/data/DataLoader.kt
+++ projects/jitbit-and-delius/src/dev/kotlin/uk/gov/justice/digital/hmpps/data/DataLoader.kt
@@ -7,9 +7,12 @@ import org.springframework.context.ApplicationListener
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.data.generator.AddressGenerator
+import uk.gov.justice.digital.hmpps.data.generator.LaoGenerator
 import uk.gov.justice.digital.hmpps.data.generator.PersonGenerator
 import uk.gov.justice.digital.hmpps.data.generator.UserGenerator
+import uk.gov.justice.digital.hmpps.entity.Exclusion
 import uk.gov.justice.digital.hmpps.entity.ReferenceData
+import uk.gov.justice.digital.hmpps.entity.Restriction
 import uk.gov.justice.digital.hmpps.repository.AddressRepository
 import uk.gov.justice.digital.hmpps.repository.PersonRepository
 import uk.gov.justice.digital.hmpps.user.AuditUserRepository
@@ -21,19 +24,30 @@ class DataLoader(
     private val personRepository: PersonRepository,
     private val addressRepository: AddressRepository,
     private val referenceDataRepository: ReferenceDataRepository,
+    private val exclusionRepository: ExclusionRepository,
+    private val restrictionRepository: RestrictionRepository,
 ) : ApplicationListener<ApplicationReadyEvent> {
 
     @PostConstruct
     fun saveAuditUser() {
         auditUserRepository.save(UserGenerator.AUDIT_USER)
+        auditUserRepository.save(UserGenerator.LIMITED_ACCESS_USER)
     }
 
     override fun onApplicationEvent(are: ApplicationReadyEvent) {
-        personRepository.save(PersonGenerator.DEFAULT)
-        personRepository.save(PersonGenerator.BASIC)
+        personRepository.saveAll(
+            listOf(
+                PersonGenerator.DEFAULT, PersonGenerator.BASIC, PersonGenerator.EXCLUSION,
+                PersonGenerator.RESTRICTION, PersonGenerator.RESTRICTION_EXCLUSION
+            )
+        )
         referenceDataRepository.save(AddressGenerator.MAIN_STATUS)
         addressRepository.save(AddressGenerator.DEFAULT)
+        exclusionRepository.saveAll(listOf(LaoGenerator.EXCLUSION, LaoGenerator.BOTH_EXCLUSION))
+        restrictionRepository.saveAll(listOf(LaoGenerator.RESTRICTION, LaoGenerator.BOTH_RESTRICTION))
     }
 }
 
 interface ReferenceDataRepository : JpaRepository<ReferenceData, Long>
+interface ExclusionRepository : JpaRepository<Exclusion, Long>
+interface RestrictionRepository : JpaRepository<Restriction, Long>
