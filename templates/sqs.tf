877058889520bbc368ba84571c22c15526317c99
Marcus Aspin <maspin@unilink.com>
2025-04-08 10:02:05 +0100

    Update sqs.tf

--- templates/sqs.tf
+++ templates/sqs.tf
@@ -48,6 +48,7 @@ module "SERVICE_NAME-dlq" {
   is_production          = var.is_production
   namespace              = var.namespace
   team_name              = var.team_name
+  use_team_name          = true
 }
 
 resource "aws_sqs_queue_policy" "SERVICE_NAME-dlq-policy" {
