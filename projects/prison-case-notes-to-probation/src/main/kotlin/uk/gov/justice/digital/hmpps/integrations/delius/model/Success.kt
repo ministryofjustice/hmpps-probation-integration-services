017d2b337fe541520a2e7f219bdef1196f10a517
Amardeep Chimber <amardeep.chimber@digital.justice.gov.uk>
2025-05-19 11:14:56 +0100

    PI-3099: handle null noms id

--- projects/prison-case-notes-to-probation/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/model/Success.kt
+++ projects/prison-case-notes-to-probation/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/model/Success.kt
@@ -4,7 +4,7 @@ import uk.gov.justice.digital.hmpps.integrations.delius.model.MergeResult.Succes
 
 sealed interface MergeResult {
 
-    data class Success(val crn: String, val noms: String, val action: Action) : MergeResult
+    data class Success(val crn: String, val noms: String?, val action: Action) : MergeResult
     data class Failure(val exception: Exception) : MergeResult
 
     enum class Action {
@@ -12,5 +12,5 @@ sealed interface MergeResult {
     }
 }
 
-fun Success?.properties(): Map<String, String> =
+fun Success?.properties(): Map<String, String?> =
     if (this == null) emptyMap() else listOfNotNull("crn" to crn, "noms" to noms, "action" to action.name).toMap()
