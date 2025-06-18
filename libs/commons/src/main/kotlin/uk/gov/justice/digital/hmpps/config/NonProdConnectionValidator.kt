954a305364fa1be5cdcc888ee141bac96a4c8d45
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2025-06-18 13:12:25 +0100

    PI-2757 prevent starting up connection to preprod or prod with auto ddl set

---
+++ libs/commons/src/main/kotlin/uk/gov/justice/digital/hmpps/config/NonProdConnectionValidator.kt
@@ -0,0 +1,23 @@
+package uk.gov.justice.digital.hmpps.config
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties
+import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer
+import org.springframework.context.annotation.Configuration
+import uk.gov.justice.digital.hmpps.logging.Logger
+import uk.gov.justice.digital.hmpps.logging.Logger.logger
+
+@ConditionalOnProperty("spring.datasource.url")
+@Configuration
+class NonProdConnectionValidator(private val dsp: DataSourceProperties) : HibernatePropertiesCustomizer {
+    override fun customize(hibernateProperties: Map<String, Any>) {
+        if (dsp.isForPreprodOrProd()) {
+            Logger.logger().warn("Application connecting to ${dsp.url}")
+            require(hibernateProperties["hibernate.hbm2ddl.auto"] == "none") {
+                "Application attempted to connect to preprod or prod with auto ddl active."
+            }
+        }
+    }
+}
+
+private fun DataSourceProperties.isForPreprodOrProd() = url.contains("PRDNDA") or url.contains("PRENDA")
