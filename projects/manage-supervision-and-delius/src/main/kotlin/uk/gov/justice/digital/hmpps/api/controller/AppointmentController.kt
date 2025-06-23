35f67787663bb14c02d16c097b77c891684e0eb3
pmcphee77 <150798161+pmcphee77@users.noreply.github.com>
2025-06-23 13:09:37 +0100

    PI-3177: Add notes and sensitive flag to create appointment (#5159)
    PI-3177: Add notes and senstive flag to create appointment

--- projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/controller/AppointmentController.kt
+++ projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/controller/AppointmentController.kt
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*
 import uk.gov.justice.digital.hmpps.api.model.appointment.CheckAppointment
 import uk.gov.justice.digital.hmpps.api.model.appointment.CreateAppointment
 import uk.gov.justice.digital.hmpps.api.model.appointment.Outcome
+import uk.gov.justice.digital.hmpps.aspect.WithDeliusUser
 import uk.gov.justice.digital.hmpps.service.AppointmentOutcomeService
 import uk.gov.justice.digital.hmpps.service.AppointmentService
 import uk.gov.justice.digital.hmpps.service.SentenceAppointmentService
@@ -25,6 +26,7 @@ class AppointmentController(
 ) {
 
     @PostMapping("/{crn}")
+    @WithDeliusUser
     @ResponseStatus(HttpStatus.CREATED)
     fun createAppointment(@PathVariable crn: String, @Valid @RequestBody createAppointment: CreateAppointment) =
         sentenceAppointmentService.createAppointment(crn, createAppointment)
