2caaa7a4bc6afdccdecfcd6ddc1088d7564f4bbc
Marcus Aspin <maspin@unilink.com>
2025-06-27 11:08:46 +0100

    PI-3164 Exclude UPW projects relating to shift workers (#5151)
    Co-authored-by: probation-integration-bot[bot] <177347787+probation-integration-bot[bot]@users.noreply.github.com>

--- projects/appointment-reminders-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/UnpaidWorkAppointmentsService.kt
+++ projects/appointment-reminders-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/UnpaidWorkAppointmentsService.kt
@@ -1,9 +1,9 @@
 package uk.gov.justice.digital.hmpps.service
 
 import io.sentry.Sentry
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.logging.Logger.logger
-import uk.gov.justice.digital.hmpps.properties.UpwAppointmentRemindersJobProperties
 import uk.gov.justice.digital.hmpps.repository.UpwAppointmentRepository
 import uk.gov.justice.digital.hmpps.telemetry.TelemetryService
 import uk.gov.service.notify.NotificationClient
@@ -11,39 +11,43 @@ import java.time.LocalDate
 
 @Service
 class UnpaidWorkAppointmentsService(
-    private val properties: UpwAppointmentRemindersJobProperties,
     private val upwAppointmentRepository: UpwAppointmentRepository,
     private val notificationClient: NotificationClient,
     private val telemetryService: TelemetryService,
+    @Value("\${jobs.unpaid-work-appointment-reminders.excluded-crns:}") private val excludedCrns: List<String>,
+    @Value("\${jobs.unpaid-work-appointment-reminders.excluded-project-codes:}") private val excludedProjectCodes: List<String>,
 ) {
     fun sendUnpaidWorkAppointmentReminders(providerCode: String, templateIds: List<String>, daysInAdvance: Long) {
-        upwAppointmentRepository.getUnpaidWorkAppointments(LocalDate.now().plusDays(daysInAdvance), providerCode)
-            .forEach {
-                val telemetryProperties = mapOf(
-                    "crn" to it.crn,
-                    "providerCode" to providerCode,
-                    "templateIds" to templateIds.joinToString(),
-                    "upwAppointmentIds" to it.upwAppointmentIds,
-                )
-                if (it.crn !in properties.excludedCrns) {
-                    val responses = templateIds.map { templateId ->
-                        log.info("Sending SMS template $templateId to ${it.mobileNumber}")
-                        val templateValues = mapOf("FirstName" to it.firstName, "NextWorkSession" to it.appointmentDate)
-                        try {
-                            notificationClient.sendSms(templateId, it.mobileNumber, templateValues, it.crn)
-                        } catch (e: Exception) {
-                            telemetryService.trackEvent("UnpaidWorkAppointmentReminderFailure", telemetryProperties)
-                            telemetryService.trackException(e, telemetryProperties)
-                            Sentry.captureException(e)
-                            return@forEach
-                        }
+        upwAppointmentRepository.getUnpaidWorkAppointments(
+            LocalDate.now().plusDays(daysInAdvance),
+            providerCode,
+            excludedProjectCodes
+        ).forEach {
+            val telemetryProperties = mapOf(
+                "crn" to it.crn,
+                "providerCode" to providerCode,
+                "templateIds" to templateIds.joinToString(),
+                "upwAppointmentIds" to it.upwAppointmentIds,
+            )
+            if (it.crn !in excludedCrns) {
+                val responses = templateIds.map { templateId ->
+                    log.info("Sending SMS template $templateId to ${it.mobileNumber}")
+                    val templateValues = mapOf("FirstName" to it.firstName, "NextWorkSession" to it.appointmentDate)
+                    try {
+                        notificationClient.sendSms(templateId, it.mobileNumber, templateValues, it.crn)
+                    } catch (e: Exception) {
+                        telemetryService.trackEvent("UnpaidWorkAppointmentReminderFailure", telemetryProperties)
+                        telemetryService.trackException(e, telemetryProperties)
+                        Sentry.captureException(e)
+                        return@forEach
                     }
-                    telemetryService.trackEvent(
-                        "UnpaidWorkAppointmentReminderSent",
-                        telemetryProperties + mapOf("notificationIds" to responses.joinToString { response -> response?.notificationId.toString() })
-                    )
-                } else telemetryService.trackEvent("UnpaidWorkAppointmentReminderNotSent", telemetryProperties)
-            }
+                }
+                telemetryService.trackEvent(
+                    "UnpaidWorkAppointmentReminderSent",
+                    telemetryProperties + mapOf("notificationIds" to responses.joinToString { response -> response?.notificationId.toString() })
+                )
+            } else telemetryService.trackEvent("UnpaidWorkAppointmentReminderNotSent", telemetryProperties)
+        }
     }
 
     companion object {
