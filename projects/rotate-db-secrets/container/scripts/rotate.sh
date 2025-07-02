4b8f10903b0f9a1ead503df8ed142ed1c436be0c
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2025-07-02 12:01:15 +0100

    PI-3153: Cron Job for DB secret rotation

--- projects/rotate-db-secrets/container/scripts/rotate.sh
+++ projects/rotate-db-secrets/container/scripts/rotate.sh
@@ -1,30 +1,37 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-[ -z "$PROJECT_NAME" ] && echo "Missing PROJECT_NAME" && exit 1
 [ -z "$NAMESPACE" ] && echo "Missing NAMESPACE" && exit 1
 
 COMMON_STRING=$(kubectl --namespace=$NAMESPACE get secret common -o json \
 | jq -r '.data | map_values(@base64d) | to_entries[] | "\(.key)=\(.value)"' | grep DB_URL)
 
-PROJECT_UN_STRING=$(kubectl --namespace=$NAMESPACE get secret $PROJECT_NAME-database -o json \
-| jq -r '.data | map_values(@base64d) | to_entries[] | "\(.key)=\(.value)"'  | grep DB_USERNAME)
+PROJECTS=$(kubectl --namespace=hmpps-probation-integration-services-dev get secrets -o name | grep "\-database$" | sed "s/secret\///g" | sed "s/-database//g")
 
-PROJECT_PW_STRING=$(kubectl --namespace=$NAMESPACE get secret $PROJECT_NAME-database -o json \
-| jq -r '.data | map_values(@base64d) | to_entries[] | "\(.key)=\(.value)"'  | grep DB_PASSWORD)
+for PROJECT_NAME in "${PROJECTS[@]}"
+do
+  echo "$PROJECT"
+  PROJECT_UN_STRING=$(kubectl --namespace=$NAMESPACE get secret $PROJECT_NAME-database -o json \
+  | jq -r '.data | map_values(@base64d) | to_entries[] | "\(.key)=\(.value)"'  | grep DB_USERNAME)
 
-DB_URL=$(echo $COMMON_STRING | sed 's/DB_URL=jdbc:oracle:thin://g')
-DB_UN=$(echo $PROJECT_UN_STRING | sed 's/DB_USERNAME=//g')
-DB_PW=$(echo $PROJECT_PW_STRING | sed 's/DB_PASSWORD=//g')
-CONN_STR="$DB_UN/$DB_PW$DB_URL"
-POD_NAME="rotate-$PROJECT_NAME"
+  PROJECT_PW_STRING=$(kubectl --namespace=$NAMESPACE get secret $PROJECT_NAME-database -o json \
+  | jq -r '.data | map_values(@base64d) | to_entries[] | "\(.key)=\(.value)"'  | grep DB_PASSWORD)
 
-SQL="select count(*) from offender o;"
+  DB_URL=$(echo $COMMON_STRING | sed 's/DB_URL=jdbc:oracle:thin://g')
+  DB_UN=$(echo $PROJECT_UN_STRING | sed 's/DB_USERNAME=//g')
+  DB_PW=$(echo $PROJECT_PW_STRING | sed 's/DB_PASSWORD=//g')
+  CONN_STR="$DB_UN/$DB_PW$DB_URL"
+  POD_NAME="rotate-$PROJECT_NAME"
 
-#echo "Rotation pod is ready"
-#echo "Executing command"
-echo "$SQL" | sqlplus -s "$CONN_STR"
-#echo "Command successful"
+  SQL="select count(*) from offender o;"
+
+  #echo "Rotation pod is ready"
+  #echo "Executing command"
+  echo "$SQL" | sqlplus -s "$CONN_STR"
+  #echo "Command successful"
+done
+
+
 
 
 
