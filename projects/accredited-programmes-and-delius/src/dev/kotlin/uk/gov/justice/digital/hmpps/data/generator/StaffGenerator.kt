f2ec3c18e8da0b7c1e7360ed328e4744537d519a
Marcus Aspin <maspin@unilink.com>
2025-07-11 17:15:16 +0100

    PI-3271 Add staff code to accredited programmes response (#5253)

--- projects/accredited-programmes-and-delius/src/dev/kotlin/uk/gov/justice/digital/hmpps/data/generator/StaffGenerator.kt
+++ projects/accredited-programmes-and-delius/src/dev/kotlin/uk/gov/justice/digital/hmpps/data/generator/StaffGenerator.kt
@@ -1,11 +1,13 @@
 package uk.gov.justice.digital.hmpps.data.generator
 
+import uk.gov.justice.digital.hmpps.data.generator.IdGenerator.id
 import uk.gov.justice.digital.hmpps.entity.Staff
 import uk.gov.justice.digital.hmpps.entity.User
 
 object StaffGenerator {
-    fun generate(user: User? = null) = Staff(
-        id = IdGenerator.getAndIncrement(),
+    fun generate(code: String = "STAFF01", user: User? = null) = Staff(
+        id = id(),
+        code = code,
         forename = "Forename",
         surname = "Surname",
         user = user
--- projects/accredited-programmes-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/model/ProbationPractitioner.kt
+++ projects/accredited-programmes-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/model/ProbationPractitioner.kt
@@ -2,5 +2,6 @@ package uk.gov.justice.digital.hmpps.model
 
 data class ProbationPractitioner(
     val name: Name,
+    val code: String,
     val email: String?
 )
