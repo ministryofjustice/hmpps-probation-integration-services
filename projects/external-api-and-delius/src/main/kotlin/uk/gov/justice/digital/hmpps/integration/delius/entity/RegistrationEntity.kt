9ce077f68df76ad938ba5c76040e06277a8d78e6
pmcphee77 <150798161+pmcphee77@users.noreply.github.com>
2025-07-23 16:15:01 +0100

    HIA-840: Return ADDRESS STATUS (as ADDRESS_TYPE) reference data (#5287)

--- projects/external-api-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integration/delius/entity/RegistrationEntity.kt
+++ projects/external-api-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integration/delius/entity/RegistrationEntity.kt
@@ -94,10 +94,16 @@ interface RegistrationRepository : JpaRepository<RegistrationEntity, Long> {
 
     @Query(
         """
-        select rdm.code_set_name as codeSet, rdl.code_value as code, rdl.code_description as description
+        select 
+        case
+            when rdm.code_set_name = 'ADDRESS STATUS' then 'ADDRESS_TYPE'
+            else rdm.code_set_name
+        end as codeSet,
+        rdl.code_value as code, 
+        rdl.code_description as description
         from r_standard_reference_list rdl join r_reference_data_master rdm 
         on rdm.reference_data_master_id = rdl.reference_data_master_id
-        where rdm.code_set_name in ('GENDER','ETHNICITY')
+        where rdm.code_set_name in ('GENDER','ETHNICITY','ADDRESS STATUS')
         union
         select 'REGISTER_TYPE' as set_name, rt.code, rt.description 
         from r_register_type rt
