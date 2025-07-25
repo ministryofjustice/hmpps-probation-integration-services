019f43967a7906d02292a6045a8ed158ea90f327
Anthony Britton <105213050+anthony-britton-moj@users.noreply.github.com>
2025-07-25 13:56:32 +0100

    PI-3187 (#5289)
    Co-authored-by: probation-integration-bot[bot] <177347787+probation-integration-bot[bot]@users.noreply.github.com>

--- projects/manage-pom-cases-and-delius/src/dev/kotlin/uk/gov/justice/digital/hmpps/data/generator/ProviderGenerator.kt
+++ projects/manage-pom-cases-and-delius/src/dev/kotlin/uk/gov/justice/digital/hmpps/data/generator/ProviderGenerator.kt
@@ -1,6 +1,7 @@
 package uk.gov.justice.digital.hmpps.data.generator
 
 import uk.gov.justice.digital.hmpps.integrations.delius.provider.entity.*
+import uk.gov.justice.digital.hmpps.integrations.delius.reference.entity.ReferenceData
 import uk.gov.justice.digital.hmpps.set
 import java.time.ZonedDateTime
 
@@ -13,6 +14,9 @@ object ProviderGenerator {
     val UNALLOCATED_TEAM = generateTeam("N03ALL", district = DEFAULT_DISTRICT)
     val DEFAULT_STAFF = generateStaff("N03DEF0", "Default", "Staff")
     val UNALLOCATED_STAFF = generateStaff("N03ALLU", "Unallocated", "Staff")
+    val FUTURE_POM_STAFF = generateStaff("N03POMF", "Future", "POM")
+    val FUTURE_POM = generatePom(PersonGenerator.DEFAULT.id, ZonedDateTime.now().plusDays(3))
+    val FUTURE_RO = generateResponsibleOfficer(PersonGenerator.DEFAULT.id, FUTURE_POM, ZonedDateTime.now().plusDays(3))
 
     fun generateProvider(providerCode: String, prisonCode: String?, id: Long = IdGenerator.getAndIncrement()) =
         ProbationArea(
@@ -41,4 +45,19 @@ object ProviderGenerator {
         user: StaffUser? = null,
         startDate: ZonedDateTime = ZonedDateTime.now(),
     ) = Staff(code, forename, surname, middleName, user, providerId, startDate).apply { user?.set("staff", this) }
+
+    fun generatePom(
+        personId: Long,
+        date: ZonedDateTime,
+        allocationReason: ReferenceData = ReferenceDataGenerator.ALLOCATION_AUTO,
+        staff: Staff = FUTURE_POM_STAFF,
+        team: Team = POM_TEAM,
+        probationArea: ProbationArea = DEFAULT_PROVIDER,
+    ) = PrisonManager(0, 0, personId, date, allocationReason, staff, team, probationArea)
+
+    fun generateResponsibleOfficer(
+        personId: Long,
+        prisonManager: PrisonManager,
+        startDate: ZonedDateTime,
+    ) = ResponsibleOfficer(personId, prisonManager, startDate)
 }
