f14447a815536f11f52d50ecab5cdf7642619123
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2025-07-30 13:58:42 +0100

    PI-3153: script change

--- projects/rotate-db-secrets/container/scripts/rotate.sh
+++ projects/rotate-db-secrets/container/scripts/rotate.sh
@@ -9,7 +9,10 @@ COMMON_STRING=$(kubectl --namespace=$NAMESPACE get secret common -o json \
 
 for PROJECT_NAME in $(kubectl --namespace=$NAMESPACE get secrets -o name | grep "\-database$" | sed "s/secret\///g" | sed "s/-database//g")
 do
+  PROJECT_NAME="find-and-refer-and-delius"
   echo "Updating DB secret for $PROJECT_NAME"
+
+  NEW_SECRET=$(pwgen -N1 16)
 
   PROJECT_UN_STRING=$(kubectl --namespace=$NAMESPACE get secret "$PROJECT_NAME-database" -o json \
   | jq -r '.data | map_values(@base64d) | to_entries[] | "\(.key)=\(.value)"'  | grep DB_USERNAME)
@@ -22,9 +25,15 @@ do
   DB_PW=$(echo $PROJECT_PW_STRING | sed 's/DB_PASSWORD=//g')
   CONN_STR="$DB_UN/$DB_PW$DB_URL"
 
-  SQL="select count(*) from offender o;"
+  SQL="select count(*) from offender o;/
+       select count(*) from alias a;/
+  "
 
   echo "$SQL" | sqlplus -s "$CONN_STR"
+
+  break
+
+  #kubectl rollout restart "deployment/$PROJECT_NAME" -n hmpps-probation-integration-services-dev
 
 done
 
