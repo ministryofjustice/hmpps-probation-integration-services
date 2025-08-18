d5cb2d6fb2d31cb3e536d5959ef4cb8daaf52709
James MacBeth <56261600+jamesmacbeth-unilink@users.noreply.github.com>
2025-08-18 09:03:53 +0100

    PI-3330 - Information page endpoint for Suicide Risk Form and Delius. (#5359)
    Co-authored-by: probation-integration-bot[bot] <177347787+probation-integration-bot[bot]@users.noreply.github.com>

--- projects/suicide-risk-form-and-delius/src/dev/kotlin/uk/gov/justice/digital/hmpps/data/generator/ReferenceDataGenerator.kt
+++ projects/suicide-risk-form-and-delius/src/dev/kotlin/uk/gov/justice/digital/hmpps/data/generator/ReferenceDataGenerator.kt
@@ -4,6 +4,34 @@ import uk.gov.justice.digital.hmpps.integrations.delius.Dataset
 import uk.gov.justice.digital.hmpps.integrations.delius.ReferenceData
 
 object ReferenceDataGenerator {
+    val REGISTER_LEVEL_DATASET = generateDataset("REGISTER LEVEL", IdGenerator.getAndIncrement())
+
+    val HIGH_RISK_REGISTER_LEVEL = generateReferenceData(
+        dataset = REGISTER_LEVEL_DATASET,
+        code = "1",
+        description = "High Risk",
+        selectable = true,
+        id = IdGenerator.getAndIncrement()
+    )
+
+    val REGISTER_TYPE_FLAG_DATASET = generateDataset("REGISTER TYPE FLAG", IdGenerator.getAndIncrement())
+
+    val SAFEGUARDING_FLAG = generateReferenceData(
+        dataset = REGISTER_TYPE_FLAG_DATASET,
+        code = "3",
+        description = "Safeguarding",
+        selectable = true,
+        id = IdGenerator.getAndIncrement()
+    )
+
+    val INFORMATION_FLAG = generateReferenceData(
+        dataset = REGISTER_TYPE_FLAG_DATASET,
+        code = "4",
+        description = "Information",
+        selectable = true,
+        id = IdGenerator.getAndIncrement()
+    )
+
     fun generateDataset(code: String, id: Long = IdGenerator.getAndIncrement()) = Dataset(code, id)
 
     fun generateReferenceData(
--- projects/suicide-risk-form-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/controller/SuicideRiskFormController.kt
+++ projects/suicide-risk-form-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/controller/SuicideRiskFormController.kt
@@ -5,11 +5,22 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.model.BasicDetails
+import uk.gov.justice.digital.hmpps.model.InformationPageResponse
 import uk.gov.justice.digital.hmpps.service.DetailsService
+import uk.gov.justice.digital.hmpps.service.RegistrationsService
 
 @RestController
-class SuicideRiskFormController(private val detailsService: DetailsService) {
+class SuicideRiskFormController(
+    private val detailsService: DetailsService,
+    private val registrationsService: RegistrationsService
+) {
+
     @PreAuthorize("hasRole('PROBATION_API__SUICIDE_RISK_FORM__CASE_DETAIL')")
     @GetMapping(value = ["/basic-details/{crn}"])
     fun getBasicDetails(@PathVariable crn: String): BasicDetails = detailsService.basicDetails(crn)
+
+    @PreAuthorize("hasRole('PROBATION_API__SUICIDE_RISK_FORM__CASE_DETAIL')")
+    @GetMapping(value = ["/information-page/{crn}"])
+    fun getInformationPage(@PathVariable crn: String): InformationPageResponse =
+        registrationsService.informationPage(crn)
 }
