f7def3d758e8f46817c894b6cd3232164d6b7f35
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2025-08-26 14:41:11 +0100

    PI-3364 listen for e supervision received and expired events

---
+++ projects/esupervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/ESupervisionService.kt
@@ -0,0 +1,47 @@
+package uk.gov.justice.digital.hmpps
+
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import uk.gov.justice.digital.hmpps.audit.service.AuditableService
+import uk.gov.justice.digital.hmpps.audit.service.AuditedInteractionService
+import uk.gov.justice.digital.hmpps.integrations.delius.*
+import uk.gov.justice.digital.hmpps.integrations.delius.audit.BusinessInteractionCode.ADD_CONTACT
+import uk.gov.justice.digital.hmpps.message.HmppsDomainEvent
+import uk.gov.justice.digital.hmpps.messaging.checkInUrl
+import uk.gov.justice.digital.hmpps.messaging.description
+
+@Transactional
+@Service
+class ESupervisionService(
+    auditedInteractionService: AuditedInteractionService,
+    private val personManagerRepository: PersonManagerRepository,
+    private val eventRepository: EventRepository,
+    private val contactTypeRepository: ContactTypeRepository,
+    private val contactRepository: ContactRepository,
+) : AuditableService(auditedInteractionService) {
+    fun handle(de: HmppsDomainEvent) = audit(ADD_CONTACT) { audit ->
+        val crn = requireNotNull(de.personReference.findCrn())
+        val com = personManagerRepository.getByCrn(crn)
+        audit["offenderId"] = com.person.id
+        val event = eventRepository.findFirstByPersonCrnOrderByReferralDateDesc(crn)
+            ?: throw IllegalStateException("Case does not have an active event")
+        audit["eventId"] = event.id
+        val contact = contactRepository.save(de.createContact(com, event))
+        audit["contactId"] = contact.id
+    }
+
+    private fun HmppsDomainEvent.createContact(com: PersonManager, event: Event): Contact = Contact(
+        com.person,
+        event,
+        contactTypeRepository.getByCode(ContactType.E_SUPERVISION_CHECK_IN),
+        occurredAt.toLocalDate(),
+        occurredAt,
+        com.provider,
+        com.team,
+        com.staff,
+        description(),
+        "For more information please visit ${checkInUrl()}",
+        false,
+        0
+    )
+}
