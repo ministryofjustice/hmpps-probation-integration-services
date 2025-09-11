42fd2cd76feadd1594e1c7d31152f8e75cb9935a
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2025-09-11 14:25:13 +0100

    PI-3429

--- projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/audit/DeliusUserAware.kt
+++ projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/audit/DeliusUserAware.kt
@@ -1,6 +1,6 @@
 package uk.gov.justice.digital.hmpps.integrations.delius.audit
 
-import org.springframework.context.annotation.Primary
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.data.domain.AuditorAware
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.stereotype.Component
@@ -8,7 +8,7 @@ import uk.gov.justice.digital.hmpps.aspect.UserContext
 import uk.gov.justice.digital.hmpps.security.ServiceContext
 import java.util.*
 
-@Primary
+@ConditionalOnProperty("use-delius-user", havingValue = "true", matchIfMissing = false, prefix = "service")
 @EnableJpaAuditing(auditorAwareRef = "deliusUserAware", dateTimeProviderRef = "auditingDateTimeProvider")
 @Component(value = "deliusUserAware")
 class DeliusUserAware : AuditorAware<Long> {
