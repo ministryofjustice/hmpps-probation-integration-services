5c278c3ac16aa4209168ab3dccf3aaf9eb709d77
James MacBeth <56261600+jamesmacbeth-unilink@users.noreply.github.com>
2025-09-18 15:32:59 +0100

    PI-3431 - Created Treatment page endpoint to get documents linked to contacts (#5476)
    Co-authored-by: probation-integration-bot[bot] <177347787+probation-integration-bot[bot]@users.noreply.github.com>

--- projects/suicide-risk-form-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/Document.kt
+++ projects/suicide-risk-form-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/Document.kt
@@ -60,4 +60,6 @@ interface DocumentRepository : JpaRepository<Document, Long> {
     fun findByExternalReference(urn: String): Document?
 
     fun existsByTableNameAndPrimaryKeyIdAndIdNot(tableName: String, primaryKeyId: Long, id: Long): Boolean
+
+    fun findByTableNameAndPrimaryKeyIdIn(tableName: String, primaryKeyId: List<Long>): List<Document>
 }
