ce28b437e25cb3ebe1035fcc694556f241485be0
James MacBeth <56261600+jamesmacbeth-unilink@users.noreply.github.com>
2025-09-18 16:04:17 +0100

    PI-3434 - Case endpoint for Suicide Risk Form (#5479)

--- projects/suicide-risk-form-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/controller/SuicideRiskFormController.kt
+++ projects/suicide-risk-form-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/controller/SuicideRiskFormController.kt
@@ -4,11 +4,13 @@ import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
 import uk.gov.justice.digital.hmpps.model.BasicDetails
 import uk.gov.justice.digital.hmpps.model.ContactDocumentResponse
+import uk.gov.justice.digital.hmpps.model.DocumentCrn
 import uk.gov.justice.digital.hmpps.model.InformationPageResponse
 import uk.gov.justice.digital.hmpps.model.SignAndSendResponse
 import uk.gov.justice.digital.hmpps.service.DetailsService
 import uk.gov.justice.digital.hmpps.service.DocumentService
 import uk.gov.justice.digital.hmpps.service.RegistrationsService
+import java.util.*
 
 @RestController
 class SuicideRiskFormController(
@@ -29,6 +31,11 @@ class SuicideRiskFormController(
     @PreAuthorize("hasRole('PROBATION_API__SUICIDE_RISK_FORM__CASE_DETAIL')")
     @GetMapping(value = ["/sign-and-send/{username}"])
     fun getSignAndSend(@PathVariable username: String): SignAndSendResponse = detailsService.signAndSend(username)
+
+    @PreAuthorize("hasRole('PROBATION_API__SUICIDE_RISK_FORM__CASE_DETAIL')")
+    @GetMapping(value = ["/case/{suicideRiskFormId}"])
+    fun findCrnForSuicideRiskForm(@PathVariable suicideRiskFormId: UUID): DocumentCrn =
+        detailsService.crnFor(suicideRiskFormId)
 
     @PreAuthorize("hasRole('PROBATION_API__SUICIDE_RISK_FORM__CASE_DETAIL')")
     @PostMapping(value = ["/treatment"])
