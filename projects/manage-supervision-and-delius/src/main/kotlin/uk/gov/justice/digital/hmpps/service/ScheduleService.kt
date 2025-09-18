2ef78c5be20270232d3cd0d3d5f71377bc31544b
Marcus Aspin <maspin@unilink.com>
2025-09-18 16:04:42 +0100

    PI-3461 Flag RAR contacts as Delius-managed

--- projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/ScheduleService.kt
+++ projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/ScheduleService.kt
@@ -191,7 +191,7 @@ fun Contact.toActivity(noteId: Int? = null) = Activity(
     eventNumber = event?.eventNumber,
     description = description,
     outcome = outcome?.description,
-    deliusManaged = CreateAppointment.Type.entries.none { it.code == type.code } || complied == false,
+    deliusManaged = CreateAppointment.Type.entries.none { it.code == type.code } || complied == false || requirement?.mainCategory?.code == "F",
     isVisor = isVisor,
     eventId = event?.id,
     component = requirement?.asComponent() ?: licenceCondition?.asComponent(),
