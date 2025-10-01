27628801a2a1f2e6d4c9c24cdaae2279a376d43a
Marcus Aspin <maspin@unilink.com>
2025-10-01 14:50:01 +0100

    Use version catalog for buildSrc + fix commit message

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,16 +1,16 @@
 plugins {
-    id("com.google.cloud.tools.jib") version "3.4.5" apply false
-    id("org.sonarqube") version "6.3.1.5724" apply false
+    alias(build.plugins.jib) apply false
+    alias(build.plugins.sonarqube) apply false
     `kotlin-dsl`
+}
+
+dependencies {
+    implementation(build.jib)
+    implementation(build.sonarqube)
 }
 
 repositories {
     mavenLocal()
     mavenCentral()
     gradlePluginPortal()
-}
-
-dependencies {
-    implementation("com.google.cloud.tools:jib-gradle-plugin:3.4.4")
-    implementation("org.sonarsource.scanner.gradle:sonarqube-gradle-plugin:6.0.1.5171")
 }
