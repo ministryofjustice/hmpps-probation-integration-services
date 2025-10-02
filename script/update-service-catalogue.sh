ffc59085df7da8a4a2b66715198c4f8cdf19c294
Marcus Aspin <maspin@unilink.com>
2025-10-02 13:27:36 +0100

    PI-3427 Update to use latest service catalogue API

--- script/update-service-catalogue.sh
+++ script/update-service-catalogue.sh
@@ -12,16 +12,25 @@ set -euo pipefail
 
 api_url="https://service-catalogue.hmpps.service.justice.gov.uk/v1"
 product_code="HMPPS518"
-product_id=$(curl -fsS -H "Authorization: Bearer $SERVICE_CATALOGUE_API_KEY" "$api_url/products?filters\[p_id\]=$product_code" | jq -r '.data[0].id // ""')
-component=$(curl -fsS -H "Authorization: Bearer $SERVICE_CATALOGUE_API_KEY" "$api_url/components?filters\[name\]=$PROJECT_NAME")
+product_id=$(curl -fsS -H "Authorization: Bearer $SERVICE_CATALOGUE_API_KEY" "$api_url/products?filters\[p_id\]=$product_code" | jq -r '.data[0].documentId // ""')
+component=$(curl -fsS -H "Authorization: Bearer $SERVICE_CATALOGUE_API_KEY" "$api_url/components?filters\[name\]=$PROJECT_NAME&populate=envs")
 
 # Get ids for existing environments
 ENVIRONMENTS=${ENVIRONMENTS:-[]}
+ENVIRONMENT_IDS=[]
 for env in dev preprod prod; do
-  env_id=$(echo "$component" | jq -r '.data[0].environments | select(.name == $env) | .id' --arg env "$env")
-  namespace_id=$(curl -fsS -H "Authorization: Bearer $SERVICE_CATALOGUE_API_KEY" "$api_url/namespaces?filters\[name\]=hmpps-probation-integration-services-$env" | jq '.data[0].id')
-  if [ -n "$env_id" ]; then ENVIRONMENTS=$(echo "$ENVIRONMENTS" | jq -r 'map(select(.name == $env) += {"id": $env_id})' --arg env "$env" --arg env_id "$env_id"); fi
-  if [ -n "$namespace_id" ]; then ENVIRONMENTS=$(echo "$ENVIRONMENTS" | jq -r 'map(select(.name == $env) += {"ns": $namespace_id})' --arg env "$env" --arg namespace_id "$namespace_id"); fi
+  env_data=$(echo "$ENVIRONMENTS" | jq -r '.[] | select(.name == $env) | {"data": .}' --arg env "$env")
+  if [ -n "$env_data" ]; then
+    env_id=$(echo "$component" | jq -r '.data[0].envs // [] | .[] | select(.name == $env) | .documentId // ""' --arg env "$env")
+    if [ -z "$env_id" ]; then
+      echo "Adding $env environment to hmpps-service-catalogue"
+      env_id=$(curl -XPOST -fsS -H "Authorization: Bearer $SERVICE_CATALOGUE_API_KEY" "$api_url/environments" --json "$env_data" | jq -r '.data.documentId')
+    else
+      echo "Updating $env environment ($env_id) in hmpps-service-catalogue"
+      curl -XPUT -fsS -H "Authorization: Bearer $SERVICE_CATALOGUE_API_KEY" "$api_url/environments/$env_id" --json "$env_data"
+    fi
+    ENVIRONMENT_IDS=$(echo "$ENVIRONMENT_IDS" | jq -r '. += [{"documentId": $env_id}]' --arg env_id "$env_id")
+  fi
 done
 
 data=$(jq -n '{"data": $ARGS.named}' \
@@ -32,11 +41,11 @@ data=$(jq -n '{"data": $ARGS.named}' \
     --arg part_of_monorepo true \
     --arg path_to_project "projects/$PROJECT_NAME" \
     --arg path_to_helm_dir "projects/$PROJECT_NAME/deploy" \
-    --argjson environments "$ENVIRONMENTS" \
+    --argjson envs "$(echo "$ENVIRONMENT_IDS" | jq '{"set": .}')" \
     --argjson jira_project_keys '["PI"]'
 )
 
-component_id=$(echo "$component" | jq -r '.data[0].id // ""')
+component_id=$(echo "$component" | jq -r '.data[0].documentId // ""')
 if [ -z "$component_id" ]; then
     echo "Adding component $PROJECT_NAME to hmpps-service-catalogue"
     curl -XPOST -fsS -H "Authorization: Bearer $SERVICE_CATALOGUE_API_KEY" "$api_url/components" --json "$data"
