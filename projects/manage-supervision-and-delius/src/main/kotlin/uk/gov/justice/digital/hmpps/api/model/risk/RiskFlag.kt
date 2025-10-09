797a93f37436bce5612aef6afb16827db77ada46
Marcus Aspin <maspin@unilink.com>
2025-10-09 10:59:00 +0100

    PI-3514 Ignore unexpected register type colours

--- projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/model/risk/RiskFlag.kt
+++ projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/model/risk/RiskFlag.kt
@@ -7,7 +7,7 @@ import java.time.LocalDate
 data class RiskFlag(
     val id: Long,
     val description: String,
-    val level: RiskLevel,
+    val level: RiskLevel?,
     val levelCode: String?,
     val levelDescription: String?,
     val riskNotes: List<NoteDetail>? = null,
@@ -24,8 +24,8 @@ enum class RiskLevel(val severity: Int, val value: String) {
     HIGH(4, "RED"), MEDIUM(3, "AMBER"), LOW(2, "GREEN"), INFORMATION_ONLY(1, "WHITE");
 
     companion object {
-        fun fromString(enumValue: String): RiskLevel {
-            return entries.first { it.value == enumValue.uppercase() }
+        fun fromString(enumValue: String): RiskLevel? {
+            return entries.firstOrNull { it.value == enumValue.uppercase() }
         }
     }
 }
