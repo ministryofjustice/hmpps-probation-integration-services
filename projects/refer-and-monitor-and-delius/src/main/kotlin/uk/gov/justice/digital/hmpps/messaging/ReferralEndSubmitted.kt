81ded7013692fbee1a968aa86a1b4ec61ff5b870
Marcus Aspin <maspin@unilink.com>
2025-10-15 11:29:56 +0100

    PI-3532 Remove CRN + URN from telemetry event name

--- projects/refer-and-monitor-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/messaging/ReferralEndSubmitted.kt
+++ projects/refer-and-monitor-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/messaging/ReferralEndSubmitted.kt
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.annotation.JsonAlias
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.config.security.nullIfNotFound
 import uk.gov.justice.digital.hmpps.detail.DomainEventDetailService
-import uk.gov.justice.digital.hmpps.exception.IgnorableMessageException
 import uk.gov.justice.digital.hmpps.exception.IgnorableMessageException.Companion.orIgnore
 import uk.gov.justice.digital.hmpps.exception.NotFoundException
 import uk.gov.justice.digital.hmpps.message.HmppsDomainEvent
@@ -33,7 +32,7 @@ class ReferralEndSubmitted(
             event.referralUrn(),
             sentReferral.relevantSentenceId,
             sentReferral.sentAt,
-            sentReferral.endDate.orIgnore { "No End Date for Termination: ${event.referralUrn()} => ${event.personReference.findCrn()}" },
+            sentReferral.endDate.orIgnore { "No end date for termination" },
             ReferralEndType.valueOf(event.deliveryState()),
             sentReferral.withdrawalCode?.toOutcome(sentReferral.withdrawalState),
             sentReferral.notes(event.referralUiUrl()),
