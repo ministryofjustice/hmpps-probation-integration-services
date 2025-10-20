9f0c39f965106763d4123965e309c02e45961a05
james.macbeth <james.macbeth@justice.gov.uk>
2025-10-20 15:30:19 +0100

    PI-3516 - Changed column names to lower case for consistency

--- projects/common-platform-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/entity/Person.kt
+++ projects/common-platform-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/entity/Person.kt
@@ -93,22 +93,22 @@ class Person(
     @Column(name = "current_remand_status")
     var remandStatus: String? = null,
 
-    @Column(name = "OFFENDER_DETAILS", columnDefinition = "clob")
+    @Column(name = "offender_details", columnDefinition = "clob")
     var notes: String? = null,
 
-    @Column(name = "DEFENDANT_ID")
+    @Column(name = "defendant_id")
     var defendantId: String? = null,
 
     @ManyToOne
-    @JoinColumn(name = "NATIONALITY_ID")
+    @JoinColumn(name = "nationality_id")
     var nationality: ReferenceData? = null,
 
     @ManyToOne
-    @JoinColumn(name = "SECOND_NATIONALITY_ID")
+    @JoinColumn(name = "second_nationality_id")
     var secondNationality: ReferenceData? = null,
 
     @ManyToOne
-    @JoinColumn(name = "ETHNICITY_ID")
+    @JoinColumn(name = "ethnicity_id")
     var ethnicity: ReferenceData? = null,
 
     @Column
