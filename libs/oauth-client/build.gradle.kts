ad9b948a6dea5b85d6c83398b0a09d44582429cd
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2025-10-23 17:43:09 +0100

    Use Jetty Http Client

--- libs/oauth-client/build.gradle.kts
+++ libs/oauth-client/build.gradle.kts
@@ -4,6 +4,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-security")
 
     api("org.springframework.boot:spring-boot-starter-oauth2-client")
+    api("org.eclipse.jetty:jetty-client:12.1.3")
 }
 
 configure<uk.gov.justice.digital.hmpps.extensions.ClassPathExtension> {
