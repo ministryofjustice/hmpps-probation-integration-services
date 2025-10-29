5ce30145fa10126feb0a2859c4000f850c937faf
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2025-10-29 13:05:59 +0000

    make staff code optional

--- projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/model/user/Provider.kt
+++ projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/model/user/Provider.kt
@@ -16,7 +16,7 @@ data class UserProviderResponse(
 
 data class DefaultUserDetails(
     val username: String,
-    val staffCode: String,
+    val staffCode: String?,
     val homeArea: String,
     val team: String?
 )
