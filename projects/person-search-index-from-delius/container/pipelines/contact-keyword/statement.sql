b38e56d5ee25e2d1693f9e427adb7508a90eccaf
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2025-11-03 13:52:22 +0000

    PI-3577 attended and complied only apply to attendance types

--- projects/person-search-index-from-delius/container/pipelines/contact-keyword/statement.sql
+++ projects/person-search-index-from-delius/container/pipelines/contact-keyword/statement.sql
@@ -17,8 +17,8 @@ from (with page as (select * from contact where :contact_id = 0
                      'endTime' value to_char(contact.contact_end_time, 'HH24:MI:SS'),
                      'description' value contact.description,
                      'notes' value contact.notes,
-                     'attended' value decode(contact.attended, 'Y', 'attended', 'N', 'fta', null),
-                     'complied' value decode(contact.complied, 'Y', 'complied', 'N', 'ftc', null),
+                     'attended' value case when r_contact_type.attendance_contact = 'Y' then decode(contact.attended, 'Y', 'attended', 'N', 'fta', null) end,
+                     'complied' value case when r_contact_type.attendance_contact = 'Y' then decode(contact.complied, 'Y', 'complied', 'N', 'ftc', null) end,
                      'startDateTime' value decode(contact.contact_start_time, null, to_char(contact.contact_date, 'yyyy-mm-dd') || 'T00:00:00', to_char(contact.contact_date, 'yyyy-mm-dd') || 'T' || to_char(contact.contact_start_time, 'hh24:mi:ss')),
                      'endDateTime' value  decode(contact.contact_end_time, null, null, to_char(contact.contact_date, 'yyyy-mm-dd') || 'T' || to_char(contact.contact_end_time, 'hh24:mi:ss')),
                      'requiresOutcome' value case when exists (select cto.contact_outcome_type_id
