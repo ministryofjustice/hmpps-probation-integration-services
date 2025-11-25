365a85bda2b9111e1be88caf7b50a02089f13b56
sam.bailey <161320275+sambailey-moj@users.noreply.github.com>
2025-11-25 14:35:37 +0000

    PI-3636 - adding in check for functional PDUs

--- projects/accredited-programmes-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/entity/staff/ProbationDeliveryUnit.kt
+++ projects/accredited-programmes-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/entity/staff/ProbationDeliveryUnit.kt
@@ -1,17 +1,15 @@
 package uk.gov.justice.digital.hmpps.entity.staff
 
-import jakarta.persistence.Column
-import jakarta.persistence.Convert
-import jakarta.persistence.Entity
-import jakarta.persistence.Id
-import jakarta.persistence.Table
+import jakarta.persistence.*
 import org.hibernate.annotations.Immutable
+import org.hibernate.annotations.SQLRestriction
 import org.hibernate.type.YesNoConverter
 import uk.gov.justice.digital.hmpps.model.CodedValue
 
 @Entity
 @Immutable
 @Table(name = "borough")
+@SQLRestriction("functional = 'Y'")
 class ProbationDeliveryUnit(
     @Id
     @Column(name = "borough_id")
@@ -24,6 +22,8 @@ class ProbationDeliveryUnit(
 
     @Convert(converter = YesNoConverter::class)
     val selectable: Boolean,
+
+    val functional: Char
 ) {
     fun toCodedValue() = CodedValue(code, description)
 }
