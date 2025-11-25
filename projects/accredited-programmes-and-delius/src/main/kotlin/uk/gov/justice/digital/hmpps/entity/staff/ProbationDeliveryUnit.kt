d48e5701d2b7c83c1bc6018f960e8eed80985646
sam.bailey <161320275+sambailey-moj@users.noreply.github.com>
2025-11-25 15:48:00 +0000

    PI-3636 - selectable check on pdu repository

--- projects/accredited-programmes-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/entity/staff/ProbationDeliveryUnit.kt
+++ projects/accredited-programmes-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/entity/staff/ProbationDeliveryUnit.kt
@@ -2,14 +2,12 @@ package uk.gov.justice.digital.hmpps.entity.staff
 
 import jakarta.persistence.*
 import org.hibernate.annotations.Immutable
-import org.hibernate.annotations.SQLRestriction
 import org.hibernate.type.YesNoConverter
 import uk.gov.justice.digital.hmpps.model.CodedValue
 
 @Entity
 @Immutable
 @Table(name = "borough")
-@SQLRestriction("functional = 'Y'")
 class ProbationDeliveryUnit(
     @Id
     @Column(name = "borough_id")
@@ -22,9 +20,6 @@ class ProbationDeliveryUnit(
 
     @Convert(converter = YesNoConverter::class)
     val selectable: Boolean,
-
-    @Convert(converter = YesNoConverter::class)
-    val functional: Boolean
 ) {
     fun toCodedValue() = CodedValue(code, description)
 }
--- projects/accredited-programmes-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/repository/PduRepository.kt
+++ projects/accredited-programmes-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/repository/PduRepository.kt
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository
 import uk.gov.justice.digital.hmpps.entity.staff.ProbationDeliveryUnit
 
 interface PduRepository : JpaRepository<ProbationDeliveryUnit, Long> {
-    fun getByCode(code: String): ProbationDeliveryUnit?
+    fun getByCodeAndSelectableTrue(code: String): ProbationDeliveryUnit?
 }
