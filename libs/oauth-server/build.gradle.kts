be2e3eec4a5df7eb57ece25f5ec93e99e751a5df
Marcus Aspin <maspin@unilink.com>
2025-11-27 12:05:21 +0000

    Return 409 conflict for database constraint failures
    This was something I noticed while testing the accredited programmes integration - creating multiple appointments with the same reference results in a 500, due to the unique index on contact.external_reference. A 409 conflict seems like the correct response in this scenario.

--- libs/oauth-server/build.gradle.kts
+++ libs/oauth-server/build.gradle.kts
@@ -1,11 +1,13 @@
 dependencies {
     implementation(project(":libs:commons"))
+    compileOnly("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-resource-server")
     implementation(libs.springdoc)
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-data-jpa")
     testImplementation(libs.bundles.mockito)
 }
 
