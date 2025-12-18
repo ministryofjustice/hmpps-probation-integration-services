be9fc3e354e55b60020efc1a33be61c39a89b3d7
Marcus Aspin <maspin@unilink.com>
2025-12-18 13:11:53 +0000

    PI-3706 Add date of birth and age to tier response (#5873)

--- projects/tier-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/controller/model/PersonDetails.kt
+++ projects/tier-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/controller/model/PersonDetails.kt
@@ -1,8 +1,12 @@
 package uk.gov.justice.digital.hmpps.controller.model
 
+import java.time.LocalDate
+
 data class PersonDetails(
     val crn: String,
-    val name: Name
+    val name: Name,
+    val dateOfBirth: LocalDate,
+    val age: Long
 )
 
 data class Name(
