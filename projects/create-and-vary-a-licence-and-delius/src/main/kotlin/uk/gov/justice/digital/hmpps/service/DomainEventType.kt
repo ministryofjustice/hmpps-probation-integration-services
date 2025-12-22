c4170c5f0e3dd509ee00bc0a0c8bdb41de004782
Marcus Aspin <maspin@unilink.com>
2025-12-22 13:52:13 +0000

    PI-3722 Support additional CVL licence types

--- projects/create-and-vary-a-licence-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/DomainEventType.kt
+++ projects/create-and-vary-a-licence-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/DomainEventType.kt
@@ -7,10 +7,18 @@ sealed interface DomainEventType {
         override val name: String = "create-and-vary-a-licence.licence.activated"
     }
 
+    data object PRRDLicenceActivated : DomainEventType {
+        override val name: String = "create-and-vary-a-licence.prrd-licence.activated"
+    }
+
+    data object TimeServedLicenceActivated : DomainEventType {
+        override val name: String = "create-and-vary-a-licence.time-served-licence.activated"
+    }
+
     data class Other(override val name: String) : DomainEventType
 
     companion object {
-        private val types = listOf(LicenceActivated)
+        private val types = listOf(LicenceActivated, PRRDLicenceActivated, TimeServedLicenceActivated)
             .associateBy { it.name }
 
         fun of(name: String): DomainEventType = types[name] ?: Other(name)
