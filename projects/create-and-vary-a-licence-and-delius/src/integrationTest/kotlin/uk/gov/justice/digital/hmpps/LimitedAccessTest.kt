e2b928822165d4d223161035e66f1b3dbf6b367a
sam.bailey <161320275+sambailey-moj@users.noreply.github.com>
2026-01-08 14:36:30 +0000

    PI-3745 adding in bulk endpoint for lao and test

--- projects/create-and-vary-a-licence-and-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/LimitedAccessTest.kt
+++ projects/create-and-vary-a-licence-and-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/LimitedAccessTest.kt
@@ -1,5 +1,6 @@
 package uk.gov.justice.digital.hmpps
 
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.jupiter.api.Test
@@ -7,11 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT
+import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
+import org.springframework.test.web.servlet.post
 import uk.gov.justice.digital.hmpps.data.generator.LimitedAccessGenerator
 import uk.gov.justice.digital.hmpps.data.generator.PersonGenerator
 import uk.gov.justice.digital.hmpps.service.CaseAccess
+import uk.gov.justice.digital.hmpps.service.UserAccess
 import uk.gov.justice.digital.hmpps.test.MockMvcExtensions.contentAsJson
 import uk.gov.justice.digital.hmpps.test.MockMvcExtensions.withToken
 
@@ -73,5 +77,26 @@ internal class LimitedAccessTest @Autowired constructor(
             .andReturn().response.contentAsJson<CaseAccess>()
         assertThat(res.userExcluded, equalTo(true))
         assertThat(res.userRestricted, equalTo(false))
+    }
+
+    @Test
+    fun `should return list of exclusions and restrictions for user given list of CRNS`() {
+        val url =
+            "/users/${LimitedAccessGenerator.LAO_DEFAULT_USER.username}/access"
+        val requestBody = listOf(LimitedAccessGenerator.LAO_RESTRICTION.person.crn,
+            LimitedAccessGenerator.LAO_EXCLUSION.person.crn,
+            PersonGenerator.DEFAULT_PERSON.crn)
+        val json = jacksonObjectMapper().writeValueAsString(requestBody)
+        val res = mockMvc.post(url) {
+            withToken()
+            content = json
+            contentType = MediaType.APPLICATION_JSON
+        }
+            .andExpect { status { isOk() } }
+            .andReturn().response.contentAsString
+
+
+        val actualResult = jacksonObjectMapper().readValue(res, UserAccess::class.java)
+        assertThat(actualResult.access.size, equalTo(requestBody.size))
     }
 }
