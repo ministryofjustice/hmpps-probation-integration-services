2be6cd19ee27116e5569ea7fd43e55d75766ef8b
James MacBeth <56261600+jamesmacbeth-unilink@users.noreply.github.com>
2026-01-09 13:09:13 +0000

    PI-3738 - Add non-working days ref data end point (#5930)

--- projects/community-payback-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/ReferenceDataService.kt
+++ projects/community-payback-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/ReferenceDataService.kt
@@ -1,9 +1,14 @@
 package uk.gov.justice.digital.hmpps.service
 
 import org.springframework.stereotype.Service
-import uk.gov.justice.digital.hmpps.integrations.delius.entity.*
+import uk.gov.justice.digital.hmpps.integrations.delius.entity.ContactOutcomeRepository
 import uk.gov.justice.digital.hmpps.integrations.delius.entity.ContactType.Code.UNPAID_WORK_APPOINTMENT
+import uk.gov.justice.digital.hmpps.integrations.delius.entity.Dataset
+import uk.gov.justice.digital.hmpps.integrations.delius.entity.ReferenceDataRepository
+import uk.gov.justice.digital.hmpps.integrations.delius.entity.toCodeDescription
 import uk.gov.justice.digital.hmpps.model.CodeDescription
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 @Service
 class ReferenceDataService(
@@ -17,5 +22,11 @@ class ReferenceDataService(
     fun getUpwAppointmentOutcomes(): List<CodeDescription> =
         contactOutcomeRepository.findForTypeCode(UNPAID_WORK_APPOINTMENT.value).map {
             CodeDescription(it.code, it.description)
+        }
+
+    fun getNonWorkingDays(): List<LocalDate> =
+        referenceDataRepository.findByDatasetCode(Dataset.NON_WORKING_DAYS).map {
+            val dateFormatter = DateTimeFormatter.ofPattern("ddMMyy")
+            LocalDate.parse(it.code, dateFormatter)
         }
 }
