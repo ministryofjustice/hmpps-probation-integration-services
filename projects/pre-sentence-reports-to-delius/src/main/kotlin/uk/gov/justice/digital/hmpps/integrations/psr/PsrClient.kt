ed15b98073302817a08d8c28d2c75edc86fc44c2
Sam Bailey <161320275+sambailey-moj@users.noreply.github.com>
2026-02-04 10:48:49 +0000

    PI-3820 remove existing code from psr (#6004)

--- projects/pre-sentence-reports-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/psr/PsrClient.kt
+++
@@ -1,9 +0,0 @@
-package uk.gov.justice.digital.hmpps.integrations.psr
-
-import org.springframework.web.service.annotation.GetExchange
-import java.net.URI
-
-interface PsrClient {
-    @GetExchange
-    fun getPsrReport(baseUrl: URI): ByteArray
-}
