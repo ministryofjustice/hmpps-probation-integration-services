0728ae86a683ccb7e6138cf5c2efc91ff9d80e8b
sam.bailey <161320275+sambailey-moj@users.noreply.github.com>
2026-02-05 21:47:09 +0000

    PI-3778 spring boot 4 upgrade

--- projects/pre-sentence-reports-to-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/DocumentDetailsIntegrationTest.kt
+++ projects/pre-sentence-reports-to-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/DocumentDetailsIntegrationTest.kt
@@ -3,8 +3,8 @@ package uk.gov.justice.digital.hmpps
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 import uk.gov.justice.digital.hmpps.data.generator.DocumentGenerator
