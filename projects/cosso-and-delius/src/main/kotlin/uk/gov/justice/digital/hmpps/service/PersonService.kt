1842cae8d3feb86f4feb1748ccab004794f21e63
sam.bailey <161320275+sambailey-moj@users.noreply.github.com>
2026-02-09 14:06:06 +0000

    PI-3750 revert git mistakes

--- projects/cosso-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/PersonService.kt
+++ projects/cosso-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/PersonService.kt
@@ -1,21 +1,28 @@
 package uk.gov.justice.digital.hmpps.service
 
+import org.springframework.ldap.core.LdapTemplate
 import org.springframework.stereotype.Service
 import uk.gov.justice.digital.hmpps.entity.PersonAddressRepository
 import uk.gov.justice.digital.hmpps.entity.PersonRepository
 import uk.gov.justice.digital.hmpps.exception.NotFoundException
 import uk.gov.justice.digital.hmpps.exception.NotFoundException.Companion.orNotFoundBy
+import uk.gov.justice.digital.hmpps.ldap.findAttributeByUsername
 import uk.gov.justice.digital.hmpps.model.Address
 import uk.gov.justice.digital.hmpps.model.Name
 import uk.gov.justice.digital.hmpps.model.PersonDetails
 
 @Service
 class PersonService(
-    val personRepository: PersonRepository,
-    val personAddressRepository: PersonAddressRepository
+    private val personRepository: PersonRepository,
+    private val personAddressRepository: PersonAddressRepository,
+    private val ldapTemplate: LdapTemplate
 ) {
 
-    fun getBasicDetails(crn: String): PersonDetails {
+    fun getBasicDetails(crn: String, username: String): PersonDetails {
+        ldapTemplate.findAttributeByUsername(username, "cn")
+            ?: throw NotFoundException("User", "username", username)
+        ldapTemplate.findAttributeByUsername(username, "userHomeArea")
+            ?: throw IllegalArgumentException("No home area found for $username")
         val person = personRepository.findByCrn(crn).orNotFoundBy("crn", crn)
         val addresses = personAddressRepository.findByPersonId(person.offenderId).map {
             Address(
@@ -29,9 +36,6 @@ class PersonService(
                 county = it.county,
                 postcode = it.postcode,
             )
-        }
-        if (addresses.none { it.status == "MAIN" }) {
-            throw NotFoundException("No main address found for person with crn:$crn")
         }
         return PersonDetails(
             title = person.title!!.description,
