112129e9342e5deacf18e6f786818a2302597044
Sam Bailey <161320275+sambailey-moj@users.noreply.github.com>
2026-02-10 12:25:55 +0000

    Cosso Bug date of birth
    adding column definition for date of birth date

--- projects/cosso-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/entity/Person.kt
+++ projects/cosso-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/entity/Person.kt
@@ -19,6 +19,7 @@ class Person(
     val secondName: String? = null,
     val thirdName: String? = null,
     val surname: String,
+    @column(name = "date_of_birth_date")
     val dateOfBirth: LocalDate,
     val telephoneNumber: String? = null,
     val mobileNumber: String? = null,
