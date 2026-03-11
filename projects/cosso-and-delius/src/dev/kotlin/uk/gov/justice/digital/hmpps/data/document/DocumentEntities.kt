41ab5cc8e077f74eadf3506b631ae961b3f59018
sam.bailey <161320275+sambailey-moj@users.noreply.github.com>
2026-03-11 16:06:43 +0000

    PI-3754 handle created and deleted messages for COSSO

--- projects/cosso-and-delius/src/dev/kotlin/uk/gov/justice/digital/hmpps/data/document/DocumentEntities.kt
+++ projects/cosso-and-delius/src/dev/kotlin/uk/gov/justice/digital/hmpps/data/document/DocumentEntities.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.data.document
 import jakarta.persistence.Entity
 import jakarta.persistence.Id
 import org.hibernate.annotations.Immutable
+import org.springframework.data.jpa.repository.JpaRepository
 
 @Immutable
 @Entity
--- projects/cosso-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/messaging/Handler.kt
+++ projects/cosso-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/messaging/Handler.kt
@@ -1,30 +1,55 @@
 package uk.gov.justice.digital.hmpps.messaging
 
+import com.asyncapi.kotlinasyncapi.annotation.Schema
 import com.asyncapi.kotlinasyncapi.annotation.channel.Channel
+import com.asyncapi.kotlinasyncapi.annotation.channel.Message
 import com.asyncapi.kotlinasyncapi.annotation.channel.Publish
 import org.springframework.stereotype.Component
 import uk.gov.justice.digital.hmpps.converter.NotificationConverter
 import uk.gov.justice.digital.hmpps.detail.DomainEventDetailService
 import uk.gov.justice.digital.hmpps.message.HmppsDomainEvent
 import uk.gov.justice.digital.hmpps.message.Notification
+import uk.gov.justice.digital.hmpps.service.DocumentService
 import uk.gov.justice.digital.hmpps.telemetry.TelemetryService
 import uk.gov.justice.digital.hmpps.telemetry.TelemetryMessagingExtensions.notificationReceived
+import java.lang.IllegalArgumentException
 
 @Component
 @Channel("cosso-and-delius-queue")
 class Handler(
     override val converter: NotificationConverter<HmppsDomainEvent>,
+    private val detailService: DomainEventDetailService,
+    private val documentService: DocumentService,
     private val telemetryService: TelemetryService
 ) : NotificationHandler<HmppsDomainEvent> {
     @Publish(
         messages = [
-            // TODO list the event types here that this service will subscribe to. For example,
-            // Message(name = "approved-premises/application-assessed"),
-            // Message(title = "probation-case.prison-identifier.added"),
+            Message(name = "probation-case.COSSO.created", payload = Schema(HmppsDomainEvent::class))
         ]
     )
     override fun handle(notification: Notification<HmppsDomainEvent>) {
         telemetryService.notificationReceived(notification)
-        TODO("Not yet implemented")
+        when (notification.eventType) {
+            "probation-case.COSSO.created" -> {
+                val file = detailService.getDetail<ByteArray>(notification.message)
+                documentService.uploadDocument(notification.message, file)
+                telemetryService.trackEvent("DocumentUploaded", notification.message.telemetry())
+            }
+            "probation-case.COSSO.deleted" -> {
+                documentService.deleteDocument(notification.message)
+                telemetryService.trackEvent("DocumentDeleted", notification.message.telemetry())
+            }
+        }
     }
 }
+
+val HmppsDomainEvent.cossoBreachNoticeId
+    get() = additionalInformation["COSSOBreachNoticeId"] as String? ?: throw IllegalArgumentException("Missing cossoBreachNoticeId")
+val HmppsDomainEvent.username
+    get() = additionalInformation["username"] as String? ?: throw IllegalArgumentException("Missing username")
+
+fun HmppsDomainEvent.telemetry() = mapOf(
+    "crn" to personReference.findCrn(),
+    "cossoBreachNoticeId" to cossoBreachNoticeId,
+    "username" to username,
+)
