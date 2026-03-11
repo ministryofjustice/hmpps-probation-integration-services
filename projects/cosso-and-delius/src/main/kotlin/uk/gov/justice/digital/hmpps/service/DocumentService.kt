23b3e644a07efc1c67c78c95a7a2f30ff3675db5
sam.bailey <161320275+sambailey-moj@users.noreply.github.com>
2026-03-11 16:49:34 +0000

    PI-3754 adding test for DocumentService

--- projects/cosso-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/DocumentService.kt
+++ projects/cosso-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/DocumentService.kt
@@ -54,8 +54,7 @@ class DocumentService(
         alfrescoUploadClient.release(document.alfrescoId)
         alfrescoUploadClient.delete(document.alfrescoId)
     }
-
-    private fun updateParent(document: DocumentEntity) {
+    fun updateParent(document: DocumentEntity) {
         val hasOtherDocuments = documentRepository
             .existsByTableNameAndPrimaryKeyIdAndIdNot(document.tableName, document.primaryKeyId, document.id)
 
