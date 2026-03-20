8d5830775906fa7fc7a6bac869ad88f026398d1f
sam.bailey <161320275+sambailey-moj@users.noreply.github.com>
2026-03-20 08:54:16 +0000

    PI-3753 column type correction

--- projects/cosso-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/entity/Contact.kt
+++ projects/cosso-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/entity/Contact.kt
@@ -14,6 +14,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import java.time.LocalDate
 import java.time.ZonedDateTime
+import jakarta.persistence.Lob
 
 @Entity
 @Table(name = "contact")
@@ -29,7 +30,9 @@ class Contact(
     @Column(name = "contact_start_time")
     val startTime: ZonedDateTime,
 
-    val notes: String,
+    @Lob
+    @Column(name = "notes")
+    val notes: String?,
 
     val eventId: Long,
 
