65a2a611d92ae26687956ef76245e56131c7e6f4
sam.bailey <161320275+sambailey-moj@users.noreply.github.com>
2026-04-21 13:46:44 +0100

    PI-3926 updated config class

--- libs/messaging/src/main/kotlin/uk/gov/justice/digital/hmpps/config/ExtendedSqsConfig.kt
+++ libs/messaging/src/main/kotlin/uk/gov/justice/digital/hmpps/config/ExtendedSqsConfig.kt
@@ -2,33 +2,30 @@ package uk.gov.justice.digital.hmpps.config
 
 import com.amazon.sqs.javamessaging.AmazonSQSExtendedAsyncClient
 import com.amazon.sqs.javamessaging.ExtendedAsyncClientConfiguration
-import io.awspring.cloud.sqs.config.SqsBootstrapConfiguration
-import org.springframework.beans.factory.annotation.Qualifier
+import io.awspring.cloud.autoconfigure.core.AwsClientBuilderConfigurer
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.autoconfigure.AutoConfigureAfter
-import org.springframework.boot.autoconfigure.AutoConfigureBefore
-import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Conditional
+import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
 import software.amazon.awssdk.services.s3.S3AsyncClient
 import software.amazon.awssdk.services.sqs.SqsAsyncClient
 
-@AutoConfiguration
+@Configuration
 @Conditional(AwsCondition::class)
 @ConditionalOnProperty("messaging.large-message.bucket")
-@AutoConfigureAfter(SqsBootstrapConfiguration::class)
-@AutoConfigureBefore(name = ["io.awspring.cloud.autoconfigure.sqs.SqsAutoConfiguration"])
 class ExtendedSqsConfig(
+    private val awsClientBuilderConfigurer: AwsClientBuilderConfigurer,
     @Value("\${messaging.large-message.bucket}") private val bucketName: String
 ) {
     @Bean
     @Primary
-    fun extendedSqsAsyncClient(@Qualifier("sqsAsyncClient") sqsAsyncClient: SqsAsyncClient): SqsAsyncClient {
-        val s3AsyncClient = S3AsyncClient.builder().build()
+    fun extendedSqsAsyncClient(): SqsAsyncClient {
+        val sqsBase = awsClientBuilderConfigurer.configure(SqsAsyncClient.builder()).build()
+        val s3AsyncClient = awsClientBuilderConfigurer.configure(S3AsyncClient.builder()).build()
         val extendedConfig = ExtendedAsyncClientConfiguration()
             .withPayloadSupportEnabled(s3AsyncClient, bucketName)
-        return AmazonSQSExtendedAsyncClient(sqsAsyncClient, extendedConfig)
+        return AmazonSQSExtendedAsyncClient(sqsBase, extendedConfig)
     }
 }
