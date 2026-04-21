832a4ea118d34b89ae8d60727205879714654a67
sam.bailey <161320275+sambailey-moj@users.noreply.github.com>
2026-04-21 13:31:54 +0100

    PI-3926 auto configuration

--- libs/messaging/src/main/kotlin/uk/gov/justice/digital/hmpps/config/ExtendedSqsConfig.kt
+++ libs/messaging/src/main/kotlin/uk/gov/justice/digital/hmpps/config/ExtendedSqsConfig.kt
@@ -7,22 +7,20 @@ import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.AutoConfigureAfter
 import org.springframework.boot.autoconfigure.AutoConfigureBefore
+import org.springframework.boot.autoconfigure.AutoConfiguration
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Conditional
-import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
 import software.amazon.awssdk.services.s3.S3AsyncClient
-import software.amazon.awssdk.services.s3.S3Client
 import software.amazon.awssdk.services.sqs.SqsAsyncClient
 
-@Configuration
+@AutoConfiguration
 @Conditional(AwsCondition::class)
 @ConditionalOnProperty("messaging.large-message.bucket")
 @AutoConfigureAfter(SqsBootstrapConfiguration::class)
 @AutoConfigureBefore(name = ["io.awspring.cloud.autoconfigure.sqs.SqsAutoConfiguration"])
 class ExtendedSqsConfig(
-    private val s3Client: S3Client,
     @Value("\${messaging.large-message.bucket}") private val bucketName: String
 ) {
     @Bean
