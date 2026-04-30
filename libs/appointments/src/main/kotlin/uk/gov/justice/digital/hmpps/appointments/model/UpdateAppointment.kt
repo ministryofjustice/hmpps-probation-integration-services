4cdd5fa87ddb41da6419bd89ce4c773defac83fe
sam.bailey <161320275+sambailey-moj@users.noreply.github.com>
2026-04-30 17:00:44 +0100

    PI-4070 back out lib change

--- libs/appointments/src/main/kotlin/uk/gov/justice/digital/hmpps/appointments/model/UpdateAppointment.kt
+++ libs/appointments/src/main/kotlin/uk/gov/justice/digital/hmpps/appointments/model/UpdateAppointment.kt
@@ -9,7 +9,6 @@ import java.time.ZonedDateTime
 class UpdateAppointment {
     data class Outcome(
         val outcomeCode: String?,
-        val enforcementActionCode: String? = null,
     ) {
         internal constructor(entity: AppointmentContact) : this(entity.outcome?.code)
     }
