4851376450f8ff839a7fa724b08faf75e71528ad
Marcus Aspin <maspin@unilink.com>
2026-05-13 18:39:06 +0100

    PI-4119 Restrict office locations
    Fixes duplicate codes being found that are either inactive or linked to an inactive provider.
    
    Resolves [COMMUNITY-PAYBACK-AND-DELIUS-K](https://ministryofjustice.sentry.io/issues/7444605007)

--- projects/community-payback-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/entity/staff/OfficeLocation.kt
+++ projects/community-payback-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/entity/staff/OfficeLocation.kt
@@ -1,14 +1,12 @@
 package uk.gov.justice.digital.hmpps.entity.staff
 
-import jakarta.persistence.Column
-import jakarta.persistence.Entity
-import jakarta.persistence.Id
-import jakarta.persistence.Table
+import jakarta.persistence.*
 import org.hibernate.annotations.Immutable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import uk.gov.justice.digital.hmpps.exception.NotFoundException.Companion.orNotFoundBy
 import uk.gov.justice.digital.hmpps.utils.Extensions.reportMissing
+import java.time.ZonedDateTime
 
 @Entity
 @Table(name = "office_location")
@@ -17,48 +15,54 @@ class OfficeLocation(
     @Id
     @Column(name = "office_location_id")
     val id: Long,
-
     @Column(columnDefinition = "char(7)")
     val code: String,
-
     val description: String,
-
     val buildingName: String?,
-
     val buildingNumber: String?,
-
     val streetName: String?,
-
     @Column(name = "town_city")
     val town: String?,
-
     val county: String?,
-
     val postcode: String?,
+    @ManyToOne
+    @JoinColumn(name = "probation_area_id")
+    val provider: Provider,
+    val startDate: ZonedDateTime,
+    val endDate: ZonedDateTime?,
 )
 
 interface OfficeLocationRepository : JpaRepository<OfficeLocation, Long> {
-    fun findByCodeIn(codes: Collection<String>): List<OfficeLocation>
-    fun findByCode(code: String): OfficeLocation?
-    fun getByCode(code: String): OfficeLocation = findByCode(code).orNotFoundBy("OfficeLocationCode", code)
-    fun getByCodeIn(codes: List<String>) =
-        codes.toSet().let { codes -> findByCodeIn(codes).associateBy { it.code }.reportMissing(codes) }
+    fun findByCodeInAndEndDateIsNullOrEndDateAfterAndProviderSelectableIsTrue(
+        codes: Collection<String>,
+        now: ZonedDateTime = ZonedDateTime.now()
+    ): List<OfficeLocation>
+
+    fun findByCodeAndEndDateIsNullOrEndDateAfterAndProviderSelectableIsTrue(
+        code: String,
+        now: ZonedDateTime = ZonedDateTime.now()
+    ): OfficeLocation?
+
+    fun getByCode(code: String): OfficeLocation =
+        findByCodeAndEndDateIsNullOrEndDateAfterAndProviderSelectableIsTrue(code).orNotFoundBy(
+            "OfficeLocationCode",
+            code
+        )
+
+    fun getByCodeIn(codes: List<String>) = codes.toSet().let { codes ->
+        findByCodeInAndEndDateIsNullOrEndDateAfterAndProviderSelectableIsTrue(codes).associateBy { it.code }
+            .reportMissing(codes)
+    }
 
     @Query(
         """
-        select ol.office_location_id, 
-            ol.code, 
-            ol.description, 
-            ol.building_name, 
-            ol.building_number, 
-            ol.street_name, 
-            ol.town_city,   
-            ol.county, 
-            ol.postcode
-        from office_location ol
-        join team_office_location tol on tol.office_location_id = ol.office_location_id
-        where tol.team_id = :teamId
-        """, nativeQuery = true
+        select ol
+        from OfficeLocation ol
+        join TeamOfficeLocation tol on tol.officeLocationId = ol.id
+        where tol.teamId = :teamId
+        and ol.endDate is null or ol.endDate > current_date
+        and ol.provider.selectable = true
+        """
     )
     fun getLocationsByTeam(teamId: Long): List<OfficeLocation>
 }
