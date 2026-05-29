762ff7d5ba2f9e2cae501fdb0d9e33af8aa7eff8
Joseph.Dundon <jdundon@unilink.com>
2026-05-29 14:29:13 +0100

    remove unused model

--- projects/manage-my-community-sentence-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/model/UpwAppointment.kt
+++
@@ -1,9 +0,0 @@
-package uk.gov.justice.digital.hmpps.model
-
-data class UpwAppointment(
-    val minutesCredited: Int? = null,
-    val softDeleted: Boolean = false,
-    val contact: Appointment,
-    val pickUpLocation: Address?,
-    val project: UpwProject?,
-)
