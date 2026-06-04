c13341143618f329f175fb391ba052151386d443
Hazzaz Imtiaz <hazzaz.imtiaz@justice.gov.uk>
2026-06-04 17:43:22 +0100

    PI-4189 Allowing null for alert type

--- projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/model/activity/Activity.kt
+++ projects/manage-supervision-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/api/model/activity/Activity.kt
@@ -60,7 +60,7 @@ data class Activity(
     val nsiId: Long? = null,
     val esupervisionId: UUID? = null,
     val externalReference: String? = null,
-    val alert: Boolean = false
+    val alert: Boolean? = null
 )
 
 data class Component(val id: Long, val description: String, val type: Type) {
