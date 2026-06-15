2ac293e271520e295a3c19d83898f533a7768ee3
Marcus Aspin <maspin@unilink.com>
2026-06-15 17:25:01 +0100

    PI-4247 Improve handling of 404 from Alfresco deletion (#6487)

--- projects/breach-notice-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/DocumentService.kt
+++ projects/breach-notice-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/DocumentService.kt
@@ -10,6 +10,7 @@ import uk.gov.justice.digital.hmpps.audit.entity.AuditedInteraction
 import uk.gov.justice.digital.hmpps.audit.service.AuditableService
 import uk.gov.justice.digital.hmpps.audit.service.AuditedInteractionService
 import uk.gov.justice.digital.hmpps.client.AlfrescoUploadClient
+import uk.gov.justice.digital.hmpps.client.RestClientUtils.nullIfNotFound
 import uk.gov.justice.digital.hmpps.exception.NotFoundException
 import uk.gov.justice.digital.hmpps.integrations.delius.Document
 import uk.gov.justice.digital.hmpps.integrations.delius.Document.Companion.breachNoticeUrn
@@ -41,7 +42,7 @@ class DocumentService(
         document.lastUpdatedUserId = auditUserService.findUser(event.username)?.id
             ?: throw NotFoundException("User", "username", event.username)
 
-        alfrescoUploadClient.delete(document.alfrescoId)
+        nullIfNotFound { alfrescoUploadClient.delete(document.alfrescoId) }
         document.alfrescoId = alfrescoUploadClient.upload(document.toMultipart(file)).id
 
         documentRepository.save(document)
@@ -53,7 +54,7 @@ class DocumentService(
         documentRepository.delete(document)
         updateParent(document)
         alfrescoUploadClient.release(document.alfrescoId)
-        alfrescoUploadClient.delete(document.alfrescoId)
+        nullIfNotFound { alfrescoUploadClient.delete(document.alfrescoId) }
     }
 
     private fun getDocument(event: HmppsDomainEvent, audit: AuditedInteraction.Parameters): Document {
