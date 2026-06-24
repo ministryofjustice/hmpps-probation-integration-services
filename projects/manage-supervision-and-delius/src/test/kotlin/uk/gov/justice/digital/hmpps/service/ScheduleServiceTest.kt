e3416008a33697e7984c07153a9878a4231c81d3
sam.bailey <161320275+sambailey-moj@users.noreply.github.com>
2026-06-24 11:44:05 +0100

    PI-4288 tweak logic to deliusManaged field extra assertions

--- projects/manage-supervision-and-delius/src/test/kotlin/uk/gov/justice/digital/hmpps/service/ScheduleServiceTest.kt
+++ projects/manage-supervision-and-delius/src/test/kotlin/uk/gov/justice/digital/hmpps/service/ScheduleServiceTest.kt
@@ -2,7 +2,6 @@ package uk.gov.justice.digital.hmpps.service
 
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.equalTo
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
@@ -13,15 +12,27 @@ import org.mockito.kotlin.whenever
 import org.springframework.data.domain.PageImpl
 import org.springframework.data.domain.PageRequest
 import org.springframework.data.domain.Sort
-import uk.gov.justice.digital.hmpps.data.generator.ContactGenerator
-import uk.gov.justice.digital.hmpps.data.generator.personalDetails.PersonDetailsGenerator.PERSONAL_DETAILS
+import uk.gov.justice.digital.hmpps.api.model.Name
+import uk.gov.justice.digital.hmpps.api.model.PersonSummary
+import uk.gov.justice.digital.hmpps.api.model.appointment.CreateAppointment
+import uk.gov.justice.digital.hmpps.datetime.EuropeLondon
+import uk.gov.justice.digital.hmpps.integrations.delius.overview.entity.Contact
 import uk.gov.justice.digital.hmpps.integrations.delius.overview.entity.ContactRepository
+import uk.gov.justice.digital.hmpps.integrations.delius.overview.entity.ContactType
 import uk.gov.justice.digital.hmpps.integrations.delius.overview.entity.EnforcementRepository
+import uk.gov.justice.digital.hmpps.integrations.delius.overview.entity.Person
 import uk.gov.justice.digital.hmpps.integrations.delius.overview.entity.PersonRepository
+import uk.gov.justice.digital.hmpps.integrations.delius.overview.entity.Requirement
+import uk.gov.justice.digital.hmpps.integrations.delius.overview.entity.RequirementMainCategory
 import uk.gov.justice.digital.hmpps.integrations.delius.personalDetails.entity.DocumentRepository
+import uk.gov.justice.digital.hmpps.integrations.delius.referencedata.entity.ReferenceData
 import uk.gov.justice.digital.hmpps.integrations.delius.sentence.entity.OffenderManagerRepository
 import uk.gov.justice.digital.hmpps.integrations.delius.user.entity.UserRepository
+import uk.gov.justice.digital.hmpps.integrations.delius.user.entity.User
 import uk.gov.justice.digital.hmpps.utils.Summary
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.ZonedDateTime
 
 @ExtendWith(MockitoExtension::class)
 internal class ScheduleServiceTest {
@@ -47,85 +58,189 @@ internal class ScheduleServiceTest {
     @InjectMocks
     lateinit var service: ScheduleService
 
-    private lateinit var personSummary: Summary
-
-    @BeforeEach
-    fun setup() {
-        personSummary = Summary(
-            id = PERSONAL_DETAILS.id,
-            forename = PERSONAL_DETAILS.forename,
-            secondName = PERSONAL_DETAILS.secondName,
-            surname = PERSONAL_DETAILS.surname, crn = PERSONAL_DETAILS.crn, pnc = PERSONAL_DETAILS.pnc,
-            noms = PERSONAL_DETAILS.noms, dateOfBirth = PERSONAL_DETAILS.dateOfBirth.atStartOfDay()
-        )
-    }
-
     @Test
     fun `calls get upcoming schedule function`() {
         val crn = "X000005"
-        val expectedContacts = listOf(ContactGenerator.NEXT_APPT_CONTACT, ContactGenerator.FIRST_APPT_CONTACT)
-        whenever(personRepository.findSummary(crn)).thenReturn(personSummary)
-        whenever(contactRepository.findUpComingAppointments(any(), any(), any(), any())).thenReturn(
-            PageImpl(
-                expectedContacts
-            )
+        val expectedContacts = listOf(
+            appointmentContact(CreateAppointment.Type.PlannedDoorstepContactNS.code),
+            appointmentContact("ZZZZ")
         )
+        whenever(personRepository.findSummary(crn)).thenReturn(summaryEntity())
+        whenever(contactRepository.findUpComingAppointments(any(), any(), any(), any())).thenReturn(PageImpl(expectedContacts))
+
         val res = service.getPersonUpcomingSchedule(crn, PageRequest.of(0, 10))
-        assertThat(
-            res.personSummary, equalTo(PERSONAL_DETAILS.toSummary())
-        )
+
+        assertThat(res.personSummary, equalTo(expectedPersonSummary()))
         assertThat(res.personSchedule.appointments, equalTo(expectedContacts.map { it.toActivity() }))
     }
 
     @Test
     fun `calls get appointment function`() {
         val crn = "X000005"
-        val expectedContact = ContactGenerator.NEXT_APPT_CONTACT
-        whenever(personRepository.findSummary(crn)).thenReturn(personSummary)
-        whenever(contactRepository.findByPersonIdAndId(any(), any())).thenReturn(expectedContact)
-        // whenever(documentRepository.findByTypeAndPrimaryKeyId(any(), any())).thenReturn(emptyList())
-        whenever(userRepository.findAllById(any())).thenReturn(emptyList())
-        val res = service.getPersonAppointment(crn, ContactGenerator.NEXT_APPT_CONTACT.id)
-        assertThat(res.personSummary, equalTo(PERSONAL_DETAILS.toSummary()))
+        val expectedContact = appointmentContact(CreateAppointment.Type.PlannedDoorstepContactNS.code)
+        whenever(personRepository.findSummary(crn)).thenReturn(summaryEntity())
+        whenever(contactRepository.findByPersonIdAndId(1, expectedContact.id!!)).thenReturn(expectedContact)
+        whenever(userRepository.findAllById(any<Iterable<Long>>())).thenReturn(emptyList())
+
+        val res = service.getPersonAppointment(crn, expectedContact.id!!)
+
+        assertThat(res.personSummary, equalTo(expectedPersonSummary()))
         assertThat(res.appointment, equalTo(expectedContact.toActivity()))
     }
 
     @Test
     fun `calls get previous schedule function`() {
         val crn = "X000005"
-        val expectedContacts =
-            listOf(ContactGenerator.PREVIOUS_APPT_CONTACT_ABSENT, ContactGenerator.PREVIOUS_APPT_CONTACT)
-        whenever(personRepository.findSummary(crn)).thenReturn(personSummary)
-        whenever(contactRepository.findPageablePreviousAppointments(any(), any(), any(), any())).thenReturn(
-            PageImpl(
-                expectedContacts
-            )
+        val expectedContacts = listOf(
+            appointmentContact("ABCD"),
+            appointmentContact("EFGH")
         )
+        whenever(personRepository.findSummary(crn)).thenReturn(summaryEntity())
+        whenever(contactRepository.findPageablePreviousAppointments(any(), any(), any(), any())).thenReturn(PageImpl(expectedContacts))
+
         val res = service.getPersonPreviousSchedule(
             crn,
             PageRequest.of(0, 10, Sort.by(Sort.Direction.DESC, "contact_date", "contact_start_time"))
         )
-        assertThat(
-            res.personSummary, equalTo(PERSONAL_DETAILS.toSummary())
-        )
+
+        assertThat(res.personSummary, equalTo(expectedPersonSummary()))
         assertThat(res.personSchedule.appointments, equalTo(expectedContacts.map { it.toActivity() }))
     }
 
     @Test
-    fun `deliusManaged is false when contact type is in CreateAppointment types`() {
-        val contact = ContactGenerator.NEXT_APPT_CONTACT
+    fun `deliusManaged is false when contact type is in CreateAppointment types and complied is false`() {
+        val contact = appointmentContact(
+            CreateAppointment.Type.PlannedDoorstepContactNS.code,
+            complied = false
+        )
+
         assertThat(contact.toActivity().deliusManaged, equalTo(false))
     }
 
     @Test
-    fun `deliusManaged is true when contact type is unknown (not in CreateAppointment types)`() {
-        val contact = ContactGenerator.FIRST_NON_APPT_CONTACT
+    fun `deliusManaged is true when contact type is unknown`() {
+        val contact = appointmentContact("ZZZZ")
+
         assertThat(contact.toActivity().deliusManaged, equalTo(true))
     }
 
     @Test
     fun `deliusManaged is true when requirement mainCategory code is F`() {
-        val contact = ContactGenerator.FIRST_NON_APPT_CONTACT
+        val contact = appointmentContact(
+            CreateAppointment.Type.PlannedDoorstepContactNS.code,
+            requirement = requirement(mainCategoryCode = "F")
+        )
+
         assertThat(contact.toActivity().deliusManaged, equalTo(true))
     }
+
+    private fun summaryEntity() = Summary(
+        id = 1,
+        forename = "Joe",
+        secondName = null,
+        thirdName = null,
+        surname = "Bloggs",
+        crn = "X000005",
+        pnc = "1234567",
+        noms = "A1234AA",
+        dateOfBirth = LocalDateTime.of(1990, 1, 1, 0, 0)
+    )
+
+    private fun expectedPersonSummary() = PersonSummary(
+        name = Name(forename = "Joe", middleName = null, surname = "Bloggs"),
+        crn = "X000005",
+        offenderId = 1,
+        pnc = "1234567",
+        noms = "A1234AA",
+        dateOfBirth = LocalDate.of(1990, 1, 1)
+    )
+
+    private fun appointmentContact(
+        code: String,
+        complied: Boolean? = true,
+        requirement: Requirement? = null,
+    ) = Contact(
+        id = 1,
+        person = person(),
+        type = contactType(code),
+        date = LocalDate.of(2024, 6, 24),
+        startTime = ZonedDateTime.of(2024, 6, 24, 9, 0, 0, 0, EuropeLondon),
+        complied = complied,
+        requirement = requirement,
+        notes = null,
+        lastUpdatedUser = user(),
+    )
+
+    private fun person() = Person(
+        id = 1,
+        crn = "X000005",
+        pnc = "1234567",
+        noms = "A1234AA",
+        forename = "Joe",
+        secondName = null,
+        thirdName = null,
+        surname = "Bloggs",
+        preferredName = null,
+        dateOfBirth = LocalDate.of(1990, 1, 1),
+        dateOfDeath = null,
+        telephoneNumber = null,
+        mobileNumber = null,
+        emailAddress = null,
+        previousSurname = null,
+        gender = referenceData(1, "M", "Male"),
+        religion = null,
+        language = null,
+        sexualOrientation = referenceData(2, "U", "Unknown"),
+        genderIdentity = referenceData(3, "U", "Unknown"),
+        genderIdentityDescription = null,
+        requiresInterpreter = false,
+        softDeleted = false,
+        exclusionMessage = null,
+        restrictionMessage = null,
+        lastUpdatedDatetime = ZonedDateTime.now(EuropeLondon),
+        lastUpdatedUserId = 0,
+        lastUpdatedUser = null,
+    )
+
+    private fun user() = User(
+        id = 1,
+        staff = null,
+        username = "JoeBloggs",
+        forename = "Joe",
+        surname = "Bloggs",
+    )
+
+    private fun contactType(code: String) = ContactType(
+        id = 1,
+        code = code,
+        attendanceContact = true,
+        description = "Type $code",
+        locationRequired = "N",
+        editable = true,
+    )
+
+    private fun requirement(mainCategoryCode: String) = Requirement(
+        id = 1,
+        length = null,
+        notes = null,
+        expectedStartDate = null,
+        startDate = LocalDate.now(),
+        commencementDate = null,
+        expectedEndDate = null,
+        terminationDate = null,
+        disposal = null,
+        mainCategory = RequirementMainCategory(
+            id = 1,
+            code = mainCategoryCode,
+            description = mainCategoryCode
+        ),
+        subCategory = null,
+        terminationDetails = null,
+    )
+
+    private fun referenceData(id: Long, code: String, description: String) = ReferenceData(
+        id = id,
+        code = code,
+        description = description,
+    )
 }
