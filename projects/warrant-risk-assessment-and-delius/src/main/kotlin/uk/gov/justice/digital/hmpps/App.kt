0d7983050a8ef16d99fe6ff77479076667179e11
Hazzaz Imtiaz <hazzaz.imtiaz@justice.gov.uk>
2026-06-25 20:10:51 +0100

    [PI-4191] BRANCH Few more suggestions integrated from the review

--- projects/warrant-risk-assessment-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/App.kt
+++ projects/warrant-risk-assessment-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/App.kt
@@ -1,8 +1,10 @@
 package uk.gov.justice.digital.hmpps
 
+import com.asyncapi.kotlinasyncapi.springweb.EnableAsyncApi
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
+@EnableAsyncApi
 @SpringBootApplication
 class App
 
