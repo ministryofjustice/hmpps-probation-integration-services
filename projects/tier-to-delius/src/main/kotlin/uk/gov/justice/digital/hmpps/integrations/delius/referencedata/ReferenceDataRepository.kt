7833b69e3beec1b90cc7db264963fc6944bea31e
Marcus Aspin <maspin@unilink.com>
2026-07-10 17:15:28 +0100

    PI-4198 Add v3 tier to Delius

--- projects/tier-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/referencedata/ReferenceDataRepository.kt
+++ projects/tier-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/referencedata/ReferenceDataRepository.kt
@@ -5,7 +5,10 @@ import uk.gov.justice.digital.hmpps.exception.NotFoundException
 
 interface ReferenceDataRepository : JpaRepository<ReferenceData, Long> {
     fun findByCodeAndSetName(code: String, set: String): ReferenceData?
-}
+    fun getByCodeAndSetName(code: String, set: String): ReferenceData =
+        findByCodeAndSetName(code, set) ?: throw NotFoundException(set, "code", code)
 
-fun ReferenceDataRepository.getByCodeAndSetName(code: String, set: String): ReferenceData =
-    findByCodeAndSetName(code, set) ?: throw NotFoundException(set, "code", code)
+    fun getV2Tier(tierScore: String) = getByCodeAndSetName("U${tierScore}", "TIER")
+    fun getV3Tier(tierScore: String, provisional: Boolean?) =
+        getByCodeAndSetName("SP${tierScore}${if (provisional == true) "I" else ""}", "TIER")
+}
--- projects/tier-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/tier/TierCalculation.kt
+++ projects/tier-to-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/tier/TierCalculation.kt
@@ -2,8 +2,15 @@ package uk.gov.justice.digital.hmpps.integrations.tier
 
 import java.time.ZonedDateTime
 
-data class TierCalculation(
+data class TierCalculationV2(
     val tierScore: String,
+    val calculationId: String,
+    val calculationDate: ZonedDateTime
+)
+
+data class TierCalculationV3(
+    val tierScore: String,
+    val provisional: Boolean,
     val calculationId: String,
     val calculationDate: ZonedDateTime
 )
