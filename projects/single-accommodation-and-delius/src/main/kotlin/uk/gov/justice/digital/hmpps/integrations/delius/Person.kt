f6b3c7ad80de117be0aceb2b295a020f765a2957
Marcus Aspin <maspin@unilink.com>
2026-07-30 13:27:36 +0100

    PI-4388 Fix staff/team lookup on caseload

--- projects/single-accommodation-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/Person.kt
+++ projects/single-accommodation-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/integrations/delius/Person.kt
@@ -4,8 +4,11 @@ import jakarta.persistence.*
 import org.hibernate.annotations.Immutable
 import org.hibernate.annotations.SQLRestriction
 import org.hibernate.type.NumericBooleanConverter
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.EntityGraph
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import java.time.LocalDate
 
 @Entity
@@ -58,4 +61,12 @@ class Person(
 interface PersonRepository : JpaRepository<Person, Long> {
     @EntityGraph(attributePaths = ["gender", "manager.team", "manager.staff.user", "roshRegistrations.type"])
     fun findByCrn(crn: String): Person?
+
+    @EntityGraph(attributePaths = ["gender", "manager.team", "manager.staff.user", "roshRegistrations.type"])
+    @Query("select distinct p from Person p join Caseload c on p.id = c.person.id where c.staff.id = :staffId")
+    fun findByCaseloadStaffId(staffId: Long, pageable: Pageable): Page<Person>
+
+    @EntityGraph(attributePaths = ["gender", "manager.team", "manager.staff.user", "roshRegistrations.type"])
+    @Query("select distinct p from Person p join Caseload c on p.id = c.person.id where c.team.code = :teamCode")
+    fun findByCaseloadTeamCode(teamCode: String, pageable: Pageable): Page<Person>
 }
