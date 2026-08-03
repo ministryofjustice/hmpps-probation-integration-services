916c6628a8b74a83b676e24a831c6701f42e157b
joseph-bcl <41573782+joseph-bcl@users.noreply.github.com>
2026-08-03 12:47:42 +0100

    PI-4425 - remove ro details from cosso's sign and send response (#6687)

--- projects/cosso-and-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/SignAndSendIntegrationTest.kt
+++ projects/cosso-and-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/SignAndSendIntegrationTest.kt
@@ -10,12 +10,9 @@ import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.get
 import uk.gov.justice.digital.hmpps.data.generator.PersonGenerator.DEFAULT_PERSON
 import uk.gov.justice.digital.hmpps.data.generator.PersonGenerator.PERSON_IN_PRISON
-import uk.gov.justice.digital.hmpps.data.generator.PersonGenerator.PERSON_WITH_RESPONSIBLE_OFFICER_WITHOUT_USER
 import uk.gov.justice.digital.hmpps.data.generator.UserGenerator
-import uk.gov.justice.digital.hmpps.model.OfficeAddress
 import uk.gov.justice.digital.hmpps.model.SignAndSendResponse
 import uk.gov.justice.digital.hmpps.test.MockMvcExtensions.contentAsJson
-import uk.gov.justice.digital.hmpps.test.MockMvcExtensions.jsonPath
 import uk.gov.justice.digital.hmpps.test.MockMvcExtensions.withToken
 
 @AutoConfigureMockMvc
@@ -32,15 +29,6 @@ class SignAndSendIntegrationTest @Autowired constructor(
             .andReturn().response.contentAsJson<SignAndSendResponse>()
         assertThat(result.userDetails?.forenames).isEqualTo("John Test")
         assertThat(result.userDetails?.surname).isEqualTo("Smith")
-        assertThat(result.responsibleOfficer.title).isEqualTo("Mr")
-        assertThat(result.responsibleOfficer.name?.forename).isEqualTo("John")
-        assertThat(result.responsibleOfficer.name?.middleName).isEqualTo("Bob")
-        assertThat(result.responsibleOfficer.name?.surname).isEqualTo("Smith")
-        assertThat(result.responsibleOfficer.telephoneNumber).isEqualTo("07247764536")
-        assertThat(result.responsibleOfficer.emailAddress).isEqualTo("john.smith@moj.gov.uk")
-        assertThat(result.responsibleOfficer.addresses.size).isEqualTo(2)
-        assertThat(result.responsibleOfficer.addresses[0].status).isEqualTo("Default")
-        assertThat(result.responsibleOfficer.addresses[1].status).isNull()
     }
 
     @Test
@@ -51,30 +39,12 @@ class SignAndSendIntegrationTest @Autowired constructor(
             .andReturn().response.contentAsJson<SignAndSendResponse>()
         assertThat(result.userDetails?.forenames).isEqualTo("Jack Test")
         assertThat(result.userDetails?.surname).isEqualTo("Harry")
-        assertThat(result.responsibleOfficer.title).isEqualTo("Mr")
-        assertThat(result.responsibleOfficer.name?.forename).isEqualTo("Jack")
-        assertThat(result.responsibleOfficer.name?.middleName).isEqualTo("Pom")
-        assertThat(result.responsibleOfficer.name?.surname).isEqualTo("Harry")
-        assertThat(result.responsibleOfficer.telephoneNumber).isEqualTo("07123456789")
-        assertThat(result.responsibleOfficer.emailAddress).isEqualTo("jack.harry@moj.gov.uk")
-        assertThat(result.responsibleOfficer.addresses.size).isEqualTo(2)
-        assertThat(result.responsibleOfficer.addresses[0].status).isEqualTo("Default")
-        assertThat(result.responsibleOfficer.addresses[1].status).isNull()
     }
 
     @Test
     fun `username not found returns 404 response`() {
         mockMvc.get("/sign-and-send/${DEFAULT_PERSON.crn}/nonexistent") { withToken() }
             .andExpect { status { isNotFound() } }
-    }
-
-    @Test
-    fun `no home area returns no addresses`() {
-        mockMvc.get("/sign-and-send/${PERSON_WITH_RESPONSIBLE_OFFICER_WITHOUT_USER.crn}/NoHomeArea") { withToken() }
-            .andExpect {
-                status { isOk() }
-                content { jsonPath("$.responsibleOfficer.addresses", emptyList<OfficeAddress>()) }
-            }
     }
 
     @Test
--- projects/cosso-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/model/SignAndSendResponse.kt
+++ projects/cosso-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/model/SignAndSendResponse.kt
@@ -1,14 +1,5 @@
 package uk.gov.justice.digital.hmpps.model
 
 data class SignAndSendResponse(
-    val userDetails: LdapName?,
-    val responsibleOfficer: ResponsibleOfficerResponse
-)
-
-data class ResponsibleOfficerResponse(
-    val title: String?,
-    val name: Name?,
-    val telephoneNumber: String?,
-    val emailAddress: String?,
-    val addresses: List<OfficeAddress>
+    val userDetails: LdapName?
 )
