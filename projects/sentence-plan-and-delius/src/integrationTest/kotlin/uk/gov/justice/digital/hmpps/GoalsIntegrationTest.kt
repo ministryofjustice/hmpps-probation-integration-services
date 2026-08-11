78e6105546376795816790c7c7aa44e6632bc606
Hazzaz Imtiaz <hazzaz.imtiaz@justice.gov.uk>
2026-08-11 14:45:51 +0100

    [PI-3930] BRANCH To raise coverage

--- projects/sentence-plan-and-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/GoalsIntegrationTest.kt
+++ projects/sentence-plan-and-delius/src/integrationTest/kotlin/uk/gov/justice/digital/hmpps/GoalsIntegrationTest.kt
@@ -2,6 +2,7 @@ package uk.gov.justice.digital.hmpps
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.any
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.context.SpringBootTest
@@ -20,6 +21,11 @@ import uk.gov.justice.digital.hmpps.service.entity.EventRepository
 import uk.gov.justice.digital.hmpps.telemetry.TelemetryService
 import java.time.LocalDate
 import java.time.ZonedDateTime
+import kotlin.text.get
+import org.mockito.kotlin.argThat
+import org.mockito.kotlin.eq
+import org.mockito.kotlin.verify
+
 
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = RANDOM_PORT)
@@ -75,4 +81,48 @@ internal class GoalsIntegrationTest @Autowired constructor(
         assertThat(updated.spGoalsComplete).isNull()
         assertThat(updated.spGoalsDate).isNull()
     }
+
+    @Test
+    fun `goals completed logs telemetry when no active disposals found`() {
+        val crn = PersonGenerator.NON_CUSTODIAL.crn  // or use a CRN with no active disposals
+        val message = HmppsDomainEvent(
+            eventType = "arns.sentence.plan.goals.completed",
+            version = 1,
+            occurredAt = ZonedDateTime.now(),
+            description = "No more open goals",
+            personReference = PersonReference(listOf(PersonIdentifier("CRN", "UNKNOWN1"))),
+            additionalInformation = mapOf("planUuid" to "some-uuid")
+        )
+        val notification = Notification(message = message, attributes = MessageAttributes(eventType = message.eventType))
+
+        channelManager.getChannel(queueName).publishAndWait(notification)
+
+        verify(telemetryService).trackEvent(
+            eq("GoalsAchievedNoActiveDisposal"),
+            argThat<Map<String, String>> { this["crn"] == "UNKNOWN1" },
+            any()
+        )
+    }
+
+    @Test
+    fun `goals added logs telemetry when no active disposals found`() {
+        val message = HmppsDomainEvent(
+            eventType = "arns.sentence.plan.goals.added",
+            version = 1,
+            occurredAt = ZonedDateTime.now(),
+            description = "There is an open goal",
+            personReference = PersonReference(listOf(PersonIdentifier("CRN", "UNKNOWN1"))),
+            additionalInformation = mapOf("planUuid" to "some-uuid")
+        )
+        val notification = Notification(message = message, attributes = MessageAttributes(eventType = message.eventType))
+
+        channelManager.getChannel(queueName).publishAndWait(notification)
+
+        verify(telemetryService).trackEvent(
+            eq("GoalsAchievedRemovedNoActiveDisposal"),
+            argThat<Map<String, String>> { this["crn"] == "UNKNOWN1" },
+            any()
+        )
+    }
+
 }
