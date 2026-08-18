aa114791d461c75384007e7525beab68cf1a14ce
joseph-bcl <41573782+joseph-bcl@users.noreply.github.com>
2026-08-18 14:22:39 +0100

    PI-4380 - Trim Create and vary a licence API response (#6732)
    - Trim the following endpoints responses:
    /staff/byid/{staffIdentifier}/caseload/managed-offenders
    /team/$teamCode/caseload/managed-offenders
    /staff/byid/{staffIdentifier}/caseload/team-managed-offenders
    
    ---------
    
    Co-authored-by: probation-integration-bot[bot] <177347787+probation-integration-bot[bot]@users.noreply.github.com>

--- projects/create-and-vary-a-licence-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/TeamService.kt
+++ projects/create-and-vary-a-licence-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/TeamService.kt
@@ -1,7 +1,7 @@
 package uk.gov.justice.digital.hmpps.service
 
 import org.springframework.stereotype.Service
-import uk.gov.justice.digital.hmpps.api.model.ManagedOffender
+import uk.gov.justice.digital.hmpps.api.model.ManagedOffenderSummary
 import uk.gov.justice.digital.hmpps.integrations.delius.caseload.entity.Caseload.CaseloadRole
 import uk.gov.justice.digital.hmpps.integrations.delius.caseload.entity.CaseloadRepository
 
@@ -9,11 +9,11 @@ import uk.gov.justice.digital.hmpps.integrations.delius.caseload.entity.Caseload
 class TeamService(
     private val caseloadRepository: CaseloadRepository,
 ) {
-    fun getManagedOffendersByTeam(teamCode: String): List<ManagedOffender> =
+    fun getManagedOffendersByTeam(teamCode: String): List<ManagedOffenderSummary> =
         caseloadRepository.findByTeamCodeAndRoleCodeOrderByAllocationDateDesc(
             teamCode,
             CaseloadRole.OFFENDER_MANAGER.value
         ).map {
-            it.asManagedOffender()
+            it.asManagedOffenderSummary()
         }
 }
