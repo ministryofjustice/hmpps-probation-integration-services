9c73f5d48516c57c3dd3b29c90622f50953087db
Hazzaz Imtiaz <hazzaz.imtiaz@justice.gov.uk>
2026-09-03 18:16:23 +0100

    [PI-4504] Revert lib case access class and passing a null access object

--- projects/single-accommodation-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/CaseListService.kt
+++ projects/single-accommodation-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/CaseListService.kt
@@ -70,18 +70,15 @@ class CaseListService(
 
     fun getCase(username: String, crn: String): Case {
         val access = userAccessService.caseAccessFor(username, crn)
-        return getCaseByAccess(access)
+        return getCaseByAccess(crn, access)
     }
 
-    fun getCase(crn: String): Case {
-        val access = CaseAccess(crn = crn, userExcluded = null, userRestricted = null)
-        return getCaseByAccess(access)
-    }
+    fun getCase(crn: String) = getCaseByAccess(crn, access = null)
 
-    private fun getCaseByAccess(access: CaseAccess): Case {
-        val person = personRepository.findByCrn(access.crn).orNotFoundBy("CRN", access.crn)
+    private fun getCaseByAccess(crn: String, access: CaseAccess?): Case {
+        val person = personRepository.findByCrn(crn).orNotFoundBy("CRN", crn)
         val caseLimitedAccess =
-            userAccessService.checkLimitedAccessFor(listOf(access.crn)).access.single { it.crn == access.crn }
+            userAccessService.checkLimitedAccessFor(listOf(access?.crn ?: crn)).access.single { it.crn == access?.crn ?: crn }
                 .isLimitedAccess()
         return person.toCase(
             access,
@@ -90,11 +87,7 @@ class CaseListService(
         )
     }
 
-    private fun CaseAccess.isLimitedAccess(): Boolean? = when {
-        userExcluded == true || userRestricted == true -> true
-        userExcluded == null || userRestricted == null -> null
-        else -> false
-    }
+    private fun CaseAccess.isLimitedAccess() = this.userExcluded || this.userRestricted
 
     private fun Person.toCase(
         access: CaseAccess?,
