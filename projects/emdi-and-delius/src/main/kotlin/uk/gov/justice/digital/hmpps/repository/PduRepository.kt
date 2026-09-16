2b30dfd71b0792a4db32d989ee2d6555790fdcbc
Hazzaz Imtiaz <hazzaz.imtiaz@justice.gov.uk>
2026-09-16 23:42:17 +0100

    [PI-4460] EMDI - API to return a list of providers and PDUs

---
+++ projects/emdi-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/repository/PduRepository.kt
@@ -0,0 +1,9 @@
+package uk.gov.justice.digital.hmpps.repository
+
+
+import org.springframework.data.jpa.repository.JpaRepository
+import uk.gov.justice.digital.hmpps.entity.staff.ProbationDeliveryUnit
+
+interface PduRepository : JpaRepository<ProbationDeliveryUnit, Long> {
+    fun findByProviderCodeAndSelectableTrueAndProviderSelectableTrue(code: String): List<ProbationDeliveryUnit>
+}
---
+++ projects/emdi-and-delius/src/main/kotlin/uk/gov/justice/digital/hmpps/service/ReferenceDataService.kt
@@ -0,0 +1,20 @@
+package uk.gov.justice.digital.hmpps.service
+
+
+import org.springframework.stereotype.Service
+import uk.gov.justice.digital.hmpps.model.CodedValue
+import uk.gov.justice.digital.hmpps.repository.PduRepository
+import uk.gov.justice.digital.hmpps.repository.ProviderRepository
+
+
+@Service
+class ReferenceDataService(
+    private val providerRepository: ProviderRepository,
+    private val pduRepository: PduRepository,
+) {
+    fun regions(): List<CodedValue> = providerRepository.findBySelectableTrue()
+        .map { it.toCodedValue() }
+
+    fun pdus(regionCode: String): List<CodedValue> = pduRepository.findByProviderCodeAndSelectableTrueAndProviderSelectableTrue(regionCode)
+        .map { it.toCodedValue() }
+}
